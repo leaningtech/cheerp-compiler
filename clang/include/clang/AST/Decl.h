af8a4cd01963516cdd93d881a2cc6c9b1dda8256
Alessandro Pignotti <a.pignotti@sssup.it>
2012-05-20 23:59:45 +0200

    Export the skel function using a metadata node

--- clang/include/clang/AST/Decl.h
+++ clang/include/clang/AST/Decl.h
@@ -2924,6 +2924,9 @@ public:
   static FunctionDecl *castFromDeclContext(const DeclContext *DC) {
     return static_cast<FunctionDecl *>(const_cast<DeclContext*>(DC));
   }
+
+  //Duetto, pointer to the skel function for this server function
+  FunctionDecl* skelFunction;
 };
 
 /// Represents a member of a struct/union/class.
