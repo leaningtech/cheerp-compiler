2d028fb958d45e936d60e2a7c38dcd339c6ea3c3
Alessandro Pignotti <a.pignotti@sssup.it>
2012-05-20 15:56:18 +0200

    Add a ByteAddressable flag in TargetInfo

--- clang/include/clang/Basic/TargetInfo.h
+++ clang/include/clang/Basic/TargetInfo.h
@@ -210,6 +210,7 @@ protected:
   // Target values set by the ctor of the actual target implementation.  Default
   // values are specified by the TargetInfo constructor.
   bool BigEndian;
+  bool ByteAddressable;
   bool TLSSupported;
   bool VLASupported;
   bool NoAsmVariants;  // True if {|} are normal characters.
@@ -1539,6 +1540,8 @@ public:
 
   /// Controls if __arithmetic_fence is supported in the targeted backend.
   virtual bool checkArithmeticFenceSupported() const { return false; }
+
+  bool isByteAddressable() const { return ByteAddressable; }
 
   /// Gets the default calling convention for the given target and
   /// declaration context.
