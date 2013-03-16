cec7d932a4a11227a4fedef94488b2442bfa40b8
Alessandro Pignotti <a.pignotti@sssup.it>
2013-03-16 17:41:24 +0100

    duetto-compiler: Support undefined values

--- llvm/tools/duetto-compiler/duetto-compiler.cpp
+++ llvm/tools/duetto-compiler/duetto-compiler.cpp
@@ -750,6 +750,10 @@ void JSWriter::compileConstant(const Constant* c)
 	{
 		stream << "null";
 	}
+	else if(UndefValue::classof(c))
+	{
+		stream << "undefined";
+	}
 	else if(c->hasName())
 	{
 		printLLVMName(c->getName());
