eeb61da7eb5adf8d734d95ccddcd84ae1c8df778
Alessandro Pignotti <a.pignotti@sssup.it>
2013-04-30 01:55:05 +0200

    llvm: Initial support for a proper duetto backend

---
+++ llvm/lib/Target/DuettoBackend/TargetInfo/DuettoBackendTargetInfo.cpp
@@ -0,0 +1,18 @@
+//===-- DuettoBackendTargetInfo.cpp - TargetInfo for the DuettoBackend ----===//
+//
+//	Copyright 2011-2013 Leaning Technlogies
+//===----------------------------------------------------------------------===//
+
+#include "DuettoTargetMachine.h"
+#include "llvm/Support/TargetRegistry.h"
+
+using namespace llvm;
+
+Target llvm::TheDuettoBackendTarget;
+
+extern "C" void LLVMInitializeDuettoBackendTargetInfo() { 
+  RegisterTarget<Triple::duetto, /*HasJIT=*/false>
+    X(TheDuettoBackendTarget, "duetto", "Duetto client side backend");
+}
+
+extern "C" void LLVMInitializeDuettoBackendTargetMC() {}
