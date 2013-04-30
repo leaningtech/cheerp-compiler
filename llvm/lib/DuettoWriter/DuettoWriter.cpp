94fce16b16842cbcd49f49fc6f7534926fb2533c
Alessandro Pignotti <a.pignotti@sssup.it>
2013-04-30 22:02:47 +0200

    duetto-writer: Allow downcasts on client types

--- llvm/lib/DuettoWriter/DuettoWriter.cpp
+++ llvm/lib/DuettoWriter/DuettoWriter.cpp
@@ -308,6 +308,12 @@ Type* DuettoWriter::findRealType(const Value* v, std::set<const PHINode*>& visit
 	return v->getType();
 }
 
+void DuettoWriter::compileDowncast(const Value* src)
+{
+	assert(isClientType(src->getType()));
+	compileOperand(src);
+}
+
 void DuettoWriter::compileCopy(const Value* dest, const Value* src, const Value* size)
 {
 	//Find out the real type of the copied object
@@ -537,6 +543,11 @@ bool DuettoWriter::handleBuiltinCall(const char* ident, const Value* callV,
 	}
 	else if(strncmp(ident,"llvm.lifetime",13)==0)
 	{
+		return true;
+	}
+	else if(strncmp(ident,"llvm.duetto.downcast",20)==0)
+	{
+		compileDowncast(*(it));
 		return true;
 	}
 	else if(strcmp(ident,"malloc")==0 ||
