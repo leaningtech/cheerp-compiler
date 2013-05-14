3d22fb179fc894d70ad1d11c221cdc4f270b0a9a
Alessandro Pignotti <a.pignotti@sssup.it>
2013-05-14 19:58:22 +0200

    Add support for virtual calls

--- clang/lib/CodeGen/CodeGenFunction.h
+++ clang/lib/CodeGen/CodeGenFunction.h
@@ -2230,6 +2230,7 @@ public:
   /// Struct with all information about dynamic [sub]class needed to set vptr.
   struct VPtr {
     BaseSubobject Base;
+    llvm::SmallVector<const CXXRecordDecl*, 4> Bases;
     const CXXRecordDecl *NearestVBase;
     CharUnits OffsetFromNearestVBase;
     const CXXRecordDecl *VTableClass;
@@ -2243,7 +2244,9 @@ public:
   typedef llvm::SmallPtrSet<const CXXRecordDecl *, 4> VisitedVirtualBasesSetTy;
   VPtrsVector getVTablePointers(const CXXRecordDecl *VTableClass);
 
-  void getVTablePointers(BaseSubobject Base, const CXXRecordDecl *NearestVBase,
+  void getVTablePointers(BaseSubobject Base,
+                         const llvm::SmallVector<const CXXRecordDecl*, 4>& Bases,
+                         const CXXRecordDecl *NearestVBase,
                          CharUnits OffsetFromNearestVBase,
                          bool BaseIsNonVirtualPrimaryBase,
                          const CXXRecordDecl *VTableClass,
