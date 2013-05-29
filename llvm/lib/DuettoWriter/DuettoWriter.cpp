3d26fa743f7aad38d07c7e2dc3987c759f84a99f
Alessandro Pignotti <a.pignotti@sssup.it>
2013-05-29 02:14:14 +0200

    DuettoWriter: Fix return of complete objects

--- llvm/lib/DuettoWriter/DuettoWriter.cpp
+++ llvm/lib/DuettoWriter/DuettoWriter.cpp
@@ -1401,7 +1401,7 @@ void DuettoWriter::compileTerminatorInstruction(const TerminatorInst& I)
 			Value* retVal = ri.getReturnValue();
 			stream << "return ";
 			if(retVal)
-				compileOperand(retVal);
+				compileOperand(retVal, OPERAND_EXPAND_COMPLETE_OBJECTS);
 			stream << ";\n";
 			break;
 		}
