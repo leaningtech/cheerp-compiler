3e0986d9155116105a18ecf31b407e945260fe79
Alessandro Pignotti <a.pignotti@sssup.it>
2013-08-25 17:27:11 +0200

    Do not memcpy-optimize in constructors on NBA targets

--- clang/lib/CodeGen/CGClass.cpp
+++ clang/lib/CodeGen/CGClass.cpp
@@ -1214,6 +1214,8 @@ namespace {
     bool isMemberInitMemcpyable(CXXCtorInitializer *MemberInit) const {
       if (!MemcpyableCtor)
         return false;
+      if (!CGF.getTarget().isByteAddressable())
+        return false;
       FieldDecl *Field = MemberInit->getMember();
       assert(Field && "No field for member init.");
       QualType FieldType = Field->getType();
