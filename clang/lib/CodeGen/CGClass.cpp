fb739afc98c713911f76ba2bd28a9e472121765c
Alessandro Pignotti <a.pignotti@sssup.it>
2013-08-25 17:26:51 +0200

    Fix upcasts for NBA targets when null check is requested

--- clang/lib/CodeGen/CGClass.cpp
+++ clang/lib/CodeGen/CGClass.cpp
@@ -425,7 +425,7 @@ Address CodeGenFunction::GetAddressOfBaseClass(
 
   // First handle the non-byte addressable case (Duetto)
   if (!getTarget().isByteAddressable())
-    return GenerateUpcast(Value, Derived, PathBegin, PathEnd);
+    Value = GenerateUpcast(Value, Derived, PathBegin, PathEnd);
   else
   {
     // Apply both offsets.
