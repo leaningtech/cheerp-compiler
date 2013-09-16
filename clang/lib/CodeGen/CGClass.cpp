1b57901935b2a7d64b07089e6b5f2a6b5997fb5a
Alessandro Pignotti <a.pignotti@sssup.it>
2013-09-17 00:34:32 +0200

    Fix use of memcpy on duetto to implement operator=

--- clang/lib/CodeGen/CGClass.cpp
+++ clang/lib/CodeGen/CGClass.cpp
@@ -1079,6 +1079,8 @@ namespace {
       Qualifiers Qual = F->getType().getQualifiers();
       if (Qual.hasVolatile() || Qual.hasObjCLifetime())
         return false;
+      if (!CGF.getTarget().isByteAddressable())
+        return false;
       return true;
     }
 
@@ -1216,8 +1218,6 @@ namespace {
     // that can be rolled into a memcpy.
     bool isMemberInitMemcpyable(CXXCtorInitializer *MemberInit) const {
       if (!MemcpyableCtor)
-        return false;
-      if (!CGF.getTarget().isByteAddressable())
         return false;
       FieldDecl *Field = MemberInit->getMember();
       assert(Field && "No field for member init.");
