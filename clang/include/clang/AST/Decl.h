a847e7902b672294a81ad3a66cf8cc9f66559a22
Alessandro Pignotti <a.pignotti@sssup.it>
2013-11-06 21:49:48 +0100

    Improve robustness of client stubs body generation

--- clang/include/clang/AST/Decl.h
+++ clang/include/clang/AST/Decl.h
@@ -2837,7 +2837,6 @@ public:
   FunctionDecl* skelFunction;
   //Duetto, pointer to the stub function for this server function
   FunctionDecl* stubFunction;
-  Stmt* stubBody;
 };
 
 /// Represents a member of a struct/union/class.
