c3eaf399e36f32dae7c89ff1ba0496b685252d4e
Alessandro Pignotti <a.pignotti@sssup.it>
2013-11-28 17:27:48 +0100

    Remove some obsolete code in client stub generation

--- clang/include/clang/AST/Decl.h
+++ clang/include/clang/AST/Decl.h
@@ -2927,8 +2927,6 @@ public:
 
   //Duetto, pointer to the skel function for this server function
   FunctionDecl* skelFunction;
-  //Duetto, pointer to the stub function for this server function
-  FunctionDecl* stubFunction;
 };
 
 /// Represents a member of a struct/union/class.
