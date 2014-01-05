30e5e2c191220426be31e0f0a2867a19b29d41f3
Alessandro Pignotti <a.pignotti@sssup.it>
2014-01-05 14:55:45 +0100

    DuettoBackend: Disable conversion from COMPLETE_OBJECT to COMPLETE_ARRAY
    In preparation for "self" optimization

--- llvm/include/llvm/Duetto/Writer.h
+++ llvm/include/llvm/Duetto/Writer.h
@@ -86,7 +86,7 @@ private:
 	void compileEqualPointersComparison(const llvm::Value* lhs, const llvm::Value* rhs, llvm::CmpInst::Predicate p);
 	void compileType(llvm::Type* t);
 	void compileTypeImpl(llvm::Type* t);
-	enum POINTER_KIND { COMPLETE_OBJECT = 0, COMPLETE_ARRAY, REGULAR };
+	enum POINTER_KIND { UNDECIDED = 0, COMPLETE_OBJECT, COMPLETE_ARRAY, REGULAR };
 	POINTER_KIND getPointerKind(const llvm::Value* v, std::map<const llvm::PHINode*, POINTER_KIND>& visitedPhis);
 	POINTER_KIND getPointerKind(const llvm::Value* v);
 	bool isDowncast(const llvm::Value* val) const;
