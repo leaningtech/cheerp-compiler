96ebc58d010c296c8ccfb4f37fbb8d65fe74387c
Alessandro Pignotti <a.pignotti@sssup.it>
2014-01-20 02:40:59 +0100

    Do not emit user cast intrinsics if a cast is duetto safe

--- clang/lib/CodeGen/CGExprScalar.cpp
+++ clang/lib/CodeGen/CGExprScalar.cpp
@@ -2091,7 +2091,8 @@ Value *ScalarExprEmitter::VisitCastExpr(CastExpr *CE) {
       }
     }
 
-    if (CGF.getTarget().isByteAddressable() || isa<llvm::ConstantPointerNull>(Src) ||
+    //We don't care about casts to functions types
+    if (CGF.getTarget().isByteAddressable() || CE->isDuettoSafe() || isa<llvm::ConstantPointerNull>(Src) ||
         (isa<llvm::Function>(Src) && isa<llvm::FunctionType>(DstTy)))
     {
       return Builder.CreateBitCast(Src, DstTy);
