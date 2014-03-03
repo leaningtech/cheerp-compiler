a9809de4e2657db1c5b5b5fdd72ccc9b9d4de9fc
Alessandro Pignotti <a.pignotti@sssup.it>
2014-03-03 17:15:35 +0100

    Use fully typed duetto.upcast.collapsed intrinsic

--- clang/lib/CodeGen/CodeGenFunction.h
+++ clang/lib/CodeGen/CodeGenFunction.h
@@ -2836,7 +2836,9 @@ public:
                                 bool NullCheckValue, SourceLocation Loc);
 
   llvm::Value * GenerateUpcastCollapsed(llvm::Value* Value,
-                               llvm::Type *BaseTy);
+                               QualType BaseTy,
+                               QualType DerivedTy,
+                               llvm::Type *BasePtrTy);
   llvm::Value * GenerateUpcast(llvm::Value* Value,
                                const CXXRecordDecl *Derived,
                                CastExpr::path_const_iterator PathBegin,
