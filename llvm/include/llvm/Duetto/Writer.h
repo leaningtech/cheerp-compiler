fff85dcddcf71f305f1be2fdb4e7c7a313a6d046
Ennio Barbaro <enniobarbaro@gmail.com>
2014-03-03 17:25:06 +0100

    Working no-self-member optimization.

--- llvm/include/llvm/Duetto/Writer.h
+++ llvm/include/llvm/Duetto/Writer.h
@@ -95,9 +95,91 @@ private:
 	enum POINTER_KIND { UNDECIDED = 0, COMPLETE_OBJECT, COMPLETE_ARRAY, REGULAR };
 	POINTER_KIND getPointerKind(const llvm::Value* v, std::map<const llvm::PHINode*, POINTER_KIND>& visitedPhis);
 	POINTER_KIND getPointerKind(const llvm::Value* v);
-	// ALWAYS_DEREFERENCED means that the pointer is always used as a base for a GEP, 0 or a store/load
-	enum POINTER_USAGE { ANY = 0, ALWAYS_DEREFERENCED };
-	POINTER_USAGE getPointerUsage(const llvm::Value* v);
+	
+	/**
+	 * \addtogroup pointers Pointer implementation
+	 * 
+	 * Three type of pointers are used:
+	 *   - COMPLETE_OBJECT This pointer can point only to a C++ struct/class type. It is implemented 
+	 *                     by directly binding a JS var to the pointed object. This type of pointer
+	 *                     does not support arithmetic nor ordering. In order to allow conversion to REGULAR pointer,
+	 *                     the object bound to a COMPLETE_OBJECT pointer shall contain the member "s", which is a JS ref to the object itself.
+	 *   - COMPLETE_ARRAY  This pointer points to the first element of a C++ array of struct/class or primitive type.
+	 *                     It is implemented in JS by binding a var to the pointed array. It does not support
+	 *                     arithmetic nor ordering.
+	 *   - REGULAR         Regular pointers are implemented with a JS object of the form: "{ d : OBJ, o : OFFSET }", where
+	 *                     OBJ is a JS reference to a *container* object of the pointer, and OFFSET is the offset of the pointed
+	 *                     object inside the container object (it can be a character or a integral value).
+	 *                     Pointers of type COMPLETE_OBJECT and COMPLETE_ARRAY are always convertible to REGULAR pointers,
+	 *                     but not viceversa. REGULAR pointers support pointer arithmetic; two REGULAR pointers are ordinable 
+	 *                     if they have the same container object. 
+	 * 
+	 * General rules (which apply both to JS immutable types and to struct/class objects):
+	 *    -# In order for an object to be bound to a regular pointer, a "container" object must exists. If the 
+	 *       object being bound to a pointer is a member object of some class/struct type or a member of an array the 
+	 *       container object naturally exists. Otherwise such object must be created inside an array of size one.
+	 * 
+	 *    -# An array of objects can always be bound to a COMPLETE_ARRAY pointer, just by taking its name.
+	 * 
+	 *    -# A single object can always be bound to a COMPLETE_OBJECT pointer, just by taking its name.
+	 * 
+	 *    -# A COMPLETE_ARRAY pointer can be converted to a REGULAR pointer by setting the "d" member to the COMPLETE_ARRAY pointer
+	 *       name, and the "o" member to zero.
+	 * 
+	 *    -# A COMPLETE_OBJECT pointer can be converted to a REGULAR pointer using the self member. Notice that
+	 *       arithmetic operations on such pointer are still available, provided the user does not dereference the result, and generate
+	 *       a on offset value like "s1, s2" etc.
+	 *       
+	 * 
+	 * Optimization:
+	 *    -  Remove self-pointer. Avoid the creation of the member ".s" if the conversion to REGULAR pointer is not required.
+	 * 
+	 * 
+	 * @{
+	 */
+	
+	// Functionalities provided by a pointer
+	enum POINTER_USAGE_FLAG {
+		POINTER_NONCONST_DEREF = 0x1, // The pointer is used to modify the pointed object
+		POINTER_ARITHMETIC = 0x2, // The pointer can be incremented/decremented etc, and/or it is used to access an array (i.e. p[i])
+		POINTER_ORDINABLE = 0x4, // The pointer is used for a comparison with another pointer
+		POINTER_CASTABLE_TO_INT = 0x8,  // The pointer is explicitly casted to an integer (usually used to implement pointers hash table)
+		
+		POINTER_UNKNOWN = (1LL << 32LL) - 1
+	};
+
+	typedef std::map<const llvm::Value *, uint32_t> pointer_usage_map_t;
+	
+	// Returns a bitmask of POINTER_USAGE_FLAG
+	/** 
+	 * Compute the usage of a single pointer, regardless of nodes
+	 */
+	//TODO at the moment if it is used in a CallInst it returns POINTER_UNKNOWN.
+	// CallInst should be handled inside getPointerUsageFlagsComplete, in order to provide information on how that pointer is used inside the function call.
+	// This is especially important at the moment for memset/memcpy/memmove.
+	uint32_t getPointerUsageFlags(const llvm::Value* v);
+	pointer_usage_map_t pointerUsageMap;
+	
+	/**
+	 * Compute the sum of the usages of all the "child" pointers, where "child pointer" means any pointer which can be initialized to this one's value.
+	 * \param v The pointer to inspect.
+	 * \param openset Set of the visited pointers in order to stop cyclic dependencies in the phi node.
+	 */
+	uint32_t dfsPointerUsageFlagsComplete(const llvm::Value * v,std::set<const llvm::Value *> & openset);
+	
+	/**
+	 * Memoization wrapper around dfsPointerUsageFlagsComplete
+	 */
+	uint32_t getPointerUsageFlagsComplete(const llvm::Value * v);
+	pointer_usage_map_t pointerCompleteUsageMap;
+	
+#ifdef DUETTO_DEBUG_POINTERS
+	typedef std::set<const llvm::Value *> known_pointers_t;
+	known_pointers_t debugAllPointersSet;
+#endif //DUETTO_DEBUG_POINTERS
+	
+	/** @} */
+
 	/*
 	 * \param v The pointer to dereference, it may be a regular pointer, a complete obj or a complete array
 	 * \param offset An offset coming from code, which may be also NULL
