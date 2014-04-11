a8f2e35a86d6367cbb4216a6ba267e20321a3b8f
Alessandro Pignotti <a.pignotti@sssup.it>
2014-04-11 23:09:29 +0200

    DuettoBackend: Cosmetic fixes

--- llvm/lib/DuettoWriter/NameGenerator.cpp
+++ llvm/lib/DuettoWriter/NameGenerator.cpp
@@ -1,3 +1,13 @@
+//===-- Opcodes.cpp - The Duetto JavaScript generator ---------------------===//
+//
+//                     Duetto: The C++ compiler for the Web
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+// Copyright 2014 Leaning Technologies
+//
+//===----------------------------------------------------------------------===//
 
 #include "llvm/Duetto/NameGenerator.h"
 
