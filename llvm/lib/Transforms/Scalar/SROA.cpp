acd3c2ae77769545f9a43ac1550110b8d7d44fec
Alessandro Pignotti <a.pignotti@sssup.it>
2014-04-16 02:12:27 +0200

    Do not collapse nagative GEP indexes during SROA

--- llvm/lib/Transforms/Scalar/SROA.cpp
+++ llvm/lib/Transforms/Scalar/SROA.cpp
@@ -1600,6 +1600,8 @@ static Value *getAdjustedPtr(IRBuilderTy &IRB, const DataLayout &DL, Value *Ptr,
       APInt GEPOffset(Offset.getBitWidth(), 0);
       if (!GEP->accumulateConstantOffset(DL, GEPOffset))
         break;
+      if (GEPOffset.isNegative())
+        break;
       Offset += GEPOffset;
       Ptr = GEP->getPointerOperand();
       if (!Visited.insert(Ptr).second)
