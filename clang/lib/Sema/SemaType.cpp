71da060b271b1a04f67e2842c8da9b3ac8fee04c
Alessandro Pignotti <a.pignotti@sssup.it>
2014-04-20 01:19:26 +0200

    Add support for the static attribute

--- clang/lib/Sema/SemaType.cpp
+++ clang/lib/Sema/SemaType.cpp
@@ -8216,6 +8216,7 @@ static void processTypeAttrs(TypeProcessingState &state, QualType &type,
       break;
 
     case ParsedAttr::IgnoredAttribute:
+    case ParsedAttr::AT_Static:
       break;
 
     case ParsedAttr::AT_BTFTypeTag:
