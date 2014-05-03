d6063e1fa1e2cc16495447402e218c983fd1e20f
Alessandro Pignotti <a.pignotti@sssup.it>
2014-05-03 18:56:16 +0200

    Simplify computation of bases metadata for duetto

--- clang/lib/AST/RecordLayoutBuilder.cpp
+++ clang/lib/AST/RecordLayoutBuilder.cpp
@@ -688,12 +688,6 @@ protected:
   /// Valid if UseExternalLayout is true.
   ExternalLayout External;
 
-  llvm::SmallVector<unsigned, 4> BaseOffsetFromNo;
-  // The first element which is a base (e.g. not the vtable)
-  unsigned firstBaseElement;
-  // The total count of bases, including the inherited ones
-  unsigned totalNumberOfBases;
-
   ItaniumRecordLayoutBuilder(const ASTContext &Context,
                              EmptySubobjectMap *EmptySubobjects)
       : Context(Context), EmptySubobjects(EmptySubobjects), Size(0),
@@ -711,10 +705,7 @@ protected:
         PaddedFieldSize(CharUnits::Zero()), PrimaryBase(nullptr),
         PrimaryBaseIsVirtual(false), HasOwnVFPtr(false), HasPackedField(false),
         HandledFirstNonOverlappingEmptyField(false),
-        FirstNearlyEmptyVBase(nullptr),
-        firstBaseElement(0xffffffff),
-        totalNumberOfBases(1) //Initialized to 1, counting itself
-        { }
+        FirstNearlyEmptyVBase(nullptr) {}
 
   void Layout(const RecordDecl *D);
   void Layout(const CXXRecordDecl *D);
@@ -1035,7 +1026,6 @@ void ItaniumRecordLayoutBuilder::EnsureVTablePointerAlignment(
 
 void ItaniumRecordLayoutBuilder::LayoutNonVirtualBases(
     const CXXRecordDecl *RD) {
-  assert(firstBaseElement == -1 && totalNumberOfBases == 1);
 
   // Only byte addressable targets have a primary base
   if (Context.getTargetInfo().isByteAddressable())
@@ -1325,13 +1315,6 @@ ItaniumRecordLayoutBuilder::LayoutBase(const BaseSubobjectInfo *Base) {
 
   // Remember max struct/class alignment.
   UpdateAlignment(BaseAlign, UnpackedAlignTo, PreferredBaseAlign);
-
-  if (firstBaseElement==0xffffffff)
-    firstBaseElement = HasOwnVFPtr ? 1 : 0;
-
-  BaseOffsetFromNo.push_back(totalNumberOfBases);
-
-  totalNumberOfBases += Layout.getTotalNumberOfBases();
 
   return Offset;
 }
@@ -3434,8 +3417,7 @@ ASTContext::getASTRecordLayout(const RecordDecl *D) const {
           Builder.PreferredNVAlignment,
           EmptySubobjects.SizeOfLargestEmptySubobject, Builder.PrimaryBase,
           Builder.PrimaryBaseIsVirtual, nullptr, false, false, Builder.Bases,
-          Builder.VBases,
-          Builder.BaseOffsetFromNo, Builder.firstBaseElement, Builder.totalNumberOfBases);
+          Builder.VBases);
     } else {
       ItaniumRecordLayoutBuilder Builder(*this, /*EmptySubobjects=*/nullptr);
       Builder.Layout(D);
