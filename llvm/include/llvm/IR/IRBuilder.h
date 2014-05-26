4f83c0ef8820e004cbc57c7141e05de952d9e468
Alessandro Pignotti <a.pignotti@sssup.it>
2014-05-26 17:28:43 +0200

    Introduce fully typed memory intrinsics

--- llvm/include/llvm/IR/IRBuilder.h
+++ llvm/include/llvm/IR/IRBuilder.h
@@ -564,15 +564,16 @@ public:
   CallInst *CreateMemSet(Value *Ptr, Value *Val, uint64_t Size,
                          MaybeAlign Align, bool isVolatile = false,
                          MDNode *TBAATag = nullptr, MDNode *ScopeTag = nullptr,
-                         MDNode *NoAliasTag = nullptr) {
+                         MDNode *NoAliasTag = nullptr, bool byteLayout = true) {
     return CreateMemSet(Ptr, Val, getInt64(Size), Align, isVolatile,
-                        TBAATag, ScopeTag, NoAliasTag);
+                        TBAATag, ScopeTag, NoAliasTag, byteLayout);
   }
 
   CallInst *CreateMemSet(Value *Ptr, Value *Val, Value *Size, MaybeAlign Align,
                          bool isVolatile = false, MDNode *TBAATag = nullptr,
                          MDNode *ScopeTag = nullptr,
-                         MDNode *NoAliasTag = nullptr);
+                         MDNode *NoAliasTag = nullptr,
+                         bool byteLayout = true);
 
   /// Create and insert an element unordered-atomic memset of the region of
   /// memory starting at the given pointer to the given value.
@@ -608,34 +609,34 @@ public:
                          bool isVolatile = false, MDNode *TBAATag = nullptr,
                          MDNode *TBAAStructTag = nullptr,
                          MDNode *ScopeTag = nullptr,
-                         MDNode *NoAliasTag = nullptr) {
+                         MDNode *NoAliasTag = nullptr, bool byteLayout = true) {
     return CreateMemCpy(Dst, DstAlign, Src, SrcAlign, getInt64(Size),
                         isVolatile, TBAATag, TBAAStructTag, ScopeTag,
-                        NoAliasTag);
+                        NoAliasTag, byteLayout);
   }
 
   CallInst *CreateMemTransferInst(
       Intrinsic::ID IntrID, Value *Dst, MaybeAlign DstAlign, Value *Src,
       MaybeAlign SrcAlign, Value *Size, bool isVolatile = false,
       MDNode *TBAATag = nullptr, MDNode *TBAAStructTag = nullptr,
-      MDNode *ScopeTag = nullptr, MDNode *NoAliasTag = nullptr);
+      MDNode *ScopeTag = nullptr, MDNode *NoAliasTag = nullptr, bool byteLayout = true);
 
   CallInst *CreateMemCpy(Value *Dst, MaybeAlign DstAlign, Value *Src,
                          MaybeAlign SrcAlign, Value *Size,
                          bool isVolatile = false, MDNode *TBAATag = nullptr,
                          MDNode *TBAAStructTag = nullptr,
                          MDNode *ScopeTag = nullptr,
-                         MDNode *NoAliasTag = nullptr) {
+                         MDNode *NoAliasTag = nullptr, bool byteLayout = true) {
     return CreateMemTransferInst(Intrinsic::memcpy, Dst, DstAlign, Src,
                                  SrcAlign, Size, isVolatile, TBAATag,
-                                 TBAAStructTag, ScopeTag, NoAliasTag);
+                                 TBAAStructTag, ScopeTag, NoAliasTag, byteLayout);
   }
 
   CallInst *
   CreateMemCpyInline(Value *Dst, MaybeAlign DstAlign, Value *Src,
                      MaybeAlign SrcAlign, Value *Size, bool IsVolatile = false,
                      MDNode *TBAATag = nullptr, MDNode *TBAAStructTag = nullptr,
-                     MDNode *ScopeTag = nullptr, MDNode *NoAliasTag = nullptr);
+                     MDNode *ScopeTag = nullptr, MDNode *NoAliasTag = nullptr, bool byteLayout = true);
 
   /// Create and insert an element unordered-atomic memcpy between the
   /// specified pointers.
@@ -664,7 +665,7 @@ public:
                           MaybeAlign SrcAlign, Value *Size,
                           bool isVolatile = false, MDNode *TBAATag = nullptr,
                           MDNode *ScopeTag = nullptr,
-                          MDNode *NoAliasTag = nullptr);
+                          MDNode *NoAliasTag = nullptr, bool byteLayout = true);
 
   /// \brief Create and insert an element unordered-atomic memmove between the
   /// specified pointers.
--- llvm/lib/IR/IRBuilder.cpp
+++ llvm/lib/IR/IRBuilder.cpp
@@ -139,8 +139,9 @@ Value *IRBuilderBase::CreateStepVector(Type *DstType, const Twine &Name) {
 CallInst *IRBuilderBase::CreateMemSet(Value *Ptr, Value *Val, Value *Size,
                                       MaybeAlign Align, bool isVolatile,
                                       MDNode *TBAATag, MDNode *ScopeTag,
-                                      MDNode *NoAliasTag) {
-  Ptr = getCastedInt8PtrValue(Ptr);
+                                      MDNode *NoAliasTag, bool byteLayout) {
+  if (byteLayout)
+    Ptr = getCastedInt8PtrValue(Ptr);
   Value *Ops[] = {Ptr, Val, Size, getInt1(isVolatile)};
   Type *Tys[] = { Ptr->getType(), Size->getType() };
   Module *M = BB->getParent()->getParent();
@@ -195,9 +196,12 @@ CallInst *IRBuilderBase::CreateElementUnorderedAtomicMemSet(
 CallInst *IRBuilderBase::CreateMemTransferInst(
     Intrinsic::ID IntrID, Value *Dst, MaybeAlign DstAlign, Value *Src,
     MaybeAlign SrcAlign, Value *Size, bool isVolatile, MDNode *TBAATag,
-    MDNode *TBAAStructTag, MDNode *ScopeTag, MDNode *NoAliasTag) {
-  Dst = getCastedInt8PtrValue(Dst);
-  Src = getCastedInt8PtrValue(Src);
+    MDNode *TBAAStructTag, MDNode *ScopeTag, MDNode *NoAliasTag, bool byteLayout) {
+  if (byteLayout)
+  {
+    Dst = getCastedInt8PtrValue(Dst);
+    Src = getCastedInt8PtrValue(Src);
+  }
 
   Value *Ops[] = {Dst, Src, Size, getInt1(isVolatile)};
   Type *Tys[] = { Dst->getType(), Src->getType(), Size->getType() };
@@ -232,9 +236,12 @@ CallInst *IRBuilderBase::CreateMemTransferInst(
 CallInst *IRBuilderBase::CreateMemCpyInline(
     Value *Dst, MaybeAlign DstAlign, Value *Src, MaybeAlign SrcAlign,
     Value *Size, bool IsVolatile, MDNode *TBAATag, MDNode *TBAAStructTag,
-    MDNode *ScopeTag, MDNode *NoAliasTag) {
-  Dst = getCastedInt8PtrValue(Dst);
-  Src = getCastedInt8PtrValue(Src);
+    MDNode *ScopeTag, MDNode *NoAliasTag, bool byteLayout) {
+  if (byteLayout)
+  {
+    Dst = getCastedInt8PtrValue(Dst);
+    Src = getCastedInt8PtrValue(Src);
+  }
 
   Value *Ops[] = {Dst, Src, Size, getInt1(IsVolatile)};
   Type *Tys[] = {Dst->getType(), Src->getType(), Size->getType()};
@@ -312,9 +319,12 @@ CallInst *IRBuilderBase::CreateMemMove(Value *Dst, MaybeAlign DstAlign,
                                        Value *Src, MaybeAlign SrcAlign,
                                        Value *Size, bool isVolatile,
                                        MDNode *TBAATag, MDNode *ScopeTag,
-                                       MDNode *NoAliasTag) {
-  Dst = getCastedInt8PtrValue(Dst);
-  Src = getCastedInt8PtrValue(Src);
+                                       MDNode *NoAliasTag, bool byteLayout) {
+  if (byteLayout)
+  {
+    Dst = getCastedInt8PtrValue(Dst);
+    Src = getCastedInt8PtrValue(Src);
+  }
 
   Value *Ops[] = {Dst, Src, Size, getInt1(isVolatile)};
   Type *Tys[] = { Dst->getType(), Src->getType(), Size->getType() };
