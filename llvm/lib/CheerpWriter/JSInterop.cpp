7d6fbde038262daea604c90db0bf8070f681e373
Ennio Barbaro <enniobarbaro@gmail.com>
2014-06-12 17:31:41 +0200

    CheerpBackend: Rewritten compileClassesExportedToJs

--- llvm/lib/CheerpWriter/JSInterop.cpp
+++ llvm/lib/CheerpWriter/JSInterop.cpp
@@ -14,99 +14,125 @@
 using namespace llvm;
 using namespace cheerp;
 
-const char* getMethodFromMangledName(StringRef className, const char* name)
-{
-	if(strncmp(name,"_ZN",3)!=0)
-		return NULL;
-	name+=3;
-	char* localClassName;
-	int classNameLen = strtol(name, &localClassName, 10);
-	if(strncmp(localClassName,className.data(),className.size())!=0)
-		return NULL;
-	name+=className.size();
-	return localClassName+classNameLen;
-}
-
 void CheerpWriter::compileClassesExportedToJs()
 {
-	Module::const_named_metadata_iterator it=module.named_metadata_begin();
-	Module::const_named_metadata_iterator itE=module.named_metadata_end();
-	//Look for metadata which ends in _methods. They are the have the list
+	//Look for metadata which ends in _methods. They are lists
 	//of exported methods for JS layout classes
-	for(;it!=itE;++it)
+	for( Module::const_named_metadata_iterator it = module.named_metadata_begin(),
+		itE = module.named_metadata_end(); it!=itE; ++it)
 	{
-		const NamedMDNode* namedNode=&(*it);
+		const NamedMDNode* namedNode = it;
 		StringRef name = namedNode->getName();
-		if(!name.endswith("_methods"))
+
+		if (!name.endswith("_methods") || !name.startswith("class.") )
 			continue;
-		//Extract the class name
-		const char* className = name.data();
-		if(strncmp(className,"class._Z",8)!=0)
-			continue;
-		className+=8;
-		char* endOfClassLen;
-		int classNameLen = strtol(className, &endOfClassLen, 10);
-		int classPartLen = classNameLen+(endOfClassLen-className);
-		//The full name of the class including the length starts at className
-		//and is classPartLen bytes long
-		StringRef jsClassName(endOfClassLen,classNameLen);
-		//Retrieve the corresponding StructType
-		StructType* t = module.getTypeByName(name.substr(0, 8+classPartLen));
-		//First compile the constructor
-		for(uint32_t i=0;i<namedNode->getNumOperands();i++)
+
+		StringRef mangledName = name.drop_front(6).drop_back(8);
+
+		demangler_iterator demangler( mangledName );
+
+		StringRef jsClassName = *demangler++;
+
+		if ( demangler != demangler_iterator() )
 		{
-			MDNode* node=namedNode->getOperand(i);
-			Function* f=cast<Function>(cast<ConstantAsMetadata>(node->getOperand(0))->getValue());
-			//Extract the method name
-			const char* methodName = getMethodFromMangledName(jsClassName, f->getName().data());
-			if(strncmp(methodName,"C1",2)!=0)
-				continue;
-			stream << "function " << jsClassName << '(';
-			for(uint32_t i=0;i<f->arg_size()-1;i++)
-			{
-				if(i!=0)
-					stream << ",";
-				stream << 'a' << i;
-			}
-			stream << "){\n";
-			compileType(t, THIS_OBJ);
-			//We need to manually add the self pointer
-			stream << ";\nthis.s=this;\n";
-			compileOperand(f);
-			stream << "({d:this,o:'s'}";
-			for(uint32_t i=0;i<f->arg_size()-1;i++)
-				stream << ",a" << i;
-			stream << ");\n}\n";
-			
-			assert( globalDeps.isReachable(f) );
-			break;
+			Twine errorString("Class: ",jsClassName);
+
+			for ( ; demangler != demangler_iterator(); ++ demangler )
+				errorString.concat("::").concat(*demangler);
+
+			errorString.concat(" is not a valid [[jsexport]] class (not in global namespace)\n");
+
+			llvm::report_fatal_error( errorString );
 		}
-		//Then compile other methods and add them to the prototype
-		for(uint32_t i=0;i<namedNode->getNumOperands();i++)
+
+		assert( jsClassName.end() > name.begin() && std::size_t(jsClassName.end() - name.begin()) <= name.size() );
+		StructType * t = module.getTypeByName( StringRef(name.begin(), jsClassName.end() - name.begin() ) );
+		assert(t);
+
+		auto getMethodName = [&](const MDNode * node) -> StringRef
 		{
-			MDNode* node=namedNode->getOperand(i);
-			Function* f=cast<Function>(cast<ConstantAsMetadata>(node->getOperand(0))->getValue());
-			//Extract the method name
-			const char* methodPart = getMethodFromMangledName(jsClassName, f->getName().data());
-			if(strncmp(methodPart,"C1",2)==0)
+			assert( isa<Function>(cast<ConstantAsMetadata>(node->getOperand(0))->getValue()) );
+
+			StringRef mangledName = cast<Function>(cast<ConstantAsMetadata>(node->getOperand(0))->getValue())->getName();
+
+			demangler_iterator dmg(mangledName);
+			if ( *dmg++ != jsClassName )
+				assert( false && "[[jsexport]]: method should be in class" );
+
+			StringRef functionName = *dmg++;
+
+			assert( dmg == demangler_iterator() );
+			return functionName;
+		};
+
+		//TODO many things to check.. For. ex C1/C2/C3, names collisions, template classes!
+		auto isConstructor = [&](const MDNode * node ) -> bool
+		{
+			return getMethodName(node).startswith("C1");
+		};
+
+		auto constructor = std::find_if(namedNode->op_begin(), namedNode->op_end(), isConstructor );
+
+		//First compile the constructor
+		if (constructor == namedNode->op_end() )
+		{
+			llvm::report_fatal_error( Twine("Class: ", jsClassName).concat(" does not define a constructor!") );
+			return;
+		}
+
+		if ( std::find_if( std::next(constructor), namedNode->op_end(), isConstructor ) != namedNode->op_end() )
+		{
+			llvm::report_fatal_error( Twine("More than one constructor defined for class: ", jsClassName) );
+			return;
+		}
+
+		const MDNode* node = *constructor;
+		const Function * f = cast<Function>(cast<ConstantAsMetadata>(node->getOperand(0))->getValue());
+
+		stream << "function " << jsClassName << '(';
+		for(uint32_t i=0;i<f->arg_size()-1;i++)
+		{
+			if(i!=0)
+				stream << ",";
+			stream << 'a' << i;
+		}
+		stream << "){" << NewLine;
+		compileType(t, THIS_OBJ);
+		//We need to manually add the self pointer
+		stream << ";" << NewLine << "this.s=this;" << NewLine;
+		compileOperand(f);
+		stream << "({d:this,o:'s'}";
+		for(uint32_t i=0;i<f->arg_size()-1;i++)
+			stream << ",a" << i;
+		stream << ");" << NewLine << "}" << NewLine;
+
+		assert( globalDeps.isReachable(f) );
+
+		//Then compile other methods and add them to the prototype
+		for ( NamedMDNode::const_op_iterator it = namedNode->op_begin(); it != namedNode->op_end(); ++ it )
+		{
+			if ( isConstructor(*it) )
 				continue;
-			char* methodName;
-			int methodNameLen = strtol(methodPart, &methodName, 10);
-			StringRef jsMethodName(methodName, methodNameLen);
-			stream << jsClassName << ".prototype." << jsMethodName << "=function (";
+
+			StringRef methodName = getMethodName(*it);
+
+			const MDNode * node = *it;
+			const Function * f = cast<Function>(cast<ConstantAsMetadata>(node->getOperand(0))->getValue());
+
+			stream << jsClassName << ".prototype." << methodName << "=function (";
 			for(uint32_t i=0;i<f->arg_size()-1;i++)
 			{
 				if(i!=0)
 					stream << ",";
 				stream << 'a' << i;
 			}
-			stream << "){\nreturn ";
+			stream << "){" << NewLine << "return ";
 			compileOperand(f);
 			stream << "({d:this,o:'s'}";
 			for(uint32_t i=0;i<f->arg_size()-1;i++)
 				stream << ",a" << i;
-			stream << ");\n}\n";
-			
+			stream << ");" << NewLine << '}' << NewLine;
+
 			assert( globalDeps.isReachable(f) );
 		}
 	}
