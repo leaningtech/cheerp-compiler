069a768cf07226dce4064a892fd5755c3462dabe
Ennio Barbaro <enniobarbaro@gmail.com>
2014-07-23 17:03:08 +0200

    CheerpBackend: pointer optimization passes

---
+++ llvm/lib/CheerpUtils/PointerPasses.cpp
@@ -0,0 +1,218 @@
+//===-- PointerPasses.cpp - Cheerp pointer optimization passes --------------===//
+//
+//                     Cheerp: The C++ compiler for the Web
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+// Copyright 2014 Leaning Technologies
+//
+//===----------------------------------------------------------------------===//
+
+#define DEBUG_TYPE "CheerpPointerPasses"
+#include "llvm/ADT/Statistic.h"
+#include "llvm/Cheerp/PointerAnalyzer.h"
+#include "llvm/Cheerp/PointerPasses.h"
+#include "llvm/IR/CallSite.h"
+#include "llvm/IR/Constants.h"
+#include "llvm/IR/Instructions.h"
+#include "llvm/Transforms/Utils/BasicBlockUtils.h"
+
+STATISTIC(NumIndirectFun, "Number of indirect functions processed");
+STATISTIC(NumAllocasTransformedToArrays, "Number of allocas of values transformed to allocas of arrays");
+
+namespace llvm {
+
+bool AllocaArrays::replaceAlloca(AllocaInst* ai)
+{
+	const ConstantInt * ci = dyn_cast<ConstantInt>(ai->getArraySize());
+	
+	if (!ci)
+	{
+		// Runtime alloca size, can not do nothing here 
+		// TODO maybe we can treat this like a malloc?
+		return false;
+	}
+
+	llvm::Type * at = llvm::ArrayType::get( ai->getAllocatedType(), ci->getZExtValue() );
+	AllocaInst * newAi = new AllocaInst( at );
+	newAi->insertAfter( ai );
+	ai->removeFromParent();
+	newAi->takeName(ai);
+
+	GetElementPtrInst * gepZero = nullptr;
+	
+	for ( User::use_iterator it = ai->use_begin(); it != ai->use_end(); )
+	{
+		Use & u = *it++;
+		
+		if ( BitCastInst * bi = dyn_cast<BitCastInst>(u) )
+		{
+			CastInst * newBi = BitCastInst::Create( bi->getOpcode(), newAi, bi->getDestTy() );
+			ReplaceInstWithInst(bi, newBi);
+			
+			newBi->takeName( bi );
+		}
+		else if ( GetElementPtrInst * gep = dyn_cast<GetElementPtrInst>(u) )
+		{
+			SmallVector< Value *, 8 > vals;
+			vals.push_back( ConstantInt::getNullValue( llvm::Type::getInt32Ty( gep->getContext() ) ) );
+			
+			std::copy(gep->idx_begin(), gep->idx_end(), std::back_inserter(vals) );
+			
+			GetElementPtrInst * newGep = GetElementPtrInst::Create(newAi, vals);
+			ReplaceInstWithInst(gep, newGep);
+			newGep->takeName( gep );
+		}
+		else
+		{
+			if (! gepZero )
+			{
+				SmallVector< Value *, 8 > vals ( 2, ConstantInt::getNullValue( llvm::Type::getInt32Ty( u->getContext() ) ) );
+
+				gepZero = GetElementPtrInst::Create(newAi, vals, "");
+				gepZero->insertAfter(newAi);
+			}
+			
+			assert ( isa<Instruction>(u) );
+			
+			u.set( gepZero );
+		}
+	}
+	
+	assert( ai->use_empty() );
+	delete ai;
+
+	return true;
+}
+
+bool AllocaArrays::runOnFunction(Function& F)
+{
+	bool Changed = false;
+	
+	for ( BasicBlock & BB : F )
+	{
+		for ( BasicBlock::iterator it = BB.begin(); it != BB.end(); )
+		{
+			AllocaInst * ai = dyn_cast<AllocaInst>(it++);
+			if (! ai ) continue;
+			
+			cheerp::PointerAnalyzer PA;
+			
+			if (PA.getPointerKind(ai) == cheerp::COMPLETE_OBJECT )
+			{
+				// No need to optimize if it is already a CO
+				continue;
+			}
+			
+			NumAllocasTransformedToArrays++;
+			Changed |= replaceAlloca( ai );
+		}
+	}
+	
+	return Changed;
+}
+
+const char* AllocaArrays::getPassName() const
+{
+	return "AllocaArrays";
+}
+
+char AllocaArrays::ID = 0;
+
+FunctionPass *createAllocaArraysPass() { return new AllocaArrays(); }
+
+
+const char* IndirectCallOptimizer::getPassName() const
+{
+	return "IndirectCallOptimizer";
+}
+
+char IndirectCallOptimizer::ID = 0;
+
+bool IndirectCallOptimizer::runOnModule(Module & m)
+{
+	bool Changed = false;
+
+	for (Module::iterator it = m.begin();it != m.end(); ++it)
+	{
+		cheerp::PointerAnalyzer PA;
+
+		if ( it->hasAddressTaken() &&
+		     !it->empty() &&
+		     // Check that at least one argument is a regular pointer
+		     std::any_of(it->arg_begin(),
+				 it->arg_end(),
+				 [&](const Argument & arg)
+				 {
+				 	return arg.getType()->isPointerTy() && (PA.getPointerKind(&arg) == cheerp::REGULAR);
+				 }) &&
+		     // Check that this function is called *directly* at least one time.
+		     std::any_of(it->use_begin(),
+				 it->use_end(),
+				 [](const Use & u)
+				 {
+					 return ImmutableCallSite(u.get());
+				 }) )
+		{
+			Function * oldFun = it;
+			Function * newFun = Function::Create( oldFun->getFunctionType(),
+							      oldFun->getLinkage(),
+							      Twine("__duettoindirect", oldFun->getName() ) );
+			
+			it = m.getFunctionList().insertAfter( it, newFun);
+			
+			oldFun->replaceAllUsesWith(newFun);
+			assert( oldFun->use_empty() );
+
+			SmallVector< Value *, 8 > newFunArgs;
+			newFunArgs.reserve ( newFun->arg_size() );
+			for ( Function::arg_iterator arg = newFun->arg_begin(); arg != newFun->arg_end(); ++ arg)
+				newFunArgs.push_back(arg);
+			
+			// Fill the new function
+			BasicBlock * newBody = BasicBlock::Create( newFun->getContext(), 
+								   "entry",
+								   newFun );
+
+			CallInst * fwdCall = CallInst::Create( oldFun,
+							      newFunArgs,
+							      "",
+							      newBody);
+
+			if ( fwdCall->getType()->isVoidTy() )
+				ReturnInst::Create( newFun->getContext(), newBody );
+			else
+				ReturnInst::Create( newFun->getContext(), fwdCall, newBody );
+			
+			// Restore direct calls uses
+			for ( Function::use_iterator ui = newFun->use_begin(); ui != newFun->use_end(); )
+			{
+				Use & u = *ui++;
+				User * U = u.getUser();
+				
+				ImmutableCallSite cs(U);
+				if ( (cs.isCall() || cs.isInvoke()) && cs.isCallee(&u) )
+				{
+					U->setOperand( u.getOperandNo(), oldFun );
+				}
+			}
+			
+			assert ( !oldFun->hasAddressTaken() );
+			
+			NumIndirectFun++;
+			Changed = true;
+		}
+	}
+	
+	assert( m.alias_empty() );
+	
+	return Changed;
+}
+
+ModulePass* createIndirectCallOptimizerPass()
+{
+	return new IndirectCallOptimizer();
+}
+
+}
