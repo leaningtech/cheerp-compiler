be9b987be4e616ae061c639bdbb538d81a44b714
Ennio Barbaro <enniobarbaro@gmail.com>
2014-07-23 17:03:08 +0200

    CheerpBackend: pointer optimization passes

---
+++ llvm/include/llvm/Cheerp/PointerPasses.h
@@ -0,0 +1,81 @@
+//===-- Cheerp/PointerPasses.h - Cheerp utility code ---------------------===//
+//
+//                     Cheerp: The C++ compiler for the Web
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+// Copyright 2014 Leaning Technologies
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef _CHEERP_POINTER_PASSES_H
+#define _CHEERP_POINTER_PASSES_H
+
+#include "llvm/Pass.h"
+#include "llvm/IR/Instructions.h"
+#include "llvm/IR/Module.h"
+
+namespace llvm
+{
+
+/**
+ * Collection of passes whose sole purpose is to help
+ * the pointer analyzer generate better code
+ */
+
+// Replace an alloca of a single value with an alloca of an array of size 1 if the 
+// generated pointer would be CO instead of regular
+class AllocaArrays: public FunctionPass
+{
+	bool replaceAlloca( AllocaInst * ai );
+public:
+	static char ID;
+	explicit AllocaArrays() : FunctionPass(ID) { }
+	bool runOnFunction(Function &F);
+	const char *getPassName() const;
+};
+
+//===----------------------------------------------------------------------===//
+//
+// AllocaArrays
+//
+FunctionPass *createAllocaArraysPass();
+
+
+/**
+ * Construct a wrapper function for the function which are called indirectly.
+ * This is used to allow the PA to pass the function parameters as CO when the function is called
+ * directly, while the wrapper function is used only in indirect calls and performs the conversion from REGULAR to CO and
+ * then forwards to the actual function.
+ * 
+ * For each function that:
+ *  1) Can be called indirectly
+ *  2) Takes non-REGULAR pointer arguments
+ * 
+ * Replace every instruction which takes the address of the function
+ * with a new function, called __duettoindirect##functionname, which calls the original
+ * one. This enables pointer kind optimizations for direct calls.
+ */
+  
+class IndirectCallOptimizer: public ModulePass
+{
+public:
+	static char ID;
+	explicit IndirectCallOptimizer() : ModulePass(ID) { }
+	bool runOnModule(Module &);
+	const char *getPassName() const;
+};
+
+
+//===----------------------------------------------------------------------===//
+//
+// IndirectCallOptimizer 
+
+//
+ModulePass *createIndirectCallOptimizerPass();
+
+
+}
+
+#endif
