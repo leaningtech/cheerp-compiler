e7314156cd01ac2fe3936ccff28f1defdb1e7a14
Ennio Barbaro <enniobarbaro@gmail.com>
2014-07-23 17:36:20 +0200

    CheerpBackend: PointerAnalyzer is now a pass

--- llvm/include/llvm/Cheerp/PointerAnalyzer.h
+++ llvm/include/llvm/Cheerp/PointerAnalyzer.h
@@ -12,6 +12,7 @@
 #ifndef _CHEERP_POINTER_ANALYZER_H
 #define _CHEERP_POINTER_ANALYZER_H
 
+#include "llvm/Pass.h"
 #include "llvm/ADT/DenseMap.h"
 #include "llvm/IR/Value.h"
 #include "llvm/IR/Function.h"
@@ -24,21 +25,36 @@ enum POINTER_KIND {
 	REGULAR
 };
 
-class PointerAnalyzer {
+class PointerAnalyzer : public llvm::ModulePass
+{
 public:
 	PointerAnalyzer() : 
+		ModulePass(ID)
 #ifndef NDEBUG
-		timerGroup("Pointer Analyzer"),
+		,timerGroup("Pointer Analyzer"),
 		gpkTimer("getPointerKind",timerGroup),
 		gpkfrTimer("getPointerKindForReturn",timerGroup)
 #endif //NDEBUG
 	{}
 
 	void prefetch( const llvm::Module & ) const;
+	static char ID;
+
+	bool runOnModule( llvm::Module & ) override;
+
+	const char *getPassName() const override;
 
 	POINTER_KIND getPointerKind(const llvm::Value* v) const;
 	POINTER_KIND getPointerKindForReturn(const llvm::Function* F) const;
 	POINTER_KIND getPointerKindForType( llvm::Type * tp) const;
+
+	/**
+	 * Functions to manually invalidate the cache
+	 */
+
+	// Notify that a value has been invalidated
+	void invalidate( const llvm::Value * );
+
 #ifndef NDEBUG
 	// Dump a pointer value info
 	void dumpPointer(const llvm::Value * v, bool dumpOwnerFuncion = true) const;
@@ -61,6 +77,11 @@ void dumpAllPointers(const llvm::Function &, const PointerAnalyzer & );
 void writePointerDumpHeader();
 
 #endif //NDEBUG
+
+inline llvm::Pass * createPointerAnalyzerPass()
+{
+	return new PointerAnalyzer;
+}
 
 }
 
