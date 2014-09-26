641449c53fcd9d15c82a419e045366aedd05735e
Alessandro Pignotti <a.pignotti@sssup.it>
2014-09-26 17:01:57 +0200

    Support byte layout structures inside unions on Cheerp

--- clang/include/clang/AST/Decl.h
+++ clang/include/clang/AST/Decl.h
@@ -4203,6 +4203,8 @@ public:
   /// leaks.
   bool isOrContainsUnion() const;
 
+  bool isByteLayout() const;
+
   // Iterator access to field members. The field iterator only visits
   // the non-static data members of this class, ignoring any static
   // data members, functions, constructors, destructors, etc.
