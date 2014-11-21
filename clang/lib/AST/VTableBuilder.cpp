f45cc46ed2dc98dfe0ddef69542197863a648943
Alessandro Pignotti <a.pignotti@sssup.it>
2014-11-21 16:03:42 +0100

    When possible use 0 as the offset for vtable pointers on Cheerp

--- clang/lib/AST/VTableBuilder.cpp
+++ clang/lib/AST/VTableBuilder.cpp
@@ -1690,6 +1690,10 @@ void ItaniumVTableBuilder::LayoutPrimaryAndSecondaryVTables(
 
   uint64_t AddressPoint = Components.size();
 
+  // On Cheerp we want to start vtable pointer to start from 0 when possible
+  if(!Context.getTargetInfo().isByteAddressable())
+    AddressPoint -= 2;
+
   // Now go through all virtual member functions and add them.
   PrimaryBasesSetVectorTy PrimaryBases;
   AddMethods(Base, OffsetInLayoutClass,
