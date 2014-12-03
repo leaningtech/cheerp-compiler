7fcdb02d05397d3a9f5662c37b45776de5dc532f
Alessandro Pignotti <a.pignotti@sssup.it>
2014-12-03 17:08:39 +0100

    Implement CheerpWritePass::getPassName

--- llvm/lib/Target/CheerpBackend/CheerpBackend.cpp
+++ llvm/lib/Target/CheerpBackend/CheerpBackend.cpp
@@ -51,6 +51,9 @@ namespace {
     explicit CheerpWritePass(raw_ostream &o) :
       ModulePass(ID), Out(o) { }
     bool runOnModule(Module &M);
+    const char *getPassName() const {
+	return "CheerpWritePass";
+    }
   };
 } // end anonymous namespace.
 
