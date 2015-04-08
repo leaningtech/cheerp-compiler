8f495ca6844a1c6a6c8a642ef00f88a89fc92c8a
Alessandro Pignotti <a.pignotti@sssup.it>
2015-04-08 10:05:14 +0200

    Fix the type used when building constant complex values

--- clang/lib/CodeGen/CGExprConstant.cpp
+++ clang/lib/CodeGen/CGExprConstant.cpp
@@ -2177,8 +2177,7 @@ llvm::Constant *ConstantEmitter::tryEmitPrivate(const APValue &Value,
                                         Value.getComplexIntImag());
 
     // FIXME: the target may want to specify that this is packed.
-    llvm::StructType *STy =
-        llvm::StructType::get(Complex[0]->getType(), Complex[1]->getType());
+    llvm::StructType *STy = cast<llvm::StructType>(CGM.getTypes().ConvertTypeForMem(DestType));
     return llvm::ConstantStruct::get(STy, Complex);
   }
   case APValue::Float: {
@@ -2200,8 +2199,7 @@ llvm::Constant *ConstantEmitter::tryEmitPrivate(const APValue &Value,
                                        Value.getComplexFloatImag());
 
     // FIXME: the target may want to specify that this is packed.
-    llvm::StructType *STy =
-        llvm::StructType::get(Complex[0]->getType(), Complex[1]->getType());
+    llvm::StructType *STy = cast<llvm::StructType>(CGM.getTypes().ConvertTypeForMem(DestType));
     return llvm::ConstantStruct::get(STy, Complex);
   }
   case APValue::Vector: {
