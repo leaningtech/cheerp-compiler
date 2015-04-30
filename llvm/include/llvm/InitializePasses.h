7dcc926df305f37393665a341ecf5e29c7617e46
Alessandro Pignotti <a.pignotti@sssup.it>
2015-04-30 17:19:18 +0200

    Introduce TypeOptimizer pass

--- llvm/include/llvm/InitializePasses.h
+++ llvm/include/llvm/InitializePasses.h
@@ -452,6 +452,7 @@ void initializeRegisterizePass(PassRegistry&);
 void initializeStructMemFuncLoweringPass(PassRegistry&);
 void initializeAllocaArraysPass(PassRegistry&);
 void initializeReplaceNopCastsPass(PassRegistry&);
+void initializeTypeOptimizerPass(PassRegistry&);
 
 } // end namespace llvm
 
