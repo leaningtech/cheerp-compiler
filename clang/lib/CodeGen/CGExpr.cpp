3ac51c09f1151d449be4180b47591b0cbffda7f5
Sander Mathijs van Veen <smvv@kompiler.org>
2015-06-02 16:23:44 +0200

    Add support for int64_t assignment and cast operators

--- clang/lib/CodeGen/CGExpr.cpp
+++ clang/lib/CodeGen/CGExpr.cpp
@@ -1840,6 +1840,21 @@ void CodeGenFunction::EmitStoreOfScalar(llvm::Value *Value, Address Addr,
     if (GV->isThreadLocal())
       Addr = Addr.withPointer(Builder.CreateThreadLocalAddress(GV));
 
+  if (cast<BuiltinType>(Ty)->isHighInt()) {
+    llvm::Value *highLoc = Builder.CreateConstGEP2_32(Value, 0, 0);
+    llvm::Value *lowLoc = Builder.CreateConstGEP2_32(Value, 0, 1);
+
+    llvm::Value *highPart = Builder.CreateLoad(highLoc);
+    llvm::Value *lowPart = Builder.CreateLoad(lowLoc);
+
+    highLoc = Builder.CreateConstGEP2_32(Addr, 0, 0);
+    lowLoc = Builder.CreateConstGEP2_32(Addr, 0, 1);
+
+    Builder.CreateStore(highPart, highLoc, Volatile);
+    Builder.CreateStore(lowPart, lowLoc, Volatile);
+    return;
+  }
+
   llvm::Type *SrcTy = Value->getType();
   if (const auto *ClangVecTy = Ty->getAs<VectorType>()) {
     auto *VecTy = dyn_cast<llvm::FixedVectorType>(SrcTy);
