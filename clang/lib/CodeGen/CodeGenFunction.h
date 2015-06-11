7414267fe9eaeebdf9ba1bdbc37283b237ef581a
Alessandro Pignotti <a.pignotti@sssup.it>
2015-06-11 17:57:29 +0200

    Clean up generation of upcasts

--- clang/lib/CodeGen/CodeGenFunction.h
+++ clang/lib/CodeGen/CodeGenFunction.h
@@ -2796,6 +2796,10 @@ public:
                                 CastExpr::path_const_iterator PathEnd,
                                 bool NullCheckValue, SourceLocation Loc);
 
+  llvm::Value * GenerateUpcast(llvm::Value* Value,
+                               const CXXRecordDecl *Derived,
+                               CastExpr::path_const_iterator PathBegin,
+                               CastExpr::path_const_iterator PathEnd);
   llvm::Value * GenerateDowncast(llvm::Value* Value,
                                  const CXXRecordDecl *Derived,
                                  unsigned BaseIdOffset);
