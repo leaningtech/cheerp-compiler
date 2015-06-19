7110822e5a0201995ff90978244983d1a4bc7738
Alessandro Pignotti <a.pignotti@sssup.it>
2015-06-19 12:14:28 +0200

    Support returning 64-bit values

--- clang/lib/CodeGen/TargetInfo.cpp
+++ clang/lib/CodeGen/TargetInfo.cpp
@@ -79,7 +79,8 @@ static void AssignToArrayRange(CodeGen::CGBuilderTy &Builder,
 
 static bool isAggregateTypeForABI(QualType T) {
   return !CodeGenFunction::hasScalarEvaluationKind(T) ||
-         T->isMemberFunctionPointerType();
+         T->isMemberFunctionPointerType() ||
+         CodeGenFunction::IsHighInt(T);
 }
 
 ABIArgInfo ABIInfo::getNaturalAlignIndirect(QualType Ty, bool ByVal,
