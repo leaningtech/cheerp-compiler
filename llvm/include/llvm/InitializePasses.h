92e205e199239ab0c5e42189413a0602d69cb3b2
Alessandro Pignotti <a.pignotti@sssup.it>
2015-08-10 18:14:31 +0200

    Add a pass that moves allocas closer to their use

--- llvm/include/llvm/InitializePasses.h
+++ llvm/include/llvm/InitializePasses.h
@@ -453,6 +453,7 @@ void initializeStructMemFuncLoweringPass(PassRegistry&);
 void initializeAllocaArraysPass(PassRegistry&);
 void initializeReplaceNopCastsPass(PassRegistry&);
 void initializeTypeOptimizerPass(PassRegistry&);
+void initializeDelayAllocasPass(PassRegistry&);
 
 } // end namespace llvm
 
