06527cffb5ecae7064c3f829d5d88851674ad1b1
Sander Mathijs van Veen <smvv@kompiler.org>
2015-08-16 18:51:57 +0200

    Show typenames in error for mismatched types in mem{cpy,move}

--- clang/lib/CodeGen/CGBuiltin.cpp
+++ clang/lib/CodeGen/CGBuiltin.cpp
@@ -3461,8 +3461,12 @@ RValue CodeGenFunction::EmitBuiltinExpr(const GlobalDecl GD, unsigned BuiltinID,
         SrcE = SrcCast->getSubExpr();
         QualType DestType = DestE->getType()->getPointeeType().getCanonicalType().getUnqualifiedType();
         QualType SrcType = SrcE->getType()->getPointeeType().getCanonicalType().getUnqualifiedType();
-        if (DestType != SrcType)
-          CGM.getDiags().Report(SrcE->getLocStart(), diag::err_cheerp_memintrinsic_same_type);
+        if (DestType != SrcType &&
+            (!SrcType->isIntegerType() || !DestType->isIntegerType() ||
+             cast<llvm::IntegerType>(ConvertType(SrcType))->getBitWidth()
+             != cast<llvm::IntegerType>(ConvertType(DestType))->getBitWidth()))
+          CGM.getDiags().Report(SrcE->getLocStart(), diag::err_cheerp_memintrinsic_same_type)
+            << SrcE->getType() << DestE->getType();
       }
     }
     Address Dest = EmitPointerWithAlignment(DestE);
@@ -3561,8 +3565,12 @@ RValue CodeGenFunction::EmitBuiltinExpr(const GlobalDecl GD, unsigned BuiltinID,
         SrcE = SrcCast->getSubExpr();
         QualType DestType = DestE->getType()->getPointeeType().getCanonicalType().getUnqualifiedType();
         QualType SrcType = SrcE->getType()->getPointeeType().getCanonicalType().getUnqualifiedType();
-        if (DestType != SrcType)
-          CGM.getDiags().Report(SrcE->getLocStart(), diag::err_cheerp_memintrinsic_same_type);
+        if (DestType != SrcType &&
+            (!SrcType->isIntegerType() || !DestType->isIntegerType() ||
+             cast<llvm::IntegerType>(ConvertType(SrcType))->getBitWidth()
+             != cast<llvm::IntegerType>(ConvertType(DestType))->getBitWidth()))
+          CGM.getDiags().Report(SrcE->getLocStart(), diag::err_cheerp_memintrinsic_same_type)
+            << SrcE->getType() << DestE->getType();
       }
     }
     Address Dest = EmitPointerWithAlignment(DestE);
