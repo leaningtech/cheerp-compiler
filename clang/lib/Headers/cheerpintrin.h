2dfe21a1dc946223c57925d244cd858e17af50ab
Alessandro Pignotti <a.pignotti@sssup.it>
2016-02-12 09:28:54 +0100

    Make cheerp_pointer_base and cheerp_pointer_offset fully typed

--- clang/lib/Headers/cheerpintrin.h
+++ clang/lib/Headers/cheerpintrin.h
@@ -24,9 +24,10 @@
 #ifndef __CHEERPINTRIN_H
 #define __CHEERPINTRIN_H
 
-template<class R>
-R* __builtin_cheerp_pointer_base(const void* ptr);
-size_t __builtin_cheerp_pointer_offset(const void* ptr);
+template<class R, class P>
+R* __builtin_cheerp_pointer_base(const P* ptr);
+template<class P>
+size_t __builtin_cheerp_pointer_offset(const P* ptr);
 /* This method returns a closure. When it is invoked it will execute func with obj as the first argument
    and its own argument as the second one
 */
