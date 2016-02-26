8b6508c431180bf5f4e35f3a13f0b3d01a82001a
Alessandro Pignotti <a.pignotti@sssup.it>
2016-02-26 10:48:47 +0100

    Improve mangling of anonymous structs in C

--- clang/lib/AST/ItaniumMangle.cpp
+++ clang/lib/AST/ItaniumMangle.cpp
@@ -1574,6 +1574,12 @@ void CXXNameMangler::mangleUnqualifiedName(GlobalDecl GD,
       Out << "Ut";
       if (UnnamedMangle > 1)
         Out << UnnamedMangle - 2;
+      if (!getASTContext().getLangOpts().CPlusPlus && UnnamedMangle==1) {
+        // C++ specifies mangling, but C does not
+        const SourceLocation& loc = TD->getOuterLocStart();
+        FullSourceLoc fullLoc(loc, getASTContext().getSourceManager());
+        Out << fullLoc.getExpansionLineNumber() << '_' << fullLoc.getExpansionColumnNumber();
+      }
       Out << '_';
       writeAbiTags(TD, AdditionalAbiTags);
       break;
