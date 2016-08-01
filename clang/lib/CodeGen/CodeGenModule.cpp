ea9cd038a4c58d6790aefb44df147f7290aa348c
Alessandro Pignotti <a.pignotti@sssup.it>
2016-08-01 10:05:16 +0200

    Add support for jsexporting free functions

--- clang/lib/CodeGen/CodeGenModule.cpp
+++ clang/lib/CodeGen/CodeGenModule.cpp
@@ -2120,6 +2120,15 @@ void CodeGenModule::SetLLVMFunctionAttributesForDefinition(const Decl *D,
       F->addTypeMetadata(0, Id);
     }
   }
+
+  if(D->hasAttr<JsExportAttr>())
+  {
+      llvm::NamedMDNode* namedNode = getModule().getOrInsertNamedMetadata("jsexported_methods");
+      llvm::SmallVector<llvm::Metadata*,1> values;
+      values.push_back(llvm::ConstantAsMetadata::get(F));
+      llvm::MDNode* node = llvm::MDNode::get(getLLVMContext(),values);
+      namedNode->addOperand(node);
+  }
 }
 
 void CodeGenModule::setLLVMFunctionFEnvAttributes(const FunctionDecl *D,
