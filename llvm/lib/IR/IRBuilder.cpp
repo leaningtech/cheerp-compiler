c14585b84c9f96fef8a0451ab2dee95ed1bad38a
Yuri Iozzelli <y.iozzelli@gmail.com>
2016-11-29 10:06:53 +0000

    set asmjs section for global strings used in asmjs functions

--- llvm/lib/IR/IRBuilder.cpp
+++ llvm/lib/IR/IRBuilder.cpp
@@ -51,6 +51,9 @@ GlobalVariable *IRBuilderBase::CreateGlobalString(StringRef Str,
                                 AddressSpace);
   GV->setUnnamedAddr(GlobalValue::UnnamedAddr::Global);
   GV->setAlignment(Align(1));
+  // for cheerp asmjs mode
+  if (BB->getParent()->getSection() == StringRef("asmjs"))
+	  GV->setSection("asmjs");
   return GV;
 }
 
