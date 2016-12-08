b14dc2e9140825a408dd137c56cc8bb3946b8bb6
Yuri Iozzelli <y.iozzelli@gmail.com>
2016-12-08 11:38:51 +0000

    Handle downcasts like the byte-addressable case for asmjs code

--- clang/lib/CodeGen/CGClass.cpp
+++ clang/lib/CodeGen/CGClass.cpp
@@ -556,7 +556,8 @@ CodeGenFunction::GetAddressOfDerivedClass(Address BaseAddr,
   llvm::Value *NonVirtualOffset =
     CGM.GetNonVirtualBaseClassOffset(Derived, PathBegin, PathEnd);
 
-  if (!NonVirtualOffset && getTarget().isByteAddressable()) {
+  bool asmjs = CurFn->getSection() == StringRef("asmjs");
+  if (!NonVirtualOffset && (getTarget().isByteAddressable() || asmjs)) {
     // No offset, we can just cast back.
     return Builder.CreateElementBitCast(BaseAddr, DerivedValueTy);
   }
@@ -574,8 +575,7 @@ CodeGenFunction::GetAddressOfDerivedClass(Address BaseAddr,
     Builder.CreateCondBr(IsNull, CastNull, CastNotNull);
     EmitBlock(CastNotNull);
   }
-
-  if (!getTarget().isByteAddressable())
+  if (!asmjs && !getTarget().isByteAddressable())
   {
     llvm::SmallVector<const CXXBaseSpecifier*, 4> path;
     for (CastExpr::path_const_iterator I = PathBegin; I != PathEnd; ++I)
