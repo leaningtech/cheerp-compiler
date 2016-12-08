9629c38c5b69f6164e05bb28ba0d2c8989341c9f
Yuri Iozzelli <y.iozzelli@gmail.com>
2016-12-08 16:23:20 +0000

    Skip cheerp sema checks on builtins in asmjs functions

--- clang/lib/Sema/SemaChecking.cpp
+++ clang/lib/Sema/SemaChecking.cpp
@@ -2692,8 +2692,9 @@ Sema::CheckBuiltinFunctionCall(FunctionDecl *FDecl, unsigned BuiltinID,
   }
   }
 
-  // Some builtins needs Special handling on Cheerp
-  if (Context.getTargetInfo().getTriple().getArch()==llvm::Triple::cheerp) {
+  bool asmjs = FDecl->hasAttr<AsmJSAttr>();
+  // Some builtins need special handling on generic Cheerp
+  if (!asmjs && Context.getTargetInfo().getTriple().getArch()==llvm::Triple::cheerp) {
     if (CheckCheerpBuiltinFunctionCall(BuiltinID, TheCall))
       return ExprError();
   }
