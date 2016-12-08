3aa2c4835b390c0abec1598bdb1710efaa5c8f29
Yuri Iozzelli <y.iozzelli@gmail.com>
2016-12-08 15:12:57 +0000

    Don't emit special builtins for memory allocation/deallocation/copy in asmjs functions

--- clang/lib/CodeGen/CGBuiltin.cpp
+++ clang/lib/CodeGen/CGBuiltin.cpp
@@ -1647,6 +1647,8 @@ RValue CodeGenFunction::emitRotate(const CallExpr *E, bool IsRotateRight) {
 RValue CodeGenFunction::EmitBuiltinExpr(const GlobalDecl GD, unsigned BuiltinID,
                                         const CallExpr *E,
                                         ReturnValueSlot ReturnValue) {
+  // CHEERP: we don't need special handling in the asmjs section
+  bool asmjs = CurFn->getSection() == StringRef("asmjs");
   const FunctionDecl *FD = GD.getDecl()->getAsFunction();
   // See if we can constant fold this builtin.  If so, don't emit it at all.
   Expr::EvalResult Result;
@@ -2629,7 +2631,7 @@ RValue CodeGenFunction::EmitBuiltinExpr(const GlobalDecl GD, unsigned BuiltinID,
   case Builtin::BI__builtin_mempcpy: {
     const Expr *DestE = E->getArg(0);
     const Expr *SrcE = E->getArg(1);
-    if (!getTarget().isByteAddressable())
+    if (!asmjs && !getTarget().isByteAddressable())
     {
       // There must be a cast from a valid type to void*
       const CastExpr *DestCast = dyn_cast<CastExpr>(DestE);
@@ -2732,7 +2734,7 @@ RValue CodeGenFunction::EmitBuiltinExpr(const GlobalDecl GD, unsigned BuiltinID,
   case Builtin::BI__builtin_memmove: {
     const Expr *DestE = E->getArg(0);
     const Expr *SrcE = E->getArg(1);
-    if (!getTarget().isByteAddressable())
+    if (!asmjs && !getTarget().isByteAddressable())
     {
       // There must be a cast from a valid type to void*
       const CastExpr *DestCast = dyn_cast<CastExpr>(DestE);
@@ -2769,7 +2771,7 @@ RValue CodeGenFunction::EmitBuiltinExpr(const GlobalDecl GD, unsigned BuiltinID,
   case Builtin::BImemset:
   case Builtin::BI__builtin_memset: {
     const Expr *DestE = E->getArg(0);
-    if (!getTarget().isByteAddressable())
+    if (!asmjs && !getTarget().isByteAddressable())
     {
       // There must be a cast from a valid type to void*
       const CastExpr *DestCast = dyn_cast<CastExpr>(DestE);
@@ -4434,8 +4436,8 @@ RValue CodeGenFunction::EmitBuiltinExpr(const GlobalDecl GD, unsigned BuiltinID,
 
   case Builtin::BImalloc:
   case Builtin::BIrealloc: {
-    // On cheerp we need special handling for malloc and realloc
-    if (getTarget().getTriple().getArch() == llvm::Triple::cheerp)
+    // On cheerp in generic code, we need special handling for malloc and realloc
+    if (!asmjs && getTarget().getTriple().getArch() == llvm::Triple::cheerp)
       return RValue::get(EmitCheerpBuiltinExpr(BuiltinID, E));
   }
   }
