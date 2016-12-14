d9d31c7de640f2343cde824322cfa987b0dca278
Yuri Iozzelli <y.iozzelli@gmail.com>
2016-12-14 14:31:30 +0000

    Support the asmjs attribute in type declarations. Put type-related globals in the asmjs section if the type has the asmjs attribute

--- clang/lib/Sema/SemaDecl.cpp
+++ clang/lib/Sema/SemaDecl.cpp
@@ -16461,6 +16461,9 @@ CreateNewDecl:
     New->startDefinition();
 
   ProcessDeclAttributeList(S, New, Attrs);
+  // CHEERP: Inject asmjs/genericjs attribute if required
+  MaybeInjectCheerpModeAttr(New);
+
   AddPragmaAttributes(S, New);
 
   // If this has an identifier, add it to the scope stack.
