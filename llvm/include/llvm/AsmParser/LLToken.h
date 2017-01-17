073940b60f2aeb3bc6a54661988d230daa36562a
Yuri Iozzelli <y.iozzelli@gmail.com>
2017-01-17 14:09:32 +0000

    Encode the asmjs information for types in the bitcode

--- llvm/include/llvm/AsmParser/LLToken.h
+++ llvm/include/llvm/AsmParser/LLToken.h
@@ -259,6 +259,7 @@ enum Kind {
   kw_static,
   kw_bytelayout,
   kw_directbase,
+  kw_asmjs,
 
   kw_type,
   kw_opaque,
--- llvm/lib/AsmParser/LLLexer.cpp
+++ llvm/lib/AsmParser/LLLexer.cpp
@@ -718,6 +718,7 @@ lltok::Kind LLLexer::LexIdentifier() {
   KEYWORD(type);
   KEYWORD(opaque);
   KEYWORD(bytelayout);
+  KEYWORD(asmjs);
 
   KEYWORD(comdat);
 
