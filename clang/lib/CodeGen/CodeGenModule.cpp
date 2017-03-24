72ac239fb8a8cafbcc5b27d2d754f44381d67ae4
Alessandro Pignotti <a.pignotti@sssup.it>
2017-03-24 12:06:59 +0100

    Support pointers to virtual member functions

--- clang/lib/CodeGen/CodeGenModule.cpp
+++ clang/lib/CodeGen/CodeGenModule.cpp
@@ -3602,6 +3602,7 @@ void CodeGenModule::EmitGlobalDefinition(GlobalDecl GD, llvm::GlobalValue *GV) {
          ThunkInfo TI;
          TI.Method = Method;
          TI.This.AdjustmentTarget = Method->getParent();
+         TI.isMemberPointerThunk = true;
          getVTables().emitThunk(GlobalDecl(Method), TI, false);
          GV->setLinkage(llvm::GlobalValue::LinkOnceODRLinkage);
       } else
