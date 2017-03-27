e49857068a4b8cc7b110aba40b77b1571c987709
Yuri Iozzelli <y.iozzelli@gmail.com>
2017-03-27 09:48:19 +0000

    Enable memcpyable fields in asm.js

--- clang/lib/CodeGen/CGClass.cpp
+++ clang/lib/CodeGen/CGClass.cpp
@@ -1088,7 +1088,8 @@ namespace {
       Qualifiers Qual = F->getType().getQualifiers();
       if (Qual.hasVolatile() || Qual.hasObjCLifetime())
         return false;
-      if (!CGF.getTarget().isByteAddressable())
+	bool asmjs = ClassDecl->hasAttr<AsmJSAttr>();
+      if (!CGF.getTarget().isByteAddressable() && !asmjs)
         return false;
       return true;
     }
