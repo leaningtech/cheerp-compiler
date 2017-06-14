ec2feead0ae5e89d107196c6b0061ae4dff5eebe
Yuri Iozzelli <y.iozzelli@gmail.com>
2017-06-14 08:55:25 +0000

    Add an alloca listener in the Interpreter, and record allocas in preexecuter

--- llvm/include/llvm/ExecutionEngine/ExecutionEngine.h
+++ llvm/include/llvm/ExecutionEngine/ExecutionEngine.h
@@ -174,6 +174,7 @@ protected:
   std::string ErrMsg;
 
   void (*StoreListener)(void* Addr);
+  void (*AllocaListener)(Type* Ty, uint32_t Size, void* Addr);
 
 public:
   /// lock - This lock protects the ExecutionEngine and MCJIT classes. It must
@@ -527,6 +528,10 @@ public:
   /// InstallStoreListener - Listener to invoke on each store
   void InstallStoreListener(void (*P)(void* Addr)) {
     StoreListener = P;
+  }
+  /// InstallAllocaListener - Listener to invoke on each alloca
+  void InstallAllocaListener(void (*P)(Type* Ty, uint32_t Size, void* Addr)) {
+    AllocaListener = P;
   }
 
 protected:
