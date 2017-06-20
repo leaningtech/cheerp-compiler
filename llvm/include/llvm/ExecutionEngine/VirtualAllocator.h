4e129aa32c799cfa7c8267e313c6ac359df3d381
Yuri Iozzelli <y.iozzelli@gmail.com>
2017-06-20 16:05:03 +0000

    Make a transparent version of the MemoryAllocator and the FunctionAddresses classes, for use in the regular interpreter

--- llvm/include/llvm/ExecutionEngine/VirtualAllocator.h
+++ llvm/include/llvm/ExecutionEngine/VirtualAllocator.h
@@ -9,7 +9,36 @@
 
 namespace llvm {
 
-class VirtualAllocator : public AllocatorBase<VirtualAllocator> {
+class VirtualAllocatorBase : public AllocatorBase<VirtualAllocatorBase> {
+public:
+	virtual void* toReal(void* virt) = 0;
+	virtual void* toVirtual(void* real) = 0;
+
+	LLVM_ATTRIBUTE_RETURNS_NONNULL void *Allocate(size_t size,size_t align) {
+		void* ret =  malloc(size);
+		allocated(ret, size, align);
+		return ret;
+	}
+	// Pull in base class overloads.
+	using AllocatorBase<VirtualAllocatorBase>::Allocate;
+
+	void Deallocate(const void *ptr, size_t size) {
+		deallocated(ptr, size);
+		free(const_cast<void*>(ptr));
+	}
+
+	// Pull in base class overloads.
+	using AllocatorBase<VirtualAllocatorBase>::Deallocate;
+
+	void PrintStats() const {}
+
+	virtual ~VirtualAllocatorBase(){};
+protected:
+	virtual void allocated(void* ptr, size_t size, size_t align) = 0;
+	virtual void deallocated(const void *ptr, size_t size) = 0;
+};
+
+class VirtualAllocator : public VirtualAllocatorBase {
 public:
 	void* toReal(void* virt)
 	{
@@ -46,27 +75,16 @@ public:
 		return reinterpret_cast<void*>(it->second.start+offset);
 	}
 
-	LLVM_ATTRIBUTE_RETURNS_NONNULL void *Allocate(size_t size,size_t align) {
-		void* ret =  malloc(size);
-		uintptr_t reti = reinterpret_cast<uintptr_t>(ret);
+	void allocated(void* ptr, size_t size,size_t align) {
+		uintptr_t reti = reinterpret_cast<uintptr_t>(ptr);
 		uint32_t padding = (align - next_virt % align) % align;
 		next_virt += padding;
 		virt_to_real.emplace(next_virt, Page(reti, size));
 		real_to_virt.emplace(reti, Page(next_virt, size));
 		next_virt += size;
-		return ret;
 	}
-	// Pull in base class overloads.
-	using AllocatorBase<VirtualAllocator>::Allocate;
-
-	void Deallocate(const void *Ptr, size_t Size) {
-		free(const_cast<void*>(Ptr));
+	void deallocated(const void *ptr, size_t size) {
 	}
-
-	// Pull in base class overloads.
-	using AllocatorBase<VirtualAllocator>::Deallocate;
-
-	void PrintStats() const {}
 
 	VirtualAllocator() {
 		virt_to_real.emplace(0,Page(0,8));
@@ -82,6 +100,22 @@ private:
 	uintptr_t next_virt;
 	std::map<uintptr_t, Page> virt_to_real;
 	std::map<uintptr_t, Page> real_to_virt;
+};
+class DirectAllocator : public VirtualAllocatorBase {
+public:
+	void* toReal(void* virt)
+	{
+		return virt;
+	}
+	void* toVirtual(void* real)
+	{
+		return real;
+	}
+
+	void allocated(void* ptr, size_t size,size_t align) {
+	}
+	void deallocated(const void *ptr, size_t size) {
+	}
 };
 }
 #endif
--- llvm/lib/ExecutionEngine/Interpreter/Interpreter.h
+++ llvm/lib/ExecutionEngine/Interpreter/Interpreter.h
@@ -208,7 +208,7 @@ private:  // Helper functions
   void SwitchToNewBasicBlock(BasicBlock *Dest, ExecutionContext &SF);
 
   void *getPointerToFunction(Function *F) override {
-	  return FunctionAddresses.getAddress(F);
+	  return FunctionAddresses->getAddress(F);
   }
 
   void initializeExecutionEngine() { }
