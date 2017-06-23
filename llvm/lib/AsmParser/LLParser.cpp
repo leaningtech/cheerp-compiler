9cf51fafa0a8832e156f9621bd7af53207e2b38f
Yuri Iozzelli <y.iozzelli@gmail.com>
2017-06-23 15:36:38 +0000

    Remove old local variable, now useless

--- llvm/lib/AsmParser/LLParser.cpp
+++ llvm/lib/AsmParser/LLParser.cpp
@@ -546,9 +546,6 @@ bool LLParser::parseNamedType() {
   LocTy NameLoc = Lex.getLoc();
   Lex.Lex();  // eat LocalVar.
 
-  bool IsDirectBase;
-  LocTy IsDirectBaseLoc;
-
   if (parseToken(lltok::equal, "expected '=' after name") ||
       parseToken(lltok::kw_type, "expected 'type' after name"))
     return true;
@@ -563,7 +560,6 @@ bool LLParser::parseNamedType() {
       return error(NameLoc, "non-struct types may not be recursive");
     Entry.first = Result;
     Entry.second = SMLoc();
-    assert(!IsDirectBase && "directbase is not possible on non-struct type");
   }
 
   return false;
