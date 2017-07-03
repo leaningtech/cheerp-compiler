4e1012fa9a8a3e09ce0b062b0b2a81880a5c5ef7
Sander Mathijs van Veen <sander@leaningtech.com>
2017-07-03 12:00:31 +0000

    Define __WASM__ macro when wast or wasm mode is used

--- clang/lib/Basic/Targets/WebAssembly.h
+++ clang/lib/Basic/Targets/WebAssembly.h
@@ -214,6 +214,12 @@ public:
         Builder.defineMacro("__ASMJS__");
     }
 
+    if (Opts.getCheerpMode() == LangOptions::CHEERP_MODE_Wast ||
+       Opts.getCheerpMode() == LangOptions::CHEERP_MODE_Wasm)
+    {
+        Builder.defineMacro("__WASM__");
+    }
+
     if (Opts.CPlusPlus)
       Builder.defineMacro("_GNU_SOURCE");
 
