1b2b627cba69ffa0e282fa7c25aa25f7fd6458c4
Yuri Iozzelli <y.iozzelli@gmail.com>
2017-07-27 14:48:11 +0000

    Correctly manage dynamic memory usage all over PreExecuter

--- llvm/include/llvm/ExecutionEngine/VirtualAddressMap.h
+++ llvm/include/llvm/ExecutionEngine/VirtualAddressMap.h
@@ -14,6 +14,7 @@ public:
 	virtual void* toReal(void* virt) = 0;
 	virtual void* toVirtual(void* real) = 0;
 	virtual void map(void* ptr, size_t size) = 0;
+	virtual void unmap(void* ptr) = 0;
 
 	virtual ~AddressMapBase(){};
 };
@@ -29,34 +30,16 @@ public:
 	void* toReal(void* virt) override
 	{
 		uintptr_t virti = reinterpret_cast<uintptr_t>(virt);
-		auto it = virt_to_real.upper_bound(virti);
-		assert(it != virt_to_real.begin() && "the requested address is out of range");
-		it--;
-		if(it->first + it->second.size <= virti)
-		{
-			llvm::errs()<<"Translating virt addr "<<virti<<":\n";
-			llvm::errs()<<"it->first = "<<it->first<<"\n";
-			llvm::errs()<<"it->second.start = "<<it->second.start<<"\n";
-			llvm::errs()<<"it->second.size = "<<it->second.size<<"\n";
-		}
-		assert(it->first + it->second.size > virti && "the requested address is out of range");
+		auto it = find_start(virt_to_real, virti);
+		assert(it != virt_to_real.end() && "the requested address is out of range");
 		ptrdiff_t offset = virti - it->first;
 		return reinterpret_cast<void*>(it->second.start+offset);
 	}
 	void* toVirtual(void* real) override
 	{
 		uintptr_t reali = reinterpret_cast<uintptr_t>(real);
-		auto it = real_to_virt.upper_bound(reali);
-		assert(it != real_to_virt.begin() && "the requested address is out of range");
-		it--;
-		if(it->first + it->second.size <= reali)
-		{
-			llvm::errs()<<"Translating real addr "<<reali<<":\n";
-			llvm::errs()<<"it->first = "<<it->first<<"\n";
-			llvm::errs()<<"it->second.start = "<<it->second.start<<"\n";
-			llvm::errs()<<"it->second.size = "<<it->second.size<<"\n";
-		}
-		assert(it->first + it->second.size > reali && "the requested address is out of range");
+		auto it = find_start(real_to_virt, reali);
+		assert(it != real_to_virt.end() && "the requested address is out of range");
 		ptrdiff_t offset = reali - it->first;
 		return reinterpret_cast<void*>(it->second.start+offset);
 	}
@@ -70,12 +53,36 @@ public:
 		next_virt += size;
 	}
 
+	void unmap(void* ptr) override {
+		auto it = real_to_virt.find(reinterpret_cast<uintptr_t>(ptr));
+		assert(it != real_to_virt.end());
+		virt_to_real.erase(it->second.start);
+		real_to_virt.erase(it);
+	}
+
 	VirtualAddressMap() {
 		virt_to_real.emplace(0,Page(0,8));
 		real_to_virt.emplace(0,Page(0,8));
 		next_virt = 8;
 	}
 private:
+	static std::map<uintptr_t, Page>::const_iterator find_start(const std::map<uintptr_t, Page>& mapping,  uintptr_t addr) {
+		auto it = mapping.upper_bound(addr);
+		if (it == mapping.begin())
+			return mapping.end();
+		it--;
+		if (it->first + it->second.size <= addr) {
+#if DEBUG
+			llvm::errs()<<"Translating addr "<<addr<<":\n";
+			llvm::errs()<<"it->first = "<<it->first<<"\n";
+			llvm::errs()<<"it->second.start = "<<it->second.start<<"\n";
+			llvm::errs()<<"it->second.size = "<<it->second.size<<"\n";
+#endif
+			return mapping.end();
+		}
+		return it;
+	}
+
 	uintptr_t next_virt;
 	std::map<uintptr_t, Page> virt_to_real;
 	std::map<uintptr_t, Page> real_to_virt;
@@ -93,6 +100,9 @@ public:
 
 	void map(void* ptr, size_t size) override {
 	}
+	void unmap(void* ptr) override {
+	}
+
 };
 }
 #endif
