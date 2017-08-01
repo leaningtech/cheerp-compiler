e098c28db8b0edf1e4691151ef7e7c70f3159bc8
Sander Mathijs van Veen <sander@leaningtech.com>
2017-08-01 09:43:03 +0000

    Compile element section for wasm

--- llvm/include/llvm/Cheerp/WastWriter.h
+++ llvm/include/llvm/Cheerp/WastWriter.h
@@ -83,6 +83,7 @@ private:
 	void compileTableSection();
 	void compileMemoryAndGlobalSection();
 	void compileStartSection();
+	void compileElementSection();
 	void compileCodeSection();
 	void compileDataSection();
 
