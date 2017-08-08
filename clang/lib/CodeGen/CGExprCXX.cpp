8fc5ea8e1bd079396284865148e2510c15415142
Yuri Iozzelli <y.iozzelli@gmail.com>
2017-08-08 13:06:12 +0000

    Generate cheerp allocation intrinsics also in asmjs mode when possible

--- clang/lib/CodeGen/CGExprCXX.cpp
+++ clang/lib/CodeGen/CGExprCXX.cpp
@@ -1333,7 +1333,7 @@ static RValue EmitNewDeleteCall(CodeGenFunction &CGF,
 
   RValue RV;
   bool asmjs = CGF.CurFn->getSection() == StringRef("asmjs");
-  if(!CGF.getTarget().isByteAddressable() && !asmjs) {
+  if(!CGF.getTarget().isByteAddressable()) {
     // If allocType is specified this is an allocation, otherwise a free
     QualType retType;
     llvm::Function* intrinsic;
