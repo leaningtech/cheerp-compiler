dec04c4313a60578beff0bd24e0bf143ae33aeda
Yuri Iozzelli <y.iozzelli@gmail.com>
2017-09-19 15:27:48 +0000

    Enable type unsafe C function in asmjs

--- clang/lib/Sema/SemaExpr.cpp
+++ clang/lib/Sema/SemaExpr.cpp
@@ -6508,7 +6508,8 @@ ExprResult Sema::BuildResolvedCallExpr(Expr *Fn, NamedDecl *NDecl,
         Diag(Fn->getExprLoc(), diag::warn_arm_interrupt_calling_convention);
     }
 
-  if (!Context.getTargetInfo().isByteAddressable()) {
+  bool asmjs = FDecl && FDecl->hasAttr<AsmJSAttr>();
+  if (!Context.getTargetInfo().isByteAddressable() && !asmjs) {
     // Cheerp: Disable some type-unsafe C functions
     switch (BuiltinID) {
       case Builtin::BIbsearch:
