7c757e845f1f28607bdd6f6ff78cb6310a32f258
Yuri Iozzelli <y.iozzelli@gmail.com>
2017-09-20 13:49:27 +0000

    Disable unsafe cast warning for linear code

--- clang/lib/CodeGen/CGExprScalar.cpp
+++ clang/lib/CodeGen/CGExprScalar.cpp
@@ -2145,9 +2145,12 @@ Value *ScalarExprEmitter::VisitCastExpr(CastExpr *CE) {
     }
     else
     {
+      bool asmjs = CGF.CurFn && CGF.CurFn->getSection()==StringRef("asmjs");
+	
       llvm::Function* intrinsic = CGF.CGM.GetUserCastIntrinsic(CE,
 		      CGF.getContext().getPointerType(E->getType()),
-		      CGF.getContext().getPointerType(DestTy));
+		      CGF.getContext().getPointerType(DestTy),
+		      asmjs);
       V = Builder.CreateCall(intrinsic, V);
     }
     return EmitLoadOfLValue(LV, CE->getExprLoc());
@@ -2285,6 +2288,7 @@ Value *ScalarExprEmitter::VisitCastExpr(CastExpr *CE) {
       return EmitLoadOfLValue(DestLV, CE->getExprLoc());
     }
 
+    bool asmjs = CGF.CurFn && CGF.CurFn->getSection() == StringRef("asmjs");
     //We don't care about casts to functions types
     if (CGF.getTarget().isByteAddressable() || isa<llvm::ConstantPointerNull>(Src) ||
         (isa<llvm::FunctionType>(SrcTy->getPointerElementType()) && isa<llvm::FunctionType>(DstTy->getPointerElementType())) ||
@@ -2295,7 +2299,6 @@ Value *ScalarExprEmitter::VisitCastExpr(CastExpr *CE) {
     else if (isa<llvm::FunctionType>(SrcTy->getPointerElementType()) &&
 		!isa<llvm::FunctionType>(DstTy->getPointerElementType()))
     {
-      bool asmjs =CGF.CurFn->getSection() == StringRef("asmjs");
       if (!asmjs)
       {
         // On Cheerp in generic code we can't allow any function pointer to become any other pointer
@@ -2304,7 +2307,7 @@ Value *ScalarExprEmitter::VisitCastExpr(CastExpr *CE) {
     }
     else
     {
-      llvm::Function* intrinsic = CGF.CGM.GetUserCastIntrinsic(CE, E->getType(), DestTy);
+      llvm::Function* intrinsic = CGF.CGM.GetUserCastIntrinsic(CE, E->getType(), DestTy, asmjs);
       return Builder.CreateCall(intrinsic, Src);
     }
     return Builder.CreateBitCast(Src, DstTy);
