2ab673ee915c876aeb04f3fed12ac547464b487c
Yuri Iozzelli <y.iozzelli@gmail.com>
2018-01-11 15:02:18 +0100

    Improve attribute propagation for templates

--- clang/lib/Sema/SemaTemplate.cpp
+++ clang/lib/Sema/SemaTemplate.cpp
@@ -1990,9 +1990,6 @@ DeclResult Sema::CheckClassTemplate(
     AddMsStructLayoutForRecord(NewClass);
   }
 
-  // CHEERP: Inject asmjs/genericjs attribute if required
-  MaybeInjectCheerpModeAttr(NewClass);
-
   ClassTemplateDecl *NewTemplate
     = ClassTemplateDecl::Create(Context, SemanticContext, NameLoc,
                                 DeclarationName(Name), TemplateParams,
@@ -2036,6 +2033,8 @@ DeclResult Sema::CheckClassTemplate(
 
   AddPushedVisibilityAttribute(NewClass);
   inferGslOwnerPointerAttribute(NewClass);
+
+  MaybeInjectCheerpModeAttr(NewClass);
 
   if (TUK != TUK_Friend) {
     // Per C++ [basic.scope.temp]p2, skip the template parameter scopes.
