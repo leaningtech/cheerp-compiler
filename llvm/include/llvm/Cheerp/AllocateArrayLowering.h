2fd831a7903a8f44787cd9542c69a05aeabe1f54
Yuri Iozzelli <y.iozzelli@gmail.com>
2018-01-17 10:19:26 +0100

    Add a pass to lower the new array intrinsics

---
+++ llvm/include/llvm/Cheerp/AllocateArrayLowering.h
@@ -0,0 +1,44 @@
+//===-- Cheerp/AllocateArrayLowering.h - Cheerp optimization pass ---------===//
+//
+//                     Cheerp: The C++ compiler for the Web
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+// Copyright 2018 Leaning Technologies
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef _CHEERP_ALLOCATE_ARRAY_LOWERING_H
+#define _CHEERP_ALLOCATE_ARRAY_LOWERING_H
+
+#include "llvm/Pass.h"
+#include "llvm/IR/Instructions.h"
+#include "llvm/IR/Module.h"
+
+namespace llvm
+{
+
+/**
+ * Lowers cheerp_allocate_array and cheerp_deallocate_array
+ */
+class AllocateArrayLowering: public FunctionPass
+{
+public:
+	static char ID;
+	explicit AllocateArrayLowering() : FunctionPass(ID) { }
+	bool runOnFunction(Function &F) override;
+	const char *getPassName() const override;
+
+	virtual void getAnalysisUsage(AnalysisUsage&) const override;
+};
+
+//===----------------------------------------------------------------------===//
+//
+// AllocateArrayLowering
+//
+FunctionPass *createAllocateArrayLoweringPass();
+
+}
+
+#endif
