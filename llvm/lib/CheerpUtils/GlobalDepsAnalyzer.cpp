ffcd8d51c6dc4a304ccb401c1b23ad6e44ee4629
Yuri Iozzelli <y.iozzelli@gmail.com>
2018-01-26 16:07:26 +0100

    Fix the exporting of asmjs function to javascript

--- llvm/lib/CheerpUtils/GlobalDepsAnalyzer.cpp
+++ llvm/lib/CheerpUtils/GlobalDepsAnalyzer.cpp
@@ -177,6 +177,10 @@ bool GlobalDepsAnalyzer::runOnModule( llvm::Module & module )
 			visitGlobal( f, visited, vec );
 			assert( visited.empty() );
 			externals.push_back(f);
+			if (f->getSection() == StringRef("asmjs"))
+			{
+				asmJSExportedFuncions.insert(f);
+			}
 		}
 	}
 
