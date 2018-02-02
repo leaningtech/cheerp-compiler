2ba71a1538dd3f3bf6ac76d3cb3ff47539e30853
Alessandro Pignotti <a.pignotti@sssup.it>
2018-02-02 11:28:58 +0100

    Fix tagging of a function

--- libcxxabi/src/abort_message.h
+++ libcxxabi/src/abort_message.h
@@ -12,6 +12,9 @@
 #include "cxxabi.h"
 
 extern "C" _LIBCXXABI_HIDDEN _LIBCXXABI_NORETURN void
+#ifdef __CHEERP__
+__attribute__((cheerp_genericjs))
+#endif
 abort_message(const char *format, ...) __attribute__((format(printf, 1, 2)));
 
 #endif
