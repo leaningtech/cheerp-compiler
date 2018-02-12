c2e20f3178a3d72c6c11659cd0103047e430c52d
Alessandro Pignotti <a.pignotti@sssup.it>
2018-02-12 12:24:11 +0100

    Fix a subtle lifetime issue that was causing crashes on mac

--- clang/lib/CodeGen/CGRecordLayoutBuilder.cpp
+++ clang/lib/CodeGen/CGRecordLayoutBuilder.cpp
@@ -827,8 +827,7 @@ CodeGenTypes::ComputeRecordLayout(const RecordDecl *D, llvm::StructType *Ty) {
       basesRange.push_back(llvm::ConstantAsMetadata::get(llvm::ConstantInt::get(CGM.Int32Ty, RL->firstBaseElement)));
       basesRange.push_back(llvm::ConstantAsMetadata::get(llvm::ConstantInt::get(CGM.Int32Ty, RL->totalNumberOfBases)));
       llvm::MDNode* meta = llvm::MDNode::get(getLLVMContext(), basesRange);
-      llvm::Twine basesMetaName(Ty->getName(),"_bases");
-      llvm::NamedMDNode* basesMeta = TheModule.getOrInsertNamedMetadata(basesMetaName.str());
+      llvm::NamedMDNode* basesMeta = TheModule.getOrInsertNamedMetadata((Ty->getName() + "_bases").str());
       basesMeta->addOperand(meta);
     }
   }
