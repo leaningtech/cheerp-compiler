66c877e3d5fecb20765008dd1c83ca7d31dc542d
Alessandro Pignotti <a.pignotti@sssup.it>
2018-03-12 16:44:57 +0100

    Deprecate and remove client/server support

--- llvm/lib/Analysis/InlineCost.cpp
+++ llvm/lib/Analysis/InlineCost.cpp
@@ -2897,6 +2897,7 @@ Optional<InlineResult> llvm::getAttributeBasedInliningDecision(
     }
 
   //CHEERP: Do not inline normal/asmjs methods called from the other side
+  const Function* caller=CS.getCaller();
   bool callerAsmJS = caller->getSection() == StringRef("asmjs");
   bool calleeAsmJS = Callee->getSection() == StringRef("asmjs");
   if (calleeAsmJS!= callerAsmJS)
