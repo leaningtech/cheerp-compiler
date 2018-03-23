a2d437ad3076fdd5608a143561423352acd822d3
Alessandro Pignotti <a.pignotti@sssup.it>
2018-03-23 14:32:33 +0100

    Restore and improve support for bounds checking in asm.js

--- llvm/include/llvm/Cheerp/Writer.h
+++ llvm/include/llvm/Cheerp/Writer.h
@@ -568,19 +568,11 @@ public:
 	/**
 	 * Compile a bound-checking statement for heap accesses in asm.js
 	 */
-	void compileCheckBoundsAsmJS(const llvm::Value* p);
+	void compileCheckBoundsAsmJS(const llvm::Value* p, int alignMask);
 	/**
 	 * Compile a bound-checking function definition for asm.js heap
 	 */
 	void compileCheckBoundsAsmJSHelper();
-	/**
-	 * Compile a statement for checking function pointer validity in asm.js
-	 */
-	void compileCheckFunctionPtrAsmJS(const llvm::Value* p, uint32_t size);
-	/**
-	 * Compile a function definition for checking function pointer validity in asm.js
-	 */
-	void compileCheckFunctionPtrAsmJSHelper();
 	/**
 	 * Compile a function to assure a GEP property access is defined
 	 */
