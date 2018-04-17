ac8b6c1a839cf735248e8ea83ef1c86a034ceaa7
Yuri Iozzelli <y.iozzelli@gmail.com>
2018-04-17 16:25:45 +0200

    Add the asmjs flag to anonymous structs when appropriate

--- clang/lib/CodeGen/ItaniumCXXABI.cpp
+++ clang/lib/CodeGen/ItaniumCXXABI.cpp
@@ -3807,12 +3807,13 @@ void ItaniumRTTIBuilder::BuildVTablePointer(const Type *Ty) {
         CGM.getLangOpts().getCheerpMode() == LangOptions::CHEERP_MODE_Wasm);
     }
     llvm::Type* WrapperTypes[] = {CGM.getTypes().GetBasicVTableType(8, asmjs)};
-    llvm::Constant *VTable = CGM.getModule().getOrInsertGlobal(VTableName, llvm::StructType::get(CGM.getLLVMContext(), WrapperTypes, false, NULL));
+    llvm::Type* VTableType = llvm::StructType::get(CGM.getLLVMContext(), WrapperTypes, false, NULL, asmjs);
+    llvm::Constant *VTable = CGM.getModule().getOrInsertGlobal(VTableName, VTableType);
     llvm::Constant *Zero = llvm::ConstantInt::get(CGM.Int32Ty, 0);
     llvm::SmallVector<llvm::Constant*, 2> GepIndexes;
     GepIndexes.push_back(Zero);
     GepIndexes.push_back(Zero);
-    VTable = llvm::ConstantExpr::getInBoundsGetElementPtr(VTable, GepIndexes);
+    VTable = llvm::ConstantExpr::getInBoundsGetElementPtr(VTableType, VTable, GepIndexes);
     VTable = llvm::ConstantExpr::getBitCast(VTable, CGM.getTypes().GetVTableBaseType(asmjs)->getPointerTo());
     Fields.push_back(VTable);
     return;
@@ -3910,6 +3911,13 @@ llvm::Constant *ItaniumRTTIBuilder::BuildTypeInfo(QualType Ty) {
   // We want to operate on the canonical type.
   Ty = Ty.getCanonicalType();
 
+  // CHEERP: TODO duplicate typeinfo for basic types
+  bool asmjs = false;
+  if (CGM.getLangOpts().getCheerpMode() == LangOptions::CHEERP_MODE_AsmJS ||
+      CGM.getLangOpts().getCheerpMode() == LangOptions::CHEERP_MODE_Wast ||
+      CGM.getLangOpts().getCheerpMode() == LangOptions::CHEERP_MODE_Wasm) {
+    asmjs = true;
+  }
   // Check if we've already emitted an RTTI descriptor for this type.
   SmallString<256> Name;
   llvm::raw_svector_ostream Out(Name);
@@ -4041,6 +4049,7 @@ llvm::Constant *ItaniumRTTIBuilder::BuildTypeInfo(
   case Type::Record: {
     const CXXRecordDecl *RD =
       cast<CXXRecordDecl>(cast<RecordType>(Ty)->getDecl());
+    asmjs = RD->hasAttr<AsmJSAttr>();
     if (!RD->hasDefinition() || !RD->getNumBases()) {
       // We don't need to emit any fields.
       break;
@@ -4077,7 +4086,7 @@ llvm::Constant *ItaniumRTTIBuilder::BuildTypeInfo(
   }
 
   llvm::Type* directBase = CGM.getTarget().isByteAddressable() ? NULL : CGM.getTypes().GetClassTypeInfoType();
-  llvm::Constant *Init = llvm::ConstantStruct::getAnon(Fields, false, directBase ? cast<llvm::StructType>(directBase) : NULL);
+  llvm::Constant *Init = llvm::ConstantStruct::getAnon(Fields, false, directBase ? cast<llvm::StructType>(directBase) : NULL, asmjs);
 
   SmallString<256> Name;
   llvm::raw_svector_ostream Out(Name);
@@ -4362,7 +4371,7 @@ void ItaniumRTTIBuilder::BuildVMIClassTypeInfo(const CXXRecordDecl *RD) {
       OffsetFlags |= BCTI_Public;
 
     baseFields.push_back(llvm::ConstantInt::get(OffsetFlagsLTy, OffsetFlags));
-    basesFields.push_back(llvm::ConstantStruct::getAnon(baseFields));
+    basesFields.push_back(llvm::ConstantStruct::getAnon(baseFields, false, NULL, asmjs));
   }
   if(!CGM.getTarget().isByteAddressable()) {
     for (auto it = CGLayout.vbases_begin(); it != CGLayout.vbases_end(); ++it) {
@@ -4376,7 +4385,7 @@ void ItaniumRTTIBuilder::BuildVMIClassTypeInfo(const CXXRecordDecl *RD) {
       else
         Offset = CGLayout.getTotalOffsetToBase(it->second);
       baseFields.push_back(llvm::ConstantInt::get(OffsetFlagsLTy, Offset));
-      basesFields.push_back(llvm::ConstantStruct::getAnon(baseFields));
+      basesFields.push_back(llvm::ConstantStruct::getAnon(baseFields, false, NULL, asmjs));
     }
   }
   llvm::ArrayType* basesArrayType = llvm::ArrayType::get(basesFields[0]->getType(), basesFields.size());
