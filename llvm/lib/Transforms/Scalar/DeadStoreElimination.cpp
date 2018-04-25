b0478d044dc2f61ab1410c839a4005d0e03f2668
Alessandro Pignotti <a.pignotti@sssup.it>
2018-04-25 09:44:59 +0200

    Do not remove stores of float/doubles to calloc-ed memory, on Cheerp they are initialized with -0

--- llvm/lib/Transforms/Scalar/DeadStoreElimination.cpp
+++ llvm/lib/Transforms/Scalar/DeadStoreElimination.cpp
@@ -1130,7 +1130,9 @@ static bool eliminateNoopStore(Instruction *Inst, BasicBlock::iterator &BBI,
 
   // Remove null stores into the calloc'ed objects
   Constant *StoredConstant = dyn_cast<Constant>(SI->getValueOperand());
-  if (StoredConstant && StoredConstant->isNullValue() && isRemovable(SI)) {
+  // Cheerp: Do not eliminate null floating point stores, the memory is initialized with -0 actually
+  if (StoredConstant && StoredConstant->isNullValue() && isRemovable(SI) &&
+       (!StoredConstant->getType()->isFloatingPointTy() || DL.isByteAddressable())) {
     Instruction *UnderlyingPointer =
         dyn_cast<Instruction>(getUnderlyingObject(SI->getPointerOperand()));
 
