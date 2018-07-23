77a20f035aacbc8a4d72deee4d8142d9fa11c283
Yuri Iozzelli <y.iozzelli@gmail.com>
2018-07-23 15:25:22 +0200

    Improve FFI checks for function arguments

--- clang/lib/Sema/SemaOverload.cpp
+++ clang/lib/Sema/SemaOverload.cpp
@@ -13145,12 +13145,6 @@ static ExprResult FinishOverloadedCallExpr(Sema &SemaRef, Scope *S, Expr *Fn,
     if (SemaRef.DiagnoseUseOfDecl(FDecl, ULE->getNameLoc()))
       return ExprError();
     Fn = SemaRef.FixOverloadedFunctionReference(Fn, (*Best)->FoundDecl, FDecl);
-    if (S && S->getFnParent())
-    {
-      if (FunctionDecl* Parent = dyn_cast<FunctionDecl>(S->getFnParent()->getEntity())) {
-        SemaRef.CheckCheerpFFICall(Parent, FDecl, Fn->getLocStart(), Args);
-      }
-    }
     return SemaRef.BuildResolvedCallExpr(Fn, FDecl, LParenLoc, Args, RParenLoc,
                                          ExecConfig, /*IsExecConfig=*/false,
                                          (*Best)->IsADLCandidate);
@@ -13719,12 +13713,6 @@ ExprResult Sema::CreateOverloadedBinOp(SourceLocation OpLoc,
           }
         }
 
-        Scope *S = getScopeForContext(CurContext);
-        if (S && S->getFnParent())
-        {
-          if (FunctionDecl* Parent = dyn_cast<FunctionDecl>(S->getFnParent()->getEntity()))
-            CheckCheerpFFICall(Parent, FnDecl, OpLoc, Args);
-        }
         // Convert the arguments.
         if (CXXMethodDecl *Method = dyn_cast<CXXMethodDecl>(FnDecl)) {
           // Best->Access is only meaningful for class members.
@@ -13785,7 +13773,7 @@ ExprResult Sema::CreateOverloadedBinOp(SourceLocation OpLoc,
                                 FnDecl))
           return ExprError();
 
-        ArrayRef<const Expr *> ArgsArray(Args, 2);
+        ArrayRef<Expr *> ArgsArray(Args, 2);
         const Expr *ImplicitThis = nullptr;
         // Cut off the implicit 'this'.
         if (isa<CXXMethodDecl>(FnDecl)) {
@@ -14128,7 +14116,7 @@ Sema::CreateOverloadedArraySubscriptExpr(SourceLocation LLoc,
         if (S && S->getFnParent())
         {
           if (FunctionDecl* Parent = dyn_cast<FunctionDecl>(S->getFnParent()->getEntity()))
-            CheckCheerpFFICall(Parent, FnDecl, RLoc, Args);
+            CheckCheerpFFICall(Parent, FnDecl, RLoc, ArrayRef<Expr*>(Args, 2));
         }
         CheckMemberOperatorAccess(LLoc, Args[0], Args[1], Best->FoundDecl);
 
