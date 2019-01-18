564c6143364f29321b76dc292f4d77e7c6ced3f9
Alessandro Pignotti <a.pignotti@sssup.it>
2019-01-18 10:50:19 +0000

    Wasm: Rationalize Trunc handling

--- llvm/lib/CheerpWriter/CheerpWasmWriter.cpp
+++ llvm/lib/CheerpWriter/CheerpWasmWriter.cpp
@@ -2357,10 +2357,7 @@ bool CheerpWasmWriter::compileInlineInstruction(WasmBuffer& code, const Instruct
 			break;
 		case Instruction::Trunc:
 		{
-			uint32_t bitWidth = I.getType()->getIntegerBitWidth();
 			compileOperand(code, I.getOperand(0));
-			encodeS32Inst(0x41, "i32.const", getMaskForBitWidth(bitWidth), code);
-			encodeInst(0x71, "i32.and", code);
 			break;
 		}
 		case Instruction::Ret:
