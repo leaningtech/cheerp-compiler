16de847df7b91684462f73d65461af86aa5fd74b
Alessandro Pignotti <a.pignotti@sssup.it>
2019-03-03 14:43:30 +0100

    Fix a link error with strerror

--- clang/lib/Basic/Targets/WebAssembly.h
+++ clang/lib/Basic/Targets/WebAssembly.h
@@ -220,6 +220,7 @@ public:
     // We don't have multiple asm variants, and we want to be able to use
     // '{' and '}' in the asm code
     NoAsmVariants = true;
+    UserLabelPrefix = "";
   }
 
   virtual ArrayRef<Builtin::Info> getTargetBuiltins() const {
