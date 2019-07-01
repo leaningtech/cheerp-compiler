7bc8be0e783b778d42ae709c9d39f433c501be7f
Alessandro Pignotti <a.pignotti@sssup.it>
2019-07-01 11:49:08 +0200

    Expose the [[cheerp::bytelayout]] attribute to the user

--- clang/lib/AST/Decl.cpp
+++ clang/lib/AST/Decl.cpp
@@ -4616,6 +4616,8 @@ bool RecordDecl::isByteLayout() const
 {
   if (hasAttr<AsmJSAttr>())
     return false;
+  if (hasAttr<ByteLayoutAttr>())
+    return true;
   // Unions and anonymous structures inside unions use bytelayout
   const RecordDecl *CurDecl = this;
   while (CurDecl && (CurDecl->isUnion() || CurDecl->isAnonymousStructOrUnion()))
