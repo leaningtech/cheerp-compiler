f1cbf9e354006185892164aaf7caa4aab5b55561
Yuri Iozzelli <y.iozzelli@gmail.com>
2019-07-10 15:13:51 +0200

    Use the ByValLowering pass at the end of the frontend stage

--- clang/lib/CodeGen/BackendUtil.cpp
+++ clang/lib/CodeGen/BackendUtil.cpp
@@ -28,6 +28,7 @@
 #include "llvm/Bitcode/BitcodeWriterPass.h"
 #include "llvm/Cheerp/NativeRewriter.h"
 #include "llvm/Cheerp/ExpandStructRegs.h"
+#include "llvm/Cheerp/ByValLowering.h"
 #include "llvm/CodeGen/RegAllocRegistry.h"
 #include "llvm/CodeGen/SchedulerRegistry.h"
 #include "llvm/CodeGen/TargetSubtargetInfo.h"
@@ -541,7 +542,7 @@ getInstrProfOptions(const CodeGenOptions &CodeGenOpts,
 }
 
 static void addCheerpPasses(const PassManagerBuilder &Builder,
-                            PassManagerBase &PM) {
+                            legacy::PassManagerBase &PM) {
   PM.add(createLowerInvokePass());
   PM.add(createCFGSimplificationPass());
   //Run mem2reg first, to remove load/stores for the this argument
@@ -553,8 +554,14 @@ static void addCheerpPasses(const PassManagerBuilder &Builder,
 }
 
 static void addPostInlineCheerpPasses(const PassManagerBuilder &Builder,
-                                      PassManagerBase &PM) {
+                                      legacy::PassManagerBase &PM) {
   PM.add(createExpandStructRegs());
+}
+
+static void addModuleCheerpPasses(const PassManagerBuilder &Builder,
+                            legacy::PassManagerBase &PM) {
+  // Lower byval parameters
+  PM.add(createByValLoweringPass());
 }
 
 static void setCommandLineOpts(const CodeGenOptions &CodeGenOpts) {
