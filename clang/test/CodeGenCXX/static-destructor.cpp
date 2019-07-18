20fa1ccf7cffde61a99b09fc7fc829ee27bc8186
Alessandro Pignotti <a.pignotti@sssup.it>
2019-07-18 09:20:28 +0000

    Fix tests after rebase

--- clang/test/CodeGenCXX/static-destructor.cpp
+++ clang/test/CodeGenCXX/static-destructor.cpp
@@ -17,12 +17,12 @@ Foo global;
 
 // X86 destructors have void return, and are registered directly with __cxa_atexit.
 // X86: define internal void @__cxx_global_var_init()
-// X86:   call i32 @__cxa_atexit(void (i8*)* bitcast (void (%class.Foo*)* @_ZN3FooD1Ev to void (i8*)*), i8* getelementptr inbounds (%class.Foo, %class.Foo* @global, i32 0, i32 0), i8* @__dso_handle)
+// X86:   call i32 @__cxa_atexit(void (i8*)* bitcast (void (%class._Z3Foo*)* @_ZN3FooD1Ev to void (i8*)*), i8* bitcast (%class._Z3Foo* @global to i8*), i8* @__dso_handle)
 
 // ARM destructors return this, but can be registered directly with __cxa_atexit
 // because the calling conventions tolerate the mismatch.
 // ARM: define internal void @__cxx_global_var_init()
-// ARM:   call i32 @__cxa_atexit(void (i8*)* bitcast (%class.Foo* (%class.Foo*)* @_ZN3FooD1Ev to void (i8*)*), i8* getelementptr inbounds (%class.Foo, %class.Foo* @global, i32 0, i32 0), i8* @__dso_handle)
+// ARM:   call i32 @__cxa_atexit(void (i8*)* bitcast (%class._Z3Foo* (%class._Z3Foo*)* @_ZN3FooD1Ev to void (i8*)*), i8* bitcast (%class._Z3Foo* @global to i8*), i8* @__dso_handle)
 
 // Wasm destructors return this, and use a wrapper function, which is registered
 // with __cxa_atexit.
