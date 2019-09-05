e60a6348d02210207374e89b271ea788b506d898
Yuri Iozzelli <y.iozzelli@gmail.com>
2019-09-05 11:05:22 +0200

    Fix double increment of parameter iterator

--- clang/lib/Sema/SemaExpr.cpp
+++ clang/lib/Sema/SemaExpr.cpp
@@ -20166,8 +20166,6 @@ void Sema::CheckCheerpFFICall(const FunctionDecl* Parent, const FunctionDecl* FD
             d << "variadic";
         }
       }
-      if (p != pe)
-        p++;
     }
   }
 }
