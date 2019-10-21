af8107ee3ac0ef7b7e6b9b89edeec6b1eadb0d4b
Carlo Piovesan <carlo.piovesan@gmail.com>
2019-10-21 09:48:50 +0200

    private + test DeterministicUnordered things

--- llvm/include/llvm/Cheerp/DeterministicUnorderedMap.h
+++ llvm/include/llvm/Cheerp/DeterministicUnorderedMap.h
@@ -17,19 +17,27 @@
 /*
    Deterministic Unordered Map
 
-   Currently implement the most common map-operations (insert, operator[], count, find, erase, clear, empty, swap, operator=)
-   with a slight per-operation additional cost over a std::map (roughly 2x in memory size and negligible in time-complexity)
+   Implement most of unordered_map-interface (insert, operator[], count, find, erase, clear, empty, swap, operator=),
+   with similar performance time-wise while having a slight memory overhead
    All operation, iterations comprised, are deterministic
 
-   No operation invalidates "live" iterators (eg. after you call delete, you should not access an iterator)
+   On construction you may provide an additional template paramether explicitly guarantee that certain restrictions apply,
+   and this information is used to improve the speed of the container.
+   Restrictions that can be voided are: NoErasure (no erase calls allowed), NoPointerStability (objects can be moved)
 
-   The determinism is achieved by keeping 2 different datastructure updated at the same time, a std::map<key,std::list<>::iterator> and a std::list<std::pair<key, mapped>>.
-   Iterations are done on the list iterators while the other operations are performed first on the map
+   If NoPointerStability is not specified, no operation invalidates "live" iterators (eg. after you call delete, you should not access an iterator.
+
+   The determinism is achieved by keeping 2 different datastructure updated at the same time.
+   One is either a std::list or std::deque (list in the general case, deque if additional guarantee are given) of pair<Key, Mapped>
+   The other is an unordered_map from key (or key*) to iterators in the other container with an appropriate Equality function.
+   Iterations are done on the list/deque iterators while the other operations are performed first on the map
 */
 
 
 namespace cheerp
 {
+
+using namespace deterministicUnorderedImplementation;
 
 template <typename Key, typename Mapped, class Hash_Key, template<typename, typename...> class Container, bool CouldErase>
 class DeterministicUnorderedMapImpl : public
@@ -41,21 +49,9 @@ public:
 	using BaseClass::BaseClass;
 	using typename BaseClass::iterator;
 	using typename BaseClass::const_iterator;
-	using BaseClass::map;
-	using BaseClass::container;
 	std::pair<iterator,bool> emplace(const Key& k, const Mapped& m)
 	{
 		return insertImpl(k, m);
-	}
-	std::pair<iterator,bool> insertImpl(const Key& k, const Mapped& m)
-	{
-		iterator W = this->find(k);
-		if (W != BaseClass::end())
-			return {W, false};
-		container.push_back(std::make_pair(k, m));
-		W = --BaseClass::end();
-		map[this->mapped(W->first)] = W;
-		return {W, true};
 	}
 	std::pair<iterator,bool> insert(const value_type& value)
 	{
@@ -89,12 +85,16 @@ public:
 			++b;
 		}
 	}
+	bool erase(const Key& t)
+	{
+		return eraseImpl<CouldErase>(t);
+	}
 	Mapped& operator[](const Key& key)
 	{
-	auto it = this->find(key);
-	if (it == this->end())
-		return ((insert(std::make_pair(key,Mapped{}))).first)->second;
-	return it->second;
+		auto it = this->find(key);
+		if (it == this->end())
+			return ((insert(std::make_pair(key,Mapped{}))).first)->second;
+		return it->second;
 	}
 	Mapped& at(const Key& key)
 	{
@@ -105,6 +105,19 @@ public:
 	const Mapped& at(const Key& key) const
 	{
 		return const_cast<DeterministicUnorderedMapImpl*>(this)->at(key);
+	}
+private:
+	using BaseClass::map;
+	using BaseClass::container;
+	std::pair<iterator,bool> insertImpl(const Key& k, const Mapped& m)
+	{
+		iterator W = this->find(k);
+		if (W != BaseClass::end())
+			return {W, false};
+		container.push_back(std::make_pair(k, m));
+		W = --BaseClass::end();
+		map[this->mapped(W->first)] = W;
+		return {W, true};
 	}
 	template<bool couldErase>
 	bool eraseImpl(const Key& t);
@@ -124,10 +137,6 @@ public:
 		static_assert(CouldErase, "No erase are possible");
 		return false;
 	}
-	bool erase(const Key& t)
-	{
-		return eraseImpl<CouldErase>(t);
-	}
 };
 
 template <typename Key, typename Mapped, class Hash_Key, bool CouldErase>
@@ -146,8 +155,7 @@ class ListMap : public DeterministicUnorderedMapImpl<Key, Mapped, Hash_Key, std:
 
 
 template <typename Key, typename Mapped, RestrictionsLifted restrictionVoided = RestrictionsLifted::None, class Hash_Key=std::hash<Key>>
-class DeterministicUnorderedMap : public 
-				  std::conditional<
+class DeterministicUnorderedMap : public std::conditional<
 		couldBeDeque<std::pair<Key,Mapped>>(restrictionVoided),
 		DequeMap<Key, Mapped, Hash_Key, !nonEraseable(restrictionVoided)>,
 		ListMap<Key, Mapped, Hash_Key, !nonEraseable(restrictionVoided)> >::type
--- llvm/include/llvm/Cheerp/DeterministicUnorderedSet.h
+++ llvm/include/llvm/Cheerp/DeterministicUnorderedSet.h
@@ -15,21 +15,29 @@
 #include "llvm/Cheerp/DeterministicUnorderedImpl.h"
 
 /*
-   Deterministic Unordered Map
+   Deterministic Unordered Set
 
-   Currently implement the most common map-operations (insert, operator[], count, find, erase, clear, empty, swap, operator=)
-   with a slight per-operation additional cost over a std::map (roughly 2x in memory size and negligible in time-complexity)
+   Implement most of unordered_set-interface (insert, count, find, erase, clear, empty, swap, operator=),
+   with similar performance time-wise while having a slight memory overhead
    All operation, iterations comprised, are deterministic
 
-   No operation invalidates "live" iterators (eg. after you call delete, you should not access an iterator)
+   On construction you may provide an additional template paramether explicitly guarantee that certain restrictions apply,
+   and this information is used to improve the speed of the container.
+   Restrictions that can be voided are: NoErasure (no erase calls allowed), NoPointerStability (objects can be moved)
 
-   The determinism is achieved by keeping 2 different datastructure updated at the same time, a std::map<key,std::list<>::iterator> and a std::list<std::pair<key, mapped>>.
-   Iterations are done on the list iterators while the other operations are performed first on the map
+   If NoPointerStability is not specified, no operation invalidates "live" iterators (eg. after you call delete, you should not access an iterator.
+
+   The determinism is achieved by keeping 2 different datastructure updated at the same time.
+   One is either a std::list or std::deque (list in the general case, deque if additional guarantee are given) of Key
+   The other is an unordered_map from key (or key*) to iterators in the other container with an appropriate Equality function.
+   Iterations are done on the list/deque iterators while the other operations are performed first on the map
 */
 
 
 namespace cheerp
 {
+
+using namespace deterministicUnorderedImplementation;
 
 template <typename Key, class Hash_Key, template<typename, typename...> class Container, bool CouldErase>
 class DeterministicUnorderedSetImpl : public
@@ -41,21 +49,9 @@ public:
 	using BaseClass::BaseClass;
 	using typename DeterministicUnorderedImpl<Key, value_type, Hash_Key, Container, CouldErase>::iterator;
 	using typename DeterministicUnorderedImpl<Key, value_type, Hash_Key, Container, CouldErase>::const_iterator;
-	using BaseClass::map;
-	using BaseClass::container;
 	std::pair<iterator,bool> emplace(const Key& k)
 	{
 		return insertImpl(k);
-	}
-	std::pair<iterator,bool> insertImpl(const Key& k)
-	{
-		iterator W = this->find(k);
-		if (W != BaseClass::end())
-			return {W, false};
-		container.push_back(k);
-		W = --BaseClass::end();
-		map[this->mapped(*W)] = W;
-		return {W, true};
 	}
 	std::pair<iterator,bool> insert(const value_type& value)
 	{
@@ -89,6 +85,23 @@ public:
 			++b;
 		}
 	}
+	bool erase(const Key& t)
+	{
+		return eraseImpl<CouldErase>(t);
+	}
+private:
+	using BaseClass::map;
+	using BaseClass::container;
+	std::pair<iterator,bool> insertImpl(const Key& k)
+	{
+		iterator W = this->find(k);
+		if (W != BaseClass::end())
+			return {W, false};
+		container.push_back(k);
+		W = --BaseClass::end();
+		map[this->mapped(*W)] = W;
+		return {W, true};
+	}
 	template<bool couldErase>
 	bool eraseImpl(const Key& t);
 	template<>
@@ -106,10 +119,6 @@ public:
 	{
 		static_assert(CouldErase, "No erase are possible");
 		return false;
-	}
-	bool erase(const Key& t)
-	{
-		return eraseImpl<CouldErase>(t);
 	}
 };
 
