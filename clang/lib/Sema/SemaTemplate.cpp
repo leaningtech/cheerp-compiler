80f13043663f628a4af8b280d0270ccd35b7f307
Carlo Piovesan <carlo.piovesan@gmail.com>
2019-10-23 12:25:02 +0200

    JsExport: Templated class or struct cannot be jsexported

--- clang/lib/Sema/SemaTemplate.cpp
+++ clang/lib/Sema/SemaTemplate.cpp
@@ -31,6 +31,7 @@
 #include "clang/Sema/Overload.h"
 #include "clang/Sema/ParsedTemplate.h"
 #include "clang/Sema/Scope.h"
+#include "clang/Sema/SemaCheerp.h"
 #include "clang/Sema/SemaInternal.h"
 #include "clang/Sema/Template.h"
 #include "clang/Sema/TemplateDeduction.h"
@@ -2134,6 +2135,11 @@ DeclResult Sema::CheckClassTemplate(
   inferGslOwnerPointerAttribute(NewClass);
 
   MaybeInjectCheerpModeAttr(NewClass);
+
+  if (NewClass->hasAttr<JsExportAttr>())
+  {
+    Diag(NameLoc, diag::err_cheerp_jsexport_on_class_template);
+  }
 
   if (TUK != TUK_Friend) {
     // Per C++ [basic.scope.temp]p2, skip the template parameter scopes.
