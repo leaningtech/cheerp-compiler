1da0fdf852c5feba5a3508fb1f357dd1fba33f15
Alessandro Pignotti <a.pignotti@sssup.it>
2019-10-23 10:53:31 +0200

    Expose a command line option to export the table from the wasm module

--- clang/lib/Driver/ToolChains/WebAssembly.cpp
+++ clang/lib/Driver/ToolChains/WebAssembly.cpp
@@ -680,12 +680,14 @@ enum CheerpWasmOpt
   INVALID,
   GROWMEM,
   SHAREDMEM,
+  EXPORTEDTABLE,
 };
 static CheerpWasmOpt parseWasmOpt(StringRef opt)
 {
   return llvm::StringSwitch<CheerpWasmOpt>(opt)
     .Case("growmem", GROWMEM)
     .Case("sharedmem", SHAREDMEM)
+    .Case("exportedtable", EXPORTEDTABLE)
     .Default(INVALID);
 }
 static std::vector<CheerpWasmOpt> getWasmFeatures(const Driver& D, const ArgList& Args)
@@ -866,6 +868,9 @@ void cheerp::CheerpCompiler::ConstructJob(Compilation &C, const JobAction &JA,
         break;
       case GROWMEM:
         noGrowMem = false;
+        break;
+      case EXPORTEDTABLE:
+        CmdArgs.push_back("-cheerp-wasm-exported-table");
         break;
       default:
         llvm_unreachable("invalid wasm option");
