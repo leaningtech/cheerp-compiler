aca46c012a45a722a4932eee3260cec5140ca250
Carlo Piovesan <carlo.piovesan@gmail.com>
2019-10-23 11:15:30 +0200

    JsExport: fail with error when there are more than 1 user defined constructors

---
+++ clang/lib/Sema/SemaCheerp.cpp
@@ -0,0 +1,13 @@
+//===-- Sema/SemaCheerp.cpp - Cheerp Sema utilities -------------------------===//
+//
+//                     Cheerp: The C++ compiler for the Web
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+// Copyright 2019 Leaning Technologies
+//
+//===----------------------------------------------------------------------===//
+
+#include "clang/Sema/SemaCheerp.h"
+
