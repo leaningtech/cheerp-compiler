77f6e0c5f2d2e54401616e9450f13cdff40d4d97
Carlo Piovesan <carlo.piovesan@gmail.com>
2019-10-23 11:53:49 +0200

    JsExport: refactoring with function couldBeJsExported(RecordDecl*)

--- clang/lib/Sema/SemaCheerp.cpp
+++ clang/lib/Sema/SemaCheerp.cpp
@@ -10,4 +10,43 @@
 //===----------------------------------------------------------------------===//
 
 #include "clang/Sema/SemaCheerp.h"
+#include "clang/Basic/PartialDiagnostic.h"
+#include "clang/Sema/SemaInternal.h"
 
+bool cheerp::couldBeJsExported(clang::CXXRecordDecl* Record, clang::Sema& sema)
+{
+	//class or struct
+	using namespace clang;
+	if (!Record->hasAttr<JsExportAttr>())
+		return false;
+
+	if (Record->isDynamicClass())
+	{
+		sema.Diag(Record->getLocation(), diag::err_cheerp_attribute_on_virtual_class) << Record->getAttr<JsExportAttr>();
+		return false;
+	}
+
+	uint32_t numUserDeclared = cheerp::getNumUserDefinedMethods<CapturedDecl::specific_decl_iterator< CXXConstructorDecl > >(Record->ctors());
+	if (numUserDeclared != 1)
+	{
+		if (numUserDeclared == 0)
+		{
+			assert(!Record->hasUserDeclaredConstructor());
+			sema.Diag(Record->getLocation(), diag::err_cheerp_jsexport_on_class_without_constructor);
+		}
+		else
+		{
+			sema.Diag(Record->getLocation(), diag::err_cheerp_jsexport_on_class_with_multiple_user_defined_constructor);
+		}
+		return false;
+	}
+
+	if (Record->hasNonTrivialDestructor())
+	{
+		sema.Diag(Record->getLocation(), diag::err_cheerp_jsexport_with_non_trivial_destructor);
+		return false;
+	}
+
+	//TODO: Check for any public data or static member
+	return true;
+}
