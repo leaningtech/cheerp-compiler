b82202f3f9553a48f61d77b12c30d3126354d509
Carlo Piovesan <carlo.piovesan@gmail.com>
2019-10-29 23:37:41 +0100

    JsExport: Refactor iteration of jsExport metadata and add jsexported names to reserved names list

--- llvm/include/llvm/Cheerp/JsExport.h
+++ llvm/include/llvm/Cheerp/JsExport.h
@@ -1,16 +1,19 @@
-//===-- Cheerp/JsExport.h - Cheerp common routines -------------------------===//
+//===-- Cheerp/JsExport.h - Cheerp JsExport utilities routines -------------------------===//
 //
 //                     Cheerp: The C++ compiler for the Web
 //
 // This file is distributed under the University of Illinois Open Source
 // License. See LICENSE.TXT for details.
 //
-// Copyright 2019 Leaning Technologies
+// Copyright 2019-2020 Leaning Technologies
 //
 //===----------------------------------------------------------------------===//
 
 #ifndef _CHEERP_JSEXPORT_H
 #define _CHEERP_JSEXPORT_H
+
+#include "llvm/Cheerp/Writer.h"
+
 
 namespace cheerp
 {
@@ -39,6 +42,30 @@ inline bool isStatic(const uint32_t value)
 inline bool isConst(const uint32_t value)
 {
 	return value & 1;
+}
+
+template <typename FuncOnFunctions, typename FuncOnRecord>
+void iterateOverJsExportedMetadata(const llvm::Module& M, FuncOnFunctions funcOnFunctions, FuncOnRecord funcOnRecord)
+{
+	for( const llvm::NamedMDNode& namedNode: M.named_metadata())
+	{
+		llvm::StringRef name = namedNode.getName();
+
+		if(name == "jsexported_methods")
+		{
+			for ( llvm::NamedMDNode::const_op_iterator it = namedNode.op_begin(); it != namedNode.op_end(); ++ it )
+			{
+				const llvm::MDNode * node = *it;
+				const llvm::Function * f = llvm::cast<llvm::Function>(llvm::cast<llvm::ConstantAsMetadata>(node->getOperand(0))->getValue());
+				funcOnFunctions(f);
+			}
+			continue;
+		}
+		if (!name.endswith("_methods") || !(name.startswith("class.") || name.startswith("struct.")) )
+			continue;
+
+		funcOnRecord(namedNode, name);
+	}
 }
 
 }//namespace cheerp
