ead2ac341c7e854e7538e977bd4d3471cfc30618
Alessandro Pignotti <a.pignotti@sssup.it>
2020-01-10 21:22:34 +0000

    Adapt to new bitcast stripping helper

--- llvm/lib/IR/Value.cpp
+++ llvm/lib/IR/Value.cpp
@@ -706,7 +706,7 @@ const Value *Value::stripInBoundsConstantOffsets() const {
 }
 
 const Value *Value::stripPointerCastsForAliasAnalysis() const {
-  return stripPointerCastsAndOffsets<PSK_ForAliasAnalysis>(this);
+  return stripPointerCastsAndOffsets<PSK_ForAliasAnalysis>(this, false);
 }
 
 const Value *Value::stripAndAccumulateConstantOffsets(
