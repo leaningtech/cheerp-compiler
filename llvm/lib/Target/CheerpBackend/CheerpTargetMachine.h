81c7b16a9369ddaeb6722f73ad1499fb46a29ef7
Alessandro Pignotti <a.pignotti@sssup.it>
2020-01-21 09:36:03 +0000

    The base TargetMachine is not supported anymore (and will be removed soon)
    Add required boilerplate

--- llvm/lib/Target/CheerpBackend/CheerpTargetMachine.h
+++ llvm/lib/Target/CheerpBackend/CheerpTargetMachine.h
@@ -13,22 +13,42 @@
 #define _CHEERP_TARGETMACHINE_H
 
 #include "llvm/ADT/Optional.h"
-#include "llvm/Target/TargetMachine.h"
 #include "llvm/IR/DataLayout.h"
+#include "llvm/Target/TargetLowering.h"
+#include "llvm/Target/TargetMachine.h"
+#include "llvm/Target/TargetSubtargetInfo.h"
 
 namespace llvm {
 
 class formatted_raw_ostream;
 
-struct CheerpTargetMachine : public TargetMachine {
+class CheerpTargetLowering: public TargetLowering {
+public:
+  CheerpTargetLowering(const TargetMachine &TM) : TargetLowering(TM) {}
+};
+
+class CheerpSubtarget: public TargetSubtargetInfo {
+private:
+  CheerpTargetLowering targetLowering;
+public:
+  CheerpSubtarget(const TargetMachine &TM, const Target &T, const Triple& TT, StringRef CPU, StringRef FS) :
+        TargetSubtargetInfo(TT, CPU, FS, None, None, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr),
+        targetLowering(TM) { }
+  virtual const CheerpTargetLowering *getTargetLowering() const override;
+};
+
+struct CheerpTargetMachine : public LLVMTargetMachine {
   CheerpTargetMachine(const Target &T, const Triple& TT,
                    StringRef CPU, StringRef FS, const TargetOptions &Options,
                    Optional<Reloc::Model> RM, CodeModel::Model CM,
                    CodeGenOpt::Level OL)
-      : TargetMachine(T, "b-e-p:32:8:8-i1:8:8-i8:8:8-i16:8:8-i32:8:8-"
+      : LLVMTargetMachine(T, "b-e-p:32:8:8-i1:8:8-i8:8:8-i16:8:8-i32:8:8-"
                         "i64:8:8-f32:8:8-f64:8:8-"
                         "a0:0:8-f80:8:8-n8:8:8-S8",
-                      TT, CPU, FS, Options) { }
+                      TT, CPU, FS, Options, Reloc::PIC_, CM, OL),
+        subTargetInfo(*this, T, TT, CPU, FS) { }
+  CheerpSubtarget subTargetInfo;
+
 public:
   virtual bool addPassesToEmitFile(PassManagerBase &PM,
                                    raw_pwrite_stream &Out,
@@ -39,6 +59,7 @@ public:
                                    AnalysisID StopBefore,
                                    AnalysisID StopAfter,
                                    MachineFunctionInitializer* MFInit) override;
+  virtual const CheerpSubtarget* getSubtargetImpl(const Function &F) const override;
 };
 
 extern Target TheCheerpBackendTarget;
