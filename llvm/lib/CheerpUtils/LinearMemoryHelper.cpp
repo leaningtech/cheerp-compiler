54f8ed0574f1c65dc6fea5ea44613e499805ef7d
Yuri Iozzelli <y.iozzelli@gmail.com>
2020-01-28 15:47:06 +0100

    Decide where to compile functions bases on sets filled in GDA.
    The sets are filled based on the function section mostly, but it is now
    possible to compile some wasm/asm.js functions outside of the module, in
    js. They just need to be added to the outside set

--- llvm/lib/CheerpUtils/LinearMemoryHelper.cpp
+++ llvm/lib/CheerpUtils/LinearMemoryHelper.cpp
@@ -447,28 +447,28 @@ void LinearMemoryHelper::addFunctions()
 			asmjsFunctions_.push_back(wasmNullptr);
 	}
 
-	std::vector<llvm::Function*> unsorted;
-	for (auto& F: module.functions())
+	std::vector<const llvm::Function*> unsorted;
+	for (auto* F: globalDeps.insideModule())
 	{
-		if (F.getSection() != StringRef("asmjs"))
+		if (F->getSection() != StringRef("asmjs"))
 			continue;
 
 		// Do not add __wasm_nullptr twice.
-		if (mode == FunctionAddressMode::Wasm && F.getName() == StringRef(wasmNullptrName))
+		if (mode == FunctionAddressMode::Wasm && F->getName() == StringRef(wasmNullptrName))
 			continue;
 
 		// Adding empty functions here will only cause a crash later
-		if (F.empty())
+		if (F->empty())
 			continue;
 
 		// WebAssembly has some builtin functions (sqrt, abs, copysign, etc.)
 		// which should be omitted, and is therefore a subset of the asmjs
 		// function list.
-		if (mode == FunctionAddressMode::Wasm && isWasmIntrinsic(&F) && !F.hasAddressTaken()) {
+		if (mode == FunctionAddressMode::Wasm && isWasmIntrinsic(F) && !F->hasAddressTaken()) {
 			continue;
 		}
 
-		unsorted.push_back(&F);
+		unsorted.push_back(F);
 	}
 
 	// Sort the list of functions by their usage.
