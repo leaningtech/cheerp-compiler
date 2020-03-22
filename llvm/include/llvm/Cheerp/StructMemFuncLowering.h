2d0827b97c3d7e419c6427f13a7238e20b28c226
Carlo Piovesan <carlo.piovesan@gmail.com>
2020-03-22 22:24:09 +0100

    StructMemFunc: createGenericLoop as abstraction over Forward and Backward loops

--- llvm/include/llvm/Cheerp/StructMemFuncLowering.h
+++ llvm/include/llvm/Cheerp/StructMemFuncLowering.h
@@ -30,6 +30,8 @@ private:
 				Type* pointedType, Value* dst, Value* src, Value* elementsCount, uint32_t baseAlign);
 	void createForwardLoop(IRBuilder<>* IRB, BasicBlock* BB, BasicBlock* endLoop, BasicBlock* memfuncBody,
 				Type* pointedType, Value* dst, Value* src, Value* elementsCount, MODE mode, uint32_t baseAlign);
+	void createGenericLoop(IRBuilder<>* IRB, BasicBlock* BB, BasicBlock* endLoop, BasicBlock* memfuncBody,
+				Type* pointedType, Value* dst, Value* src, Value* elementsCount, MODE, uint32_t baseAlign, const bool isForward);
 	bool createLoops(BasicBlock& BB, BasicBlock* endLoop, Type* int32Type, Value* src, Value* dst, Value* size, Type* pointedType, MODE mode, uint32_t baseAlign);
 	static bool isDoubleAggregate(llvm::Type* t);
 	SmallVector<BasicBlock*, 10> basicBlocks;
