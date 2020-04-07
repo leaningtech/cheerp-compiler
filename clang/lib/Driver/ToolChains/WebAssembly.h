a89160e1dea7a5ea61adf72a7764e80f7b160b72
Alessandro Pignotti <a.pignotti@sssup.it>
2020-04-07 14:44:25 +0000

    Remove an unneeded override

--- clang/lib/Driver/ToolChains/WebAssembly.h
+++ clang/lib/Driver/ToolChains/WebAssembly.h
@@ -134,7 +134,6 @@ public:
   Cheerp(const Driver &D, const llvm::Triple& Triple,
          const llvm::opt::ArgList &Args);
 
-  bool IsUnwindTablesDefault() const override;
   bool isPICDefault() const override;
   bool isPIEDefault() const override;
   bool isPICDefaultForced() const override;
