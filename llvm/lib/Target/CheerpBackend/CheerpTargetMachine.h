cb395307d2b47437e31a817629a9bdbaed2590bd
Alessandro Pignotti <a.pignotti@sssup.it>
2020-04-21 17:31:52 +0200

    Align i24 integers to 1 byte
    i24 are generated by TypeOptimizer when merging short integers in genericjs struct
    without altering their size. Even in packed structs these fields cause unexpected
    padding to be added, possibly due to buggy code deep in LLVM. Lowering the alignment
    is a safe solution to avoid these problems.

--- llvm/lib/Target/CheerpBackend/CheerpTargetMachine.h
+++ llvm/lib/Target/CheerpBackend/CheerpTargetMachine.h
@@ -42,7 +42,7 @@ struct CheerpTargetMachine : public LLVMTargetMachine {
                    StringRef CPU, StringRef FS, const TargetOptions &Options,
                    Optional<Reloc::Model> RM, CodeModel::Model CM,
                    CodeGenOpt::Level OL)
-      : LLVMTargetMachine(T, "b-e-p:32:32:32-i1:8:8-i8:8:8-i16:16:16-i32:32:32-"
+      : LLVMTargetMachine(T, "b-e-p:32:32:32-i1:8:8-i8:8:8-i16:16:16-i24:8:8-i32:32:32-"
                         "i64:64:64-f32:32:32-f64:64:64-"
                         "a:0:32-f16:16:16-f32:32:32-f64:64:64-n8:16:32-S64",
                       TT, CPU, FS, Options, Reloc::PIC_, CM, OL),
