2691f955b8102a144136d1cdfcafdf3ff14c3ada
Carlo Piovesan <carlo.piovesan@gmail.com>
2020-05-06 16:36:12 +0200

    Revert "Temporarily work around a bug in the deterministic container"
    This reverts commit a44c37bdf9f54174ca6bf00764b641b0afc2ba99.

--- llvm/include/llvm/Cheerp/WasmWriter.h
+++ llvm/include/llvm/Cheerp/WasmWriter.h
@@ -289,7 +289,7 @@ private:
 public:
 	TeeLocals teeLocals;
 	std::vector<const llvm::Instruction*> deferred;
-	using DeterministicInstructionSet = llvm::DenseSet<const llvm::Instruction*>;
+	using DeterministicInstructionSet = cheerp::DeterministicUnorderedSet<const llvm::Instruction*, RestrictionsLifted::NoPointerStability>;
 	using InstructionToDependenciesMap = llvm::DenseMap<const llvm::Instruction*, DeterministicInstructionSet>;
 	InstructionToDependenciesMap memoryDependencies;
 	InstructionToDependenciesMap localsDependencies;
