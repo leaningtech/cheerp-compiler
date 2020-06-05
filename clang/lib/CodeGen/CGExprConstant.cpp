b5eb3c796527bb4bf769833b78ac081394eaa65c
Alessandro Pignotti <a.pignotti@sssup.it>
2020-06-05 23:51:29 +0200

    Increase robustness of constant generation

--- clang/lib/CodeGen/CGExprConstant.cpp
+++ clang/lib/CodeGen/CGExprConstant.cpp
@@ -1986,7 +1986,9 @@ private:
     }
     }
 
-    C = llvm::ConstantExpr::getGetElementPtr(C->getType()->getPointerElementType(), C, Indexes);
+    assert(!Indexes.empty());
+    if(Indexes.size() > 1)
+      C = llvm::ConstantExpr::getGetElementPtr(C->getType()->getPointerElementType(), C, Indexes);
 
     if (OffsetVal == 0)
       return C;
