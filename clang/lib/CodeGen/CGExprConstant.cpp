e93c14bd1a6a9d481efa3b3f928bc99aa5af594d
Yuri Iozzelli <y.iozzelli@gmail.com>
2020-06-26 14:53:44 +0200

    HighInt: Emit i64 as not for memory when initializing variables of i64 type

--- clang/lib/CodeGen/CGExprConstant.cpp
+++ clang/lib/CodeGen/CGExprConstant.cpp
@@ -1767,6 +1767,10 @@ llvm::Constant *ConstantEmitter::tryEmitPrivateForVarInit(const VarDecl &D) {
   // Try to emit the initializer.  Note that this can allow some things that
   // are not allowed by tryEmitPrivateForMemory alone.
   if (auto value = D.evaluateValue()) {
+    if (CodeGenTypes::isHighInt(destType)) {
+      auto nonMemoryDestType = getNonMemoryType(CGM, destType);
+      return tryEmitPrivate(*value, nonMemoryDestType);
+    }
     return tryEmitPrivateForMemory(*value, destType);
   }
 
@@ -1785,6 +1789,8 @@ llvm::Constant *ConstantEmitter::tryEmitPrivateForVarInit(const VarDecl &D) {
   auto nonMemoryDestType = getNonMemoryType(CGM, destType);
   auto C =
     ConstExprEmitter(*this).Visit(const_cast<Expr*>(E), nonMemoryDestType);
+  if (CodeGenTypes::isHighInt(destType))
+    return C;
   return (C ? emitForMemory(C, destType) : nullptr);
 }
 
