592213b240fad71a991784c3426f0af335c58d26
Alessandro Pignotti <a.pignotti@sssup.it>
2020-07-03 22:30:31 +0200

    Disable emission of type-unsafe constants just to save some compile-time memory

--- clang/lib/CodeGen/CGExprConstant.cpp
+++ clang/lib/CodeGen/CGExprConstant.cpp
@@ -990,7 +990,7 @@ EmitArrayConstant(CodeGenModule &CGM, llvm::ArrayType *DesiredType,
 
   // Add a zeroinitializer array filler if we have lots of trailing zeroes.
   unsigned TrailingZeroes = ArrayBound - NonzeroLength;
-  if (TrailingZeroes >= 8) {
+  if (CGM.getTarget().isByteAddressable() && TrailingZeroes >= 8) {
     assert(Elements.size() >= NonzeroLength &&
            "missing initializer for non-zero element");
 
