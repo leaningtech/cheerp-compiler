ca2af586873d6682bce0e53ac4aa015649730b60
Carlo Piovesan <carlo.piovesan@gmail.com>
2020-07-11 06:54:44 +0200

    Do not allow i64 instructions to be inlined in genericjs

--- llvm/lib/Analysis/InlineCost.cpp
+++ llvm/lib/Analysis/InlineCost.cpp
@@ -3039,6 +3039,8 @@ bool llvm::isInlineViableCheerp(Function &F, Function &Caller) {
         if (li->getType()->isPointerTy() && li->getType()->getPointerElementType()->isPointerTy())
           return false;
       }
+      if (II->getType()->isIntegerTy(64))
+	return false;
     }
   }
   return true;
