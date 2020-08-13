66c3a4f65c567f1d01f05b39df94140fa9720950
Alessandro Pignotti <a.pignotti@sssup.it>
2020-08-13 12:57:45 +0000

    Fix tests after rebase

--- clang/test/CodeGenCXX/debug-info-lambda.cpp
+++ clang/test/CodeGenCXX/debug-info-lambda.cpp
@@ -3,7 +3,7 @@
 
 // CHECK-LABEL: define{{.*}}lambda_in_func
 void lambda_in_func(int &ref) {
-  // CHECK: [[ref_slot:%.*]] = getelementptr inbounds %class.anon, %class.anon* {{.*}}, i32 0, i32 0, !dbg [[lambda_decl_loc:![0-9]+]]
+  // CHECK: [[ref_slot:%.*]] = getelementptr inbounds {{.*}}, i32 0, i32 0, !dbg [[lambda_decl_loc:![0-9]+]]
   // CHECK-NEXT: %1 = load i32*, i32** %ref.addr, align {{.*}}, !dbg [[capture_init_loc:![0-9]+]]
   // CHECK-NEXT: store i32* %1, i32** %0, align {{.*}}, !dbg [[lambda_decl_loc]]
   // CHECK-NEXT: call {{.*}}void {{.*}}, !dbg [[lambda_call_loc:![0-9]+]]
