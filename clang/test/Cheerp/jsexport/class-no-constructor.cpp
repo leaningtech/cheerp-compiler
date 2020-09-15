42a1e98cb69a1660175c34d65f4929cb6bde8944
Alessandro Pignotti <a.pignotti@sssup.it>
2020-09-15 17:53:34 +0200

    Add explicit wasm triple to jsexport tests

--- clang/test/Cheerp/jsexport/class-no-constructor.cpp
+++ clang/test/Cheerp/jsexport/class-no-constructor.cpp
@@ -1,4 +1,4 @@
-// RUN: not %clang_cc1 %s 2>&1 | FileCheck %s --check-prefix=CHECK-WASM
+// RUN: not %clang_cc1 -triple cheerp-leaningtech-webbrowser-wasm %s 2>&1 | FileCheck %s --check-prefix=CHECK-WASM
 // CHECK-WASM: error: 'wasm' and 'jsexport' attributes are not compatible
 
 // RUN: not %clang_cc1 -triple cheerp-leaningtech-webbrowser-genericjs %s 2>&1 | FileCheck %s --check-prefix=CHECK-GENERIC
