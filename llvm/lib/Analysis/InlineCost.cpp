57a27b051edc97f4dbd47f1c8e6688168e98b91b
Alessandro Pignotti <a.pignotti@sssup.it>
2020-10-06 10:38:29 +0200

    Bring back "Avoid too much inlining when internalizing functions", but only for JS output
    This is necessary to avoid excessive inlining in JS mode. Wasm engines are more
    tolerant to large functions

--- llvm/lib/Analysis/InlineCost.cpp
+++ llvm/lib/Analysis/InlineCost.cpp
@@ -1895,7 +1895,7 @@ void InlineCostCallAnalyzer::updateThreshold(CallBase &Call, Function &Callee) {
   SingleBBBonus = Threshold * SingleBBBonusPercent / 100;
   VectorBonus = Threshold * VectorBonusPercent / 100;
 
-  bool OnlyOneCallAndLocalLinkage =
+  bool OnlyOneCallAndLocalLinkage = (DL.isByteAddressable() || F.getSection() == "asmjs") &&
       F.hasLocalLinkage() && F.hasOneLiveUse() && &F == Call.getCalledFunction();
   // If there is only one call of the function, and it has internal linkage,
   // the cost of inlining it drops dramatically. It may seem odd to update
@@ -2710,7 +2710,7 @@ InlineResult CallAnalyzer::analyze() {
     onBlockAnalyzed(BB);
   }
 
-  bool OnlyOneCallAndLocalLinkage = F.hasLocalLinkage() && F.hasOneLiveUse() &&
+  bool OnlyOneCallAndLocalLinkage = (DL.isByteAddressable() || F.getSection() == "asmjs") && F.hasLocalLinkage() && F.hasOneLiveUse() &&
                                     &F == CandidateCall.getCalledFunction();
   // If this is a noduplicate call, we can still inline as long as
   // inlining this would cause the removal of the caller (so the instruction
