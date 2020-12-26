2a065309c555a06ba359125b7bd7c612a0fa3aa5
Alessandro Pignotti <a.pignotti@sssup.it>
2020-12-26 18:19:40 +0000

    Adapt to CallBase changes

--- llvm/lib/Analysis/InlineCost.cpp
+++ llvm/lib/Analysis/InlineCost.cpp
@@ -2906,7 +2906,7 @@ Optional<InlineResult> llvm::getAttributeBasedInliningDecision(
                                      " address space");
     }
 
-  Function* caller=CS.getCaller();
+  Function* caller=Call.getCaller();
   if (!isInlineViableCheerp(*Callee, *caller))
     return llvm::InlineCost::getNever("not Cheerp viable");
 
