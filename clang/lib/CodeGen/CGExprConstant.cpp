4e9f67b072b3c9c6d3ff4c6235f9af3a5b7da96d
Alessandro Pignotti <a.pignotti@sssup.it>
2020-12-29 23:49:34 +0100

    LValue now direcly encodes type info

--- clang/lib/CodeGen/CGExprConstant.cpp
+++ clang/lib/CodeGen/CGExprConstant.cpp
@@ -1919,6 +1919,9 @@ private:
       CurType = D->getType();
     else if (const Expr *E = base.dyn_cast<const Expr*>())
       CurType = E->getType();
+    else
+      CurType = base.getTypeInfoType();
+
 
   llvm::Type* CurrentType = C->getType()->getPointerElementType();
 
