be4642d79d8e3e28b1c92bdea9bf4d84207b6466
Carlo Piovesan <carlo.piovesan@gmail.com>
2021-01-18 08:36:03 +0100

    NamespaceClient: Add checks for namespace client getters and setters both at definition and declarations

--- clang/lib/Sema/SemaDecl.cpp
+++ clang/lib/Sema/SemaDecl.cpp
@@ -9943,8 +9943,8 @@ Sema::ActOnFunctionDeclarator(Scope *S, Declarator &D, DeclContext *DC,
     }
   }
 
-  //Perform all checks related to functions and methods. Currently limited to [[cheerp::jsexport]] attribute but expansible
-  cheerp::checkFunction(NewFD, *this);
+  //Perform all cheerp specific checks related to functions and methods
+  cheerp::checkFunctionOnDeclaration(NewFD, *this);
 
   ProcessPragmaWeak(S, NewFD);
   checkAttributesAfterMerging(*this, *NewFD);
@@ -14347,6 +14347,10 @@ Decl *Sema::ActOnStartOfFunctionDef(Scope *FnBodyScope, Decl *D,
       InstantiateDefaultCtorDefaultArgs(Ctor);
     }
   }
+
+  //Perform all cheerp specific checks related to functions and methods that have a body
+  if (FD)
+    cheerp::checkFunctionOnDefinition(FD, *this);
 
   // See if this is a redefinition. If 'will have body' (or similar) is already
   // set, then these checks were already performed when it was set.
