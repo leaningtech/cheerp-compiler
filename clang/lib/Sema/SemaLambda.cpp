49aea4b85479c567a48021178bf2d45b56aa100c
Alessandro Pignotti <a.pignotti@sssup.it>
2021-01-19 12:17:15 +0000

    Adapt to API change

--- clang/lib/Sema/SemaLambda.cpp
+++ clang/lib/Sema/SemaLambda.cpp
@@ -255,9 +255,9 @@ Sema::createLambdaClosureType(SourceRange IntroducerRange, TypeSourceInfo *Info,
   // CHEERP: Inherit asmjs/genericjs attribute from the parent declaration
   if (Decl* d = dyn_cast<Decl>(DC)) {
     if (d->hasAttr<AsmJSAttr>())
-      Class->addAttr(AsmJSAttr::CreateImplicit(Context, AsmJSAttr::GNU_cheerp_asmjs));
+      Class->addAttr(AsmJSAttr::CreateImplicit(Context, IntroducerRange.getBegin(), AttributeCommonInfo::AS_GNU, AsmJSAttr::GNU_cheerp_asmjs));
     else if (d->hasAttr<GenericJSAttr>())
-      Class->addAttr(GenericJSAttr::CreateImplicit(Context, GenericJSAttr::GNU_cheerp_genericjs));
+      Class->addAttr(GenericJSAttr::CreateImplicit(Context, IntroducerRange.getBegin(), AttributeCommonInfo::AS_GNU, GenericJSAttr::GNU_cheerp_genericjs));
   }
 
   return Class;
