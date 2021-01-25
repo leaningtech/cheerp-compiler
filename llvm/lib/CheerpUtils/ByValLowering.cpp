cfd2520ce402f42ac7a7aa44ad7ecbcdcad0bb9c
Alessandro Pignotti <a.pignotti@sssup.it>
2021-01-25 11:45:00 +0000

    Adapt to API change

--- llvm/lib/CheerpUtils/ByValLowering.cpp
+++ llvm/lib/CheerpUtils/ByValLowering.cpp
@@ -47,7 +47,7 @@ static bool ExpandCall(const DataLayout& DL, CallInst* Call)
 					AllocAlignment = a.value();
 			}
 			// Make a copy of the byval argument.
-			Instruction *CopyBuf = new AllocaInst(ArgType, 0, 0, AllocAlignment,
+			Instruction *CopyBuf = new AllocaInst(ArgType, 0, 0, Alignment,
 				ArgPtr->getName() + ".byval_copy");
 			NumNewAllocas++;
 			Function *Func = Call->getParent()->getParent();
