leaningtech/cheerp-llvm

Disable another GEP optmization that loses type info

lib/Transforms/InstCombine/InstructionCombining.cpp
@@ -1332,7 +1332,7 @@ Instruction *InstCombiner::visitGetElementPtrInst(GetElementPtrInst &GEP) {
 
       // If this GEP instruction doesn't move the pointer, just replace the GEP
       // with a bitcast of the real input to the dest type.
-      if (!Offset) {
+      if (!Offset && TD->isByteAddressable()) {
         // If the bitcast is of an allocation, and the allocation will be
         // converted to match the type of the cast, don't touch this.
         if (isa<AllocaInst>(BCI->getOperand(0)) ||

leaningtech/cheerp-compiler

llvm/lib/Transforms/InstCombine/InstructionCombining.cpp
@@ -2402,7 +2402,7 @@ Instruction *InstCombinerImpl::visitGetElementPtrInst(GetElementPtrInst &GEP) {
     if (!isa<BitCastInst>(SrcOp) && GEP.accumulateConstantOffset(DL, Offset)) {
       // If this GEP instruction doesn't move the pointer, just replace the GEP
       // with a bitcast of the real input to the dest type.
-      if (!Offset) {
+      if (!Offset && DL->isByteAddressable()) {
         // If the bitcast is of an allocation, and the allocation will be
         // converted to match the type of the cast, don't touch this.
         if (isa<AllocaInst>(SrcOp) || isAllocationFn(SrcOp, &TLI)) {