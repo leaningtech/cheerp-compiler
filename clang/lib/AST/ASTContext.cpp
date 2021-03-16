leaningtech/cheerp-clang

Introduce the HighInt flag to BuiltinType
It is used to support 64-bit integers represented as a pair of 32-bit integres

lib/AST/ASTContext.cpp
@@ -910,7 +910,8 @@ TypeDecl *ASTContext::getFloat128StubType() const {
 }
 
 void ASTContext::InitBuiltinType(CanQualType &R, BuiltinType::Kind K) {
-  BuiltinType *Ty = new (*this, TypeAlignment) BuiltinType(K);
+  bool HighInt = ((K == BuiltinType::LongLong || K == BuiltinType::ULongLong) && !this->Target->isByteAddressable()) ? true : false;
+  BuiltinType *Ty = new (*this, TypeAlignment) BuiltinType(K, HighInt);
   R = CanQualType::CreateUnsafe(QualType(Ty, 0));
   Types.push_back(Ty);
 }

leaningtech/cheerp-compiler

clang/lib/AST/ASTContext.cpp
@@ -1249,7 +1249,8 @@ TypedefDecl *ASTContext::getUInt128Decl() const {
 }
 
 void ASTContext::InitBuiltinType(CanQualType &R, BuiltinType::Kind K) {
-  auto *Ty = new (*this, TypeAlignment) BuiltinType(K);
+  bool HighInt = ((K == BuiltinType::LongLong || K == BuiltinType::ULongLong) && !this->Target->isByteAddressable()) ? true : false;
+  auto *Ty = new (*this, TypeAlignment) BuiltinType(K, HighInt);
   R = CanQualType::CreateUnsafe(QualType(Ty, 0));
   Types.push_back(Ty);
 }