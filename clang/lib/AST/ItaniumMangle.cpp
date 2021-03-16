leaningtech/cheerp-clang

Add an API to mangle type names

lib/AST/ItaniumMangle.cpp
@@ -151,6 +151,7 @@ public:
                      raw_ostream &);
   void mangleCXXDtor(const CXXDestructorDecl *D, CXXDtorType Type,
                      raw_ostream &);
+  void mangleType(QualType Ty, raw_ostream &);
 
   void mangleItaniumGuardVariable(const VarDecl *D, raw_ostream &);
   void mangleItaniumThreadLocalInit(const VarDecl *D, raw_ostream &);
@@ -3641,6 +3642,11 @@ void ItaniumMangleContext::mangleCXXDtor(const CXXDestructorDecl *D,
                                          raw_ostream &Out) {
   CXXNameMangler Mangler(*this, Out, D, Type);
   Mangler.mangle(D);
+}
+
+void ItaniumMangleContext::mangleType(QualType Ty, raw_ostream &Out) {
+  CXXNameMangler Mangler(*this, Out);
+  Mangler.mangleType(Ty);
 }
 
 void ItaniumMangleContext::mangleThunk(const CXXMethodDecl *MD,

leaningtech/cheerp-compiler

clang/lib/AST/ItaniumMangle.cpp
@@ -153,6 +153,7 @@ public:
   void mangleCXXRTTI(QualType T, raw_ostream &) override;
   void mangleCXXRTTIName(QualType T, raw_ostream &) override;
   void mangleTypeName(QualType T, raw_ostream &) override;
+  void mangleType(QualType Ty, raw_ostream &) override;
 
   void mangleCXXCtorComdat(const CXXConstructorDecl *D, raw_ostream &) override;
   void mangleCXXDtorComdat(const CXXDestructorDecl *D, raw_ostream &) override;
@@ -5179,6 +5180,11 @@ void ItaniumMangleContextImpl::mangleCXXDtorComdat(const CXXDestructorDecl *D,
                                                    raw_ostream &Out) {
   CXXNameMangler Mangler(*this, Out, D, Dtor_Comdat);
   Mangler.mangle(GlobalDecl(D, Dtor_Comdat));
+}
+
+void ItaniumMangleContextImpl::mangleType(QualType Ty, raw_ostream &Out) {
+  CXXNameMangler Mangler(*this, Out);
+  Mangler.mangleType(Ty);
 }
 
 void ItaniumMangleContextImpl::mangleThunk(const CXXMethodDecl *MD,