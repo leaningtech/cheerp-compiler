leaningtech/cheerp-clang

Require classes in the client namespace to derive from a class with the client_layout attribute, or have it themselves.

lib/Sema/SemaDeclAttr.cpp
@@ -5979,6 +5979,10 @@ static void handleDefaultNewAttr(Sema &S, Decl *D, const AttributeList &Attr) {
   D->addAttr(::new (S.Context) DefaultNewAttr(Attr.getRange(), S.Context, Attr.getAttributeSpellingListIndex()));
 }
 
+static void handleClientLayoutAttr(Sema &S, Decl *D, const AttributeList &Attr) {
+  D->addAttr(::new (S.Context) ClientLayoutAttr(Attr.getRange(), S.Context, Attr.getAttributeSpellingListIndex()));
+}
+
 //===----------------------------------------------------------------------===//
 // Top Level Sema Entry Points
 //===----------------------------------------------------------------------===//
@@ -6649,6 +6653,9 @@ static void ProcessDeclAttribute(Sema &S, Scope *scope, Decl *D,
     break;
   case AttributeList::AT_DefaultNew:
     handleDefaultNewAttr(S, D, Attr);
+    break;
+  case AttributeList::AT_ClientLayout:
+    handleClientLayoutAttr(S, D, Attr);
     break;
   }
 }

leaningtech/cheerp-compiler

clang/lib/Sema/SemaDeclAttr.cpp
@@ -6842,6 +6842,10 @@ static void handleDefaultNewAttr(Sema &S, Decl *D, const AttributeList &Attr) {
   D->addAttr(::new (S.Context) DefaultNewAttr(Attr.getRange(), S.Context, Attr.getAttributeSpellingListIndex()));
 }
 
+static void handleClientLayoutAttr(Sema &S, Decl *D, const AttributeList &Attr) {
+  D->addAttr(::new (S.Context) ClientLayoutAttr(Attr.getRange(), S.Context, Attr.getAttributeSpellingListIndex()));
+}
+
 //===----------------------------------------------------------------------===//
 // Top Level Sema Entry Points
 //===----------------------------------------------------------------------===//
@@ -7537,6 +7541,9 @@ static void ProcessDeclAttribute(Sema &S, Scope *scope, Decl *D,
     break;
   case AttributeList::AT_DefaultNew:
     handleDefaultNewAttr(S, D, AL);
+    break;
+  case AttributeList::AT_ClientLayout:
+    handleClientLayoutAttr(S, D, AL);
     break;
   }
 }