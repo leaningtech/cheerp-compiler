leaningtech/cheerp-clang

Keep track of the base offsets for each base
And use it for metadata in downcasting

lib/CodeGen/CGClass.cpp
@@ -52,6 +52,34 @@ ComputeNonVirtualBaseClassGepPath(CodeGenTypes& Types,
   }
 }
 
+static unsigned ComputeBaseIdOffset(CodeGenTypes& Types,
+                                 const CXXRecordDecl *DerivedClass,
+                                 CastExpr::path_const_iterator Start,
+                                 CastExpr::path_const_iterator End) {
+  unsigned Offset=0;
+
+  const CXXRecordDecl *RD = DerivedClass;
+
+  for (CastExpr::path_const_iterator I = Start; I != End; ++I) {
+    const CXXBaseSpecifier *Base = *I;
+    assert(!Base->isVirtual() && "Should not see virtual bases here!");
+
+    const CXXRecordDecl *BaseDecl =
+      cast<CXXRecordDecl>(Base->getType()->getAs<RecordType>()->getDecl());
+
+    // Get the layout.
+    const CGRecordLayout &Layout = Types.getCGRecordLayout(RD);
+
+    unsigned baseId = Layout.getNonVirtualBaseLLVMFieldNo(BaseDecl);
+
+    Offset += Layout.getTotalOffsetToBase(baseId);
+
+    RD = BaseDecl;
+  }
+
+  return Offset;
+}
+
 static CharUnits 
 ComputeNonVirtualBaseClassOffset(ASTContext &Context, 
                                  const CXXRecordDecl *DerivedClass,
@@ -298,8 +326,8 @@ CodeGenFunction::GetAddressOfDerivedClass(llvm::Value *Value,
 
   llvm::Value *NonVirtualOffset =
     CGM.GetNonVirtualBaseClassOffset(Derived, PathBegin, PathEnd);
-  
-  if (!NonVirtualOffset) {
+
+  if (!NonVirtualOffset && getTarget().isByteAddressable()) {
     // No offset, we can just cast back.
     return Builder.CreateBitCast(Value, DerivedPtrTy);
   }
@@ -318,13 +346,26 @@ CodeGenFunction::GetAddressOfDerivedClass(llvm::Value *Value,
     EmitBlock(CastNotNull);
   }
   
-  // Apply the offset.
-  Value = Builder.CreateBitCast(Value, Int8PtrTy);
-  Value = Builder.CreateGEP(Value, Builder.CreateNeg(NonVirtualOffset),
+  if (!getTarget().isByteAddressable())
+  {
+    unsigned BaseIdOffset = ComputeBaseIdOffset(getTypes(), Derived, PathBegin, PathEnd);
+    llvm::Constant* baseOffset = llvm::ConstantInt::get(PtrDiffTy, BaseIdOffset);
+    Value=Builder.CreateBitCast(Value, DerivedPtrTy);
+    llvm::SmallVector<llvm::Value*, 1> tmp;
+    tmp.push_back(baseOffset);
+    llvm::MDNode* md=llvm::MDNode::get(getTypes().getLLVMContext(), tmp);
+    //The builder returns values, but we know it's an instruction
+    cast<llvm::Instruction>(Value)->setMetadata("duetto.downcast", md);
+  }
+  else
+  {
+    // Apply the offset.
+    Value = Builder.CreateBitCast(Value, Int8PtrTy);
+    Value = Builder.CreateGEP(Value, Builder.CreateNeg(NonVirtualOffset),
                             "sub.ptr");
-
-  // Just cast.
-  Value = Builder.CreateBitCast(Value, DerivedPtrTy);
+    // Just cast.
+    Value = Builder.CreateBitCast(Value, DerivedPtrTy);
+  }
 
   if (NullCheckValue) {
     Builder.CreateBr(CastEnd);

leaningtech/cheerp-compiler

clang/lib/CodeGen/CGClass.cpp
@@ -58,6 +58,34 @@ ComputeNonVirtualBaseClassGepPath(CodeGenTypes& Types,
   }
 }
 
+static unsigned ComputeBaseIdOffset(CodeGenTypes& Types,
+                                 const CXXRecordDecl *DerivedClass,
+                                 CastExpr::path_const_iterator Start,
+                                 CastExpr::path_const_iterator End) {
+  unsigned Offset=0;
+
+  const CXXRecordDecl *RD = DerivedClass;
+
+  for (CastExpr::path_const_iterator I = Start; I != End; ++I) {
+    const CXXBaseSpecifier *Base = *I;
+    assert(!Base->isVirtual() && "Should not see virtual bases here!");
+
+    const CXXRecordDecl *BaseDecl =
+      cast<CXXRecordDecl>(Base->getType()->getAs<RecordType>()->getDecl());
+
+    // Get the layout.
+    const CGRecordLayout &Layout = Types.getCGRecordLayout(RD);
+
+    unsigned baseId = Layout.getNonVirtualBaseLLVMFieldNo(BaseDecl);
+
+    Offset += Layout.getTotalOffsetToBase(baseId);
+
+    RD = BaseDecl;
+  }
+
+  return Offset;
+}
+
 /// Return the best known alignment for an unknown pointer to a
 /// particular class.
 CharUnits CodeGenModule::getClassPointerAlignment(const CXXRecordDecl *RD) {
@@ -471,7 +499,7 @@ CodeGenFunction::GetAddressOfDerivedClass(Address BaseAddr,
   llvm::Value *NonVirtualOffset =
     CGM.GetNonVirtualBaseClassOffset(Derived, PathBegin, PathEnd);
 
-  if (!NonVirtualOffset) {
+  if (!NonVirtualOffset && getTarget().isByteAddressable()) {
     // No offset, we can just cast back.
     return Builder.CreateBitCast(BaseAddr, DerivedPtrTy);
   }
@@ -490,13 +518,27 @@ CodeGenFunction::GetAddressOfDerivedClass(Address BaseAddr,
     EmitBlock(CastNotNull);
   }
 
-  // Apply the offset.
-  llvm::Value *Value = Builder.CreateBitCast(BaseAddr.getPointer(), Int8PtrTy);
-  Value = Builder.CreateInBoundsGEP(Value, Builder.CreateNeg(NonVirtualOffset),
+  if (!getTarget().isByteAddressable())
+  {
+    unsigned BaseIdOffset = ComputeBaseIdOffset(getTypes(), Derived, PathBegin, PathEnd);
+    llvm::Constant* baseOffset = llvm::ConstantInt::get(PtrDiffTy, BaseIdOffset);
+    Value=Builder.CreateBitCast(Value, DerivedPtrTy);
+    llvm::SmallVector<llvm::Metadata*, 1> tmp;
+    tmp.push_back(llvm::ConstantAsMetadata::get(baseOffset));
+    llvm::MDNode* md=llvm::MDNode::get(getLLVMContext(), tmp);
+    //The builder returns values, but we know it's an instruction
+    cast<llvm::Instruction>(Value)->setMetadata("duetto.downcast", md);
+  }
+  else
+  {
+    // Apply the offset.
+    llvm::Value *Value = Builder.CreateBitCast(BaseAddr.getPointer(), Int8PtrTy);
+    Value = Builder.CreateInBoundsGEP(Value, Builder.CreateNeg(NonVirtualOffset),
                                     "sub.ptr");
 
-  // Just cast.
-  Value = Builder.CreateBitCast(Value, DerivedPtrTy);
+    // Just cast.
+    Value = Builder.CreateBitCast(Value, DerivedPtrTy);
+  }
 
   // Produce a PHI if we had a null-check.
   if (NullCheckValue) {