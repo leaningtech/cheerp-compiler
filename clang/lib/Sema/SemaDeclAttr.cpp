leaningtech/cheerp-clang

Fixups for issues caused by rebasing

lib/Sema/SemaDeclAttr.cpp
@@ -4046,8 +4046,12 @@ static void ProcessDeclAttribute(Sema &S, Scope *scope, Decl *D,
 
   // Ignore C++11 attributes on declarator chunks: they appertain to the type
   // instead.
-  if (Attr.isCXX11Attribute() && !IncludeCXX11Attributes)
+  if (Attr.isCXX11Attribute() && !IncludeCXX11Attributes &&
+      Attr.getKind()!=AttributeList::AT_Server &&
+      Attr.getKind()!=AttributeList::AT_Client)
+  {
     return;
+  }
 
   // Unknown attributes are automatically warned on. Target-specific attributes
   // which do not apply to the current target architecture are treated as

leaningtech/cheerp-compiler

clang/lib/Sema/SemaDeclAttr.cpp
@@ -6810,8 +6810,12 @@ static void ProcessDeclAttribute(Sema &S, Scope *scope, Decl *D,
 
   // Ignore C++11 attributes on declarator chunks: they appertain to the type
   // instead.
-  if (AL.isCXX11Attribute() && !IncludeCXX11Attributes)
+  if (AL.isCXX11Attribute() && !IncludeCXX11Attributes &&
+      Attr.getKind()!=AttributeList::AT_Server &&
+      Attr.getKind()!=AttributeList::AT_Client)
+  {
     return;
+  }
 
   // Unknown attributes are automatically warned on. Target-specific attributes
   // which do not apply to the current target architecture are treated as