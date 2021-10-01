a49b9971ab1ffbf41f217a92a40a8d1730774618
Alessandro Pignotti <a.pignotti@sssup.it>
2021-10-01 19:44:18 +0200

    Generalize struct support in unions
    Now not only anonymous struct, but any struct in a union is considered bytelayout

--- clang/lib/AST/Decl.cpp
+++ clang/lib/AST/Decl.cpp
@@ -4769,7 +4769,7 @@ bool RecordDecl::isByteLayout() const
     return true;
   // Unions and anonymous structures inside unions use bytelayout
   const RecordDecl *CurDecl = this;
-  while (CurDecl && (CurDecl->isUnion() || CurDecl->isAnonymousStructOrUnion()))
+  while (CurDecl)
   {
     if (CurDecl->isUnion())
       return true;
