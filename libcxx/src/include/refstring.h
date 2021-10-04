5701aadb7f5a9e36ccde25361100fe3ac0d8cce9
Alessandro Pignotti <a.pignotti@sssup.it>
2021-10-04 23:33:12 +0200

    libcxx: Implement a type safe solution for exception strings ref-counting

--- libcxx/src/include/refstring.h
+++ libcxx/src/include/refstring.h
@@ -35,22 +35,9 @@
 _LIBCPP_BEGIN_NAMESPACE_STD
 
 namespace __refstring_imp { namespace {
-typedef int count_t;
-
-struct _Rep_base {
-    std::size_t len;
-    std::size_t cap;
-    count_t     count;
-};
-
-inline _Rep_base* rep_from_data(const char *data_) noexcept {
-    char *data = const_cast<char *>(data_);
-    return reinterpret_cast<_Rep_base *>(data - sizeof(_Rep_base));
-}
 
 inline char * data_from_rep(_Rep_base *rep) noexcept {
-    char *data = reinterpret_cast<char *>(rep);
-    return data + sizeof(*rep);
+    return rep->data;
 }
 
 #if defined(_LIBCPP_CHECK_FOR_GCC_EMPTY_STRING_STORAGE)
@@ -82,13 +69,13 @@ using namespace __refstring_imp;
 inline
 __libcpp_refstring::__libcpp_refstring(const char* msg) {
     std::size_t len = strlen(msg);
-    _Rep_base* rep = static_cast<_Rep_base *>(::operator new(sizeof(*rep) + len + 1));
+    _Rep_base* rep = static_cast<_Rep_base *>(malloc(sizeof(*rep) + len + 1));
     rep->len = len;
     rep->cap = len;
     rep->count = 0;
     char *data = data_from_rep(rep);
     std::memcpy(data, msg, len + 1);
-    __imp_ = data;
+    __imp_ = rep;
 }
 
 inline
@@ -96,21 +83,21 @@ __libcpp_refstring::__libcpp_refstring(const __libcpp_refstring &s) noexcept
     : __imp_(s.__imp_)
 {
     if (__uses_refcount())
-        __libcpp_atomic_add(&rep_from_data(__imp_)->count, 1);
+        __libcpp_atomic_add(&__imp_->count, 1);
 }
 
 inline
 __libcpp_refstring& __libcpp_refstring::operator=(__libcpp_refstring const& s) noexcept {
     bool adjust_old_count = __uses_refcount();
-    struct _Rep_base *old_rep = rep_from_data(__imp_);
+    struct _Rep_base *old_rep = __imp_;
     __imp_ = s.__imp_;
     if (__uses_refcount())
-        __libcpp_atomic_add(&rep_from_data(__imp_)->count, 1);
+        __libcpp_atomic_add(&__imp_->count, 1);
     if (adjust_old_count)
     {
         if (__libcpp_atomic_add(&old_rep->count, count_t(-1)) < 0)
         {
-            ::operator delete(old_rep);
+            free(old_rep);
         }
     }
     return *this;
@@ -119,9 +106,9 @@ __libcpp_refstring& __libcpp_refstring::operator=(__libcpp_refstring const& s) n
 inline
 __libcpp_refstring::~__libcpp_refstring() {
     if (__uses_refcount()) {
-        _Rep_base* rep = rep_from_data(__imp_);
+        _Rep_base* rep = __imp_;
         if (__libcpp_atomic_add(&rep->count, count_t(-1)) < 0) {
-            ::operator delete(rep);
+            free(rep);
         }
     }
 }
