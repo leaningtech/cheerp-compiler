9906f585db6b62b5aae7646af4597f8a16f02d34
Carlo Piovesan <piovesan.carlo@gmail.com>
2021-10-06 13:06:50 +0200

    SemaCheerp: Test on templated function

---
+++ clang/test/Cheerp/client/template-invalid.cpp
@@ -0,0 +1,10 @@
+// RUN: not %clang_cc1 %s 2>&1 | FileCheck %s
+// RUN: not %clang_cc1 -triple cheerp-leaningtech-webbrowser-genericjs %s 2>&1 | FileCheck %s
+
+//CHECK: error: Cheerp: Functions or methods forward declared in namespace client can not have pointers to base types as parameter type
+
+namespace [[cheerp::genericjs]] client
+{
+	template <class C>
+	void some_func(C *, int* Z);
+} //namespace client
