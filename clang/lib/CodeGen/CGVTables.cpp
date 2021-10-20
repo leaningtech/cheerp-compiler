leaningtech/cheerp-clang

Store the vcall and vbase offsets in bytes in asmjs/wasm

lib/CodeGen/CGVTables.cpp
@@ -587,9 +587,11 @@ llvm::Constant *CodeGenVTables::CreateVTableInitializer(
 
     switch (Component.getKind()) {
     case VTableComponent::CK_VCallOffset:
-      if(!CGM.getTarget().isByteAddressable() && !asmjs) {
+      if(!CGM.getTarget().isByteAddressable()) {
         int32_t Offset = 0;
-        if (LayoutClass != Component.getVCall() && Component.getVCall() != nullptr)
+        if(asmjs)
+          Offset = Component.getVCallOffset().getQuantity();
+        else if (LayoutClass != Component.getVCall() && Component.getVCall() != nullptr)
          Offset =  CGM.ComputeVirtualBaseIdOffset(LayoutClass, Component.getVCall());
         Init = llvm::ConstantInt::get(PtrDiffTy, Offset);
       } else {
@@ -599,8 +601,12 @@ llvm::Constant *CodeGenVTables::CreateVTableInitializer(
       }
       break;
     case VTableComponent::CK_VBaseOffset:
-      if(!CGM.getTarget().isByteAddressable() && !asmjs) {
-        int32_t Offset = CGM.ComputeVirtualBaseIdOffset(LayoutClass, Component.getVBase());
+      if(!CGM.getTarget().isByteAddressable()) {
+        int32_t Offset;
+        if(asmjs)
+          Offset = Component.getVBaseOffset().getQuantity();
+        else
+          Offset = CGM.ComputeVirtualBaseIdOffset(LayoutClass, Component.getVBase());
         Init = llvm::ConstantInt::get(PtrDiffTy, Offset);
       } else {
         Init = llvm::ConstantInt::get(PtrDiffTy,

leaningtech/cheerp-compiler

clang/lib/CodeGen/CGVTables.cpp
@@ -767,17 +767,23 @@ void CodeGenVTables::addVTableComponent(AggregateBuilderPublic &builder,
 
   switch (component.getKind()) {
   case VTableComponent::CK_VCallOffset:
-    if(!CGM.getTarget().isByteAddressable() && !asmjs) {
+    if(!CGM.getTarget().isByteAddressable()) {
       int32_t Offset = 0;
-      if (LayoutClass != component.getVCall() && component.getVCall() != nullptr)
+      if(asmjs)
+        Offset = component.getVCallOffset().getQuantity();
+      else if (LayoutClass != component.getVCall() && component.getVCall() != nullptr)
         Offset =  CGM.ComputeVirtualBaseIdOffset(LayoutClass, component.getVCall());
       return builder.addInt(CGM.PtrDiffTy, Offset);
     }
     return addOffsetConstant(CGM, builder, component.getVCallOffset());
 
   case VTableComponent::CK_VBaseOffset:
-    if(!CGM.getTarget().isByteAddressable() && !asmjs) {
-      int32_t Offset = CGM.ComputeVirtualBaseIdOffset(LayoutClass, component.getVBase());
+    if(!CGM.getTarget().isByteAddressable()) {
+      int32_t Offset;
+      if(asmjs)
+        Offset = component.getVBaseOffset().getQuantity();
+      else
+        Offset = CGM.ComputeVirtualBaseIdOffset(LayoutClass, component.getVBase());
       return builder.addInt(CGM.PtrDiffTy, Offset);
     }
     return addOffsetConstant(CGM, builder, component.getVBaseOffset());