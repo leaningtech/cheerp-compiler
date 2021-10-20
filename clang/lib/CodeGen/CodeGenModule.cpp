leaningtech/cheerp-clang

Fix warnings on duetto safe casts

lib/CodeGen/CodeGenModule.cpp
@@ -2149,11 +2149,10 @@ void CodeGenModule::EmitGlobalFunctionDefinition(GlobalDecl GD) {
   }
 }
 
-llvm::Function* CodeGenModule::GetUserCastIntrinsic(SourceLocation srcLoc, QualType SrcTy, QualType DestTy)
+llvm::Function* CodeGenModule::GetUserCastIntrinsic(const CastExpr* CE, QualType SrcTy, QualType DestTy)
 {
-  //Do not print the waring when casting function pointers
-  if(!(SrcTy->isFunctionPointerType() && DestTy->isFunctionPointerType()))
-    getDiags().Report(srcLoc, diag::warn_duetto_unsafe_cast);
+  if(!CE->isDuettoSafe())
+    getDiags().Report(CE->getLocStart(), diag::warn_duetto_unsafe_cast);
 
   llvm::Type* types[] = { getTypes().ConvertType(DestTy), getTypes().ConvertType(SrcTy) };
 

leaningtech/cheerp-compiler

clang/lib/CodeGen/CodeGenModule.cpp
@@ -4912,11 +4912,10 @@ void CodeGenModule::EmitGlobalFunctionDefinition(GlobalDecl GD,
     AddGlobalDtor(Fn, DA->getPriority(), true);
 }
 
-llvm::Function* CodeGenModule::GetUserCastIntrinsic(SourceLocation srcLoc, QualType SrcTy, QualType DestTy)
+llvm::Function* CodeGenModule::GetUserCastIntrinsic(const CastExpr* CE, QualType SrcTy, QualType DestTy)
 {
-  //Do not print the waring when casting function pointers
-  if(!(SrcTy->isFunctionPointerType() && DestTy->isFunctionPointerType()))
-    getDiags().Report(srcLoc, diag::warn_duetto_unsafe_cast);
+  if(!CE->isDuettoSafe())
+    getDiags().Report(CE->getBeginLoc(), diag::warn_duetto_unsafe_cast);
 
   llvm::Type* types[] = { getTypes().ConvertType(DestTy), getTypes().ConvertType(SrcTy) };
 