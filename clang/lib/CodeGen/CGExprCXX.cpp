leaningtech/cheerp-clang

Fix support for nothrow new

lib/CodeGen/CGExprCXX.cpp
@@ -1070,10 +1070,8 @@ static RValue EmitNewDeleteCall(CodeGenFunction &CGF,
       CalleeAddr = llvm::Intrinsic::getDeclaration(&CGF.CGM.getModule(),
                                   llvm::Intrinsic::cheerp_deallocate);
     }
-    RV =
-      CGF.EmitCall(CGF.CGM.getTypes().arrangeFreeFunctionCall(retType, Args, FunctionType::ExtInfo(), RequiredArgs::All),
-                   CalleeAddr, ReturnValueSlot(), Args,
-                   Callee, &CallOrInvoke);
+    llvm::Value* SizeArg[] = { Args[0].RV.getScalarVal() };
+    RV = RValue::get(CGF.Builder.CreateCall(CalleeAddr, SizeArg));
   }
   else
   {

leaningtech/cheerp-compiler

clang/lib/CodeGen/CGExprCXX.cpp
@@ -1351,10 +1351,8 @@ static RValue EmitNewDeleteCall(CodeGenFunction &CGF,
       intrinsic = llvm::Intrinsic::getDeclaration(&CGF.CGM.getModule(),
                                   llvm::Intrinsic::cheerp_deallocate);
     }
-    CGCallee Callee = CGCallee::forDirect(intrinsic, GlobalDecl(CalleeDecl));
-    RV =
-      CGF.EmitCall(CGF.CGM.getTypes().arrangeFreeFunctionCall(retType, Args, FunctionType::ExtInfo(), RequiredArgs::All),
-                   Callee, ReturnValueSlot(), Args, &CallOrInvoke);
+    llvm::Value* SizeArg[] = { Args[0].RV.getScalarVal() };
+    RV = RValue::get(CGF.Builder.CreateCall(intrinsic, SizeArg));
   }
   else
   {