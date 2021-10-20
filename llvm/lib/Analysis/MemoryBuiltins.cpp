leaningtech/cheerp-llvm

Directly handle cheerp_allocate_array in the writer. remove the AllocateArrayLowering pass

lib/Analysis/MemoryBuiltins.cpp
@@ -54,6 +54,7 @@ struct AllocFnsTy {
 static const AllocFnsTy AllocationFnData[] = {
   // Keep this one as the first one
   {LibFunc::cheerp_allocate,     CallocLike,  1, 0,  -1},
+  {LibFunc::cheerp_allocate_array,CallocLike,  1, 0,  -1},
   {LibFunc::malloc,              MallocLike,  1, 0,  -1},
   {LibFunc::valloc,              MallocLike,  1, 0,  -1},
   {LibFunc::Znwj,                OpNewLike,   1, 0,  -1}, // new(unsigned int)
@@ -98,7 +99,8 @@ static const AllocFnsTy *getAllocationData(const Value *V, AllocType AllocTy,
   // Skip all intrinsics but cheerp.allocate
   if (const IntrinsicInst* II = dyn_cast<IntrinsicInst>(V))
   {
-    if (II->getIntrinsicID() == Intrinsic::cheerp_allocate)
+    if (II->getIntrinsicID() == Intrinsic::cheerp_allocate ||
+        II->getIntrinsicID() == Intrinsic::cheerp_allocate_array)
       return &AllocationFnData[0];
     return nullptr;
   }

leaningtech/cheerp-compiler

llvm/lib/Analysis/MemoryBuiltins.cpp
@@ -73,6 +73,7 @@ struct AllocFnsTy {
 static const std::pair<LibFunc, AllocFnsTy> AllocationFnData[] = {
   // Keep this one as the first one
   {LibFunc_cheerp_allocate,     {CallocLike,  1, 0,  -1}},
+  {LibFunc_cheerp_allocate_array,{CallocLike,  1, 0,  -1}},
   {LibFunc_malloc,              {MallocLike,  1, 0,  -1}},
   {LibFunc_vec_malloc,          {MallocLike,  1, 0,  -1}},
   {LibFunc_valloc,              {MallocLike,  1, 0,  -1}},
@@ -143,7 +144,8 @@ static Optional<AllocFnsTy>
 getAllocationDataForFunction(const Function *Callee, AllocType AllocTy,
                              const TargetLibraryInfo *TLI) {
 
-  if (Callee->getIntrinsicID() == Intrinsic::cheerp_allocate)
+  if (Callee->getIntrinsicID() == Intrinsic::cheerp_allocate ||
+      Callee->getIntrinsicID() == Intrinsic::cheerp_allocate_array)
     return &AllocationFnData[0];
 
   // Make sure that the function is available.