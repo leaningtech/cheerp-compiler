50e51e1b115d3db5482a0370aa05635fec01dd5b
Carlo Piovesan <piovesan.carlo@gmail.com>
2021-10-20 10:11:05 +0200

    cheerp::interface_name: Handling and basic checks in clang's Sema

--- clang/lib/Sema/SemaDeclAttr.cpp
+++ clang/lib/Sema/SemaDeclAttr.cpp
@@ -5992,6 +5992,15 @@ static void handleObjCBridgeAttr(Sema &S, Decl *D, const ParsedAttr &AL) {
   D->addAttr(::new (S.Context) ObjCBridgeAttr(S.Context, AL, Parm->Ident));
 }
 
+static void handleCheerpInterfaceNameAttr(Sema &S, Decl *D, const ParsedAttr &AL) {
+  StringRef Str;
+  SourceLocation ArgLoc;
+  if (!S.checkStringLiteralArgumentAttr(AL, 0, Str, &ArgLoc))
+    return;
+
+  D->addAttr(::new (S.Context) CheerpInterfaceNameAttr(S.Context, AL, Str));
+}
+
 static void handleObjCBridgeMutableAttr(Sema &S, Decl *D,
                                         const ParsedAttr &AL) {
   IdentifierLoc *Parm = AL.isArgIdent(0) ? AL.getArgAsIdent(0) : nullptr;
@@ -8569,6 +8578,9 @@ static void ProcessDeclAttribute(Sema &S, Scope *scope, Decl *D,
     break;
   case ParsedAttr::AT_ObjCBridge:
     handleObjCBridgeAttr(S, D, AL);
+    break;
+  case ParsedAttr::AT_CheerpInterfaceName:
+    handleCheerpInterfaceNameAttr(S, D, AL);
     break;
   case ParsedAttr::AT_ObjCBridgeMutable:
     handleObjCBridgeMutableAttr(S, D, AL);
