leaningtech/cheerp-clang

Define a new action for the final duetto-compiler step

lib/Driver/Action.cpp
@@ -34,6 +34,7 @@ const char *Action::getClassName(ActionClass AC) {
   case LipoJobClass: return "lipo";
   case DsymutilJobClass: return "dsymutil";
   case VerifyJobClass: return "verify";
+  case DuettoCompileJobClass: return "duetto-compiler";
   }
 
   llvm_unreachable("invalid class");
@@ -119,4 +120,10 @@ void VerifyJobAction::anchor() {}
 
 VerifyJobAction::VerifyJobAction(ActionList &Inputs, types::ID Type)
   : JobAction(VerifyJobClass, Inputs, Type) {
+}
+
+void DuettoCompileJobAction::anchor() {}
+
+DuettoCompileJobAction::DuettoCompileJobAction(ActionList &Inputs, types::ID Type)
+  : JobAction(DuettoCompileJobClass, Inputs, Type) {
 }

leaningtech/cheerp-compiler

clang/lib/Driver/Action.cpp
@@ -45,6 +45,7 @@ const char *Action::getClassName(ActionClass AC) {
     return "clang-offload-wrapper";
   case StaticLibJobClass:
     return "static-lib-linker";
+  case CheerpCompileJobClass: return "cheerp-compiler";
   }
 
   llvm_unreachable("invalid class");
@@ -422,3 +423,9 @@ void StaticLibJobAction::anchor() {}
 
 StaticLibJobAction::StaticLibJobAction(ActionList &Inputs, types::ID Type)
     : JobAction(StaticLibJobClass, Inputs, Type) {}
+
+void CheerpCompileJobAction::anchor() {}
+
+CheerpCompileJobAction::CheerpCompileJobAction(ActionList &Inputs, types::ID Type)
+  : JobAction(CheerpCompileJobClass, Inputs, Type) {
+}