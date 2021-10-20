leaningtech/cheerp-clang

Add `[[cheerp::genericjs]]` and `[[cheerp::asmjs]]` attributes, for setting globals and functions in the generic or asmjs section manually

lib/Sema/SemaDeclAttr.cpp
@@ -4302,6 +4302,14 @@ static void handleJsExportAttr(Sema &S, Decl *D, const AttributeList &Attr) {
     S.Diag(Attr.getLoc(), diag::warn_attribute_ignored) << Attr.getName();
 }
 
+static void handleAsmJSAttr(Sema &S, Decl *D, const AttributeList &Attr) {
+  D->addAttr(::new (S.Context) AsmJSAttr(Attr.getRange(), S.Context, Attr.getAttributeSpellingListIndex()));
+}
+
+static void handleGenericJSAttr(Sema &S, Decl *D, const AttributeList &Attr) {
+  D->addAttr(::new (S.Context) GenericJSAttr(Attr.getRange(), S.Context, Attr.getAttributeSpellingListIndex()));
+}
+
 //===----------------------------------------------------------------------===//
 // Top Level Sema Entry Points
 //===----------------------------------------------------------------------===//
@@ -4850,6 +4858,12 @@ static void ProcessDeclAttribute(Sema &S, Scope *scope, Decl *D,
   case AttributeList::AT_JsExport:
     checkCheerpUnprefixedDeprecations(S, Attr);
     handleJsExportAttr(S, D, Attr);
+    break;
+  case AttributeList::AT_AsmJS:
+    handleAsmJSAttr(S, D, Attr);
+    break;
+  case AttributeList::AT_GenericJS:
+    handleGenericJSAttr(S, D, Attr);
     break;
   }
 }

leaningtech/cheerp-compiler

clang/lib/Sema/SemaDeclAttr.cpp
@@ -7890,6 +7890,14 @@ static void handleJsExportAttr(Sema &S, Decl *D, const AttributeList &Attr) {
     S.Diag(Attr.getLoc(), diag::warn_attribute_ignored) << Attr.getName();
 }
 
+static void handleAsmJSAttr(Sema &S, Decl *D, const AttributeList &Attr) {
+  D->addAttr(::new (S.Context) AsmJSAttr(Attr.getRange(), S.Context, Attr.getAttributeSpellingListIndex()));
+}
+
+static void handleGenericJSAttr(Sema &S, Decl *D, const AttributeList &Attr) {
+  D->addAttr(::new (S.Context) GenericJSAttr(Attr.getRange(), S.Context, Attr.getAttributeSpellingListIndex()));
+}
+
 //===----------------------------------------------------------------------===//
 // Top Level Sema Entry Points
 //===----------------------------------------------------------------------===//
@@ -8570,6 +8578,12 @@ static void ProcessDeclAttribute(Sema &S, Scope *scope, Decl *D,
   case AttributeList::AT_JsExport:
     checkCheerpUnprefixedDeprecations(S, AL);
     handleJsExportAttr(S, D, AL);
+    break;
+  case AttributeList::AT_AsmJS:
+    handleAsmJSAttr(S, D, AL);
+    break;
+  case AttributeList::AT_GenericJS:
+    handleGenericJSAttr(S, D, AL);
     break;
   }
 }