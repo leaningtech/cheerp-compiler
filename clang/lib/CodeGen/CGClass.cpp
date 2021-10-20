leaningtech/cheerp-clang

Avoid null checks when downcasting with 0 offset on genericjs

lib/CodeGen/CGClass.cpp
@@ -448,9 +448,13 @@ CodeGenFunction::GetAddressOfDerivedClass(llvm::Value *Value,
     CGM.GetNonVirtualBaseClassOffset(Derived, PathBegin, PathEnd);
 
   bool asmjs = CurFn->getSection() == StringRef("asmjs");
-  if (!NonVirtualOffset && (getTarget().isByteAddressable() || asmjs)) {
+  if (!NonVirtualOffset) {
     // No offset, we can just cast back.
-    return Builder.CreateBitCast(Value, DerivedPtrTy);
+    if(getTarget().isByteAddressable() || asmjs) {
+      return Builder.CreateBitCast(Value, DerivedPtrTy);
+    } else {
+      return GenerateDowncast(Value, Derived, 0u);
+    }
   }
 
   llvm::BasicBlock *CastNull = nullptr;

leaningtech/cheerp-compiler

clang/lib/CodeGen/CGClass.cpp
@@ -608,9 +608,13 @@ CodeGenFunction::GetAddressOfDerivedClass(Address BaseAddr,
     CGM.GetNonVirtualBaseClassOffset(Derived, PathBegin, PathEnd);
 
   bool asmjs = CurFn->getSection() == StringRef("asmjs");
-  if (!NonVirtualOffset && (getTarget().isByteAddressable() || asmjs)) {
+  if (!NonVirtualOffset) {
     // No offset, we can just cast back.
-    return Builder.CreateBitCast(BaseAddr, DerivedPtrTy);
+    if(getTarget().isByteAddressable() || asmjs) {
+      return Builder.CreateBitCast(BaseAddr, DerivedPtrTy);
+    } else {
+      return GenerateDowncast(Value, Derived, 0u);
+    }
   }
 
   llvm::BasicBlock *CastNull = nullptr;