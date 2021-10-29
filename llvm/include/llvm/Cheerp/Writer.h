598a208f3c3cd9bce91255d21df34eb26d70a057
Carlo Piovesan <piovesan.carlo@gmail.com>
2021-10-29 12:03:47 +0200

    Refactor ffi object definition
    Now it works properly with 'importing' functions in ES6 modules

--- llvm/include/llvm/Cheerp/Writer.h
+++ llvm/include/llvm/Cheerp/Writer.h
@@ -756,6 +756,7 @@ private:
 	void compileModuleClosureEnd();
 	void compileHelpers();
 	void compileImports();
+	void compileAsmJSffiObject();
 	void compileAsmJSClosure();
 	void compileAsmJSTopLevel();
 	void compileGenericJS();
