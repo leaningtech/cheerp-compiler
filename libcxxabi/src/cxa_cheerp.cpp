dce376fa35581ed760274a155905d5c8684968e0
Yuri Iozzelli <y.iozzelli@gmail.com>
2021-12-01 14:48:36 +0100

    Exceptions: change ABI to avoid passing the js exception in __cheerp_landingpad
    To prepare for exceptions in wasm, we make so that __cheerp_landingpad
    can be in the asmjs section, by removing the client object field.
    At the same time we add this object as a field in Exception.
    We also use a custom allocator backed by a known array for Exception
    objects, so we can convert it from a linear pointer to a genericjs
    pointer and back.

--- libcxxabi/src/cxa_cheerp.cpp
+++ libcxxabi/src/cxa_cheerp.cpp
@@ -3,6 +3,7 @@
 #include <typeinfo>
 #include <cassert>
 #include <cstdlib>
+#include <utility>
 #include <cheerpintrin.h>
 #include <cheerp/client.h>
 
@@ -27,17 +28,78 @@ namespace [[cheerp::genericjs]] cheerp {
 }
 namespace [[cheerp::genericjs]] __cxxabiv1 {
 
-struct __cheerp_landingpad
+struct
+#ifdef __ASMJS__
+[[cheerp::wasm]]
+#endif
+ __cheerp_landingpad
 {
 	void* val;
 	int sel;
-	void(*jsObj)();
+};
+
+template<typename T>
+class IdAllocator
+{
+	T* store;
+	bool* slots;
+	int len;
+
+	int find_free_id()
+	{
+		int id = 0;
+		for (; id < len; id++)
+		{
+			if (!slots[id])
+				break;
+		}
+		if (id == len)
+		{
+			len = len*2;
+			store = static_cast<T*>(realloc(store, len));
+			slots = static_cast<bool*>(realloc(slots, len));
+		}
+		return id;
+	};
+public:
+	IdAllocator(): slots(new bool[16]), len(16)
+	{
+		store = static_cast<T*>(malloc(16*sizeof(T)));
+	}
+	template<typename... Args>
+	T* allocate(Args&&... args)
+	{
+		int id = find_free_id();
+		T* ret = new(&store[id]) T(cheerp::forward<Args>(args)...);
+		slots[id] = true;
+		return ret;
+	}
+	void deallocate(T* t)
+	{
+		int id = get_id(t);
+		store[id].~T();
+		slots[id] = false;
+	}
+	int get_id(T* t)
+	{
+		return t - store;
+	}
+	T* get_object(int id)
+	{
+		return &store[id];
+	}
+	T* get_object(void* unwind)
+	{
+		int id = __builtin_cheerp_pointer_offset(unwind);
+		return &store[id];
+	}
 };
 
 struct Exception
 {
-	static Exception* global_list;
+	static IdAllocator<Exception> allocator;
 
+	client::Object* jsObj;
 	void* obj;
 	void* adjustedPtr;
 	const std::type_info* tinfo;
@@ -51,20 +113,29 @@ struct Exception
 		: obj(obj), adjustedPtr(nullptr), tinfo(tinfo), dest(dest), refCount(1), 
 		  handlerCount(0), next(nullptr), primary(primary)
 	{
-		this->global_next = global_list;
-		global_list = this;
 	}
 	~Exception() noexcept
 	{
+		run_dest();
+	}
+	template<typename... Args>
+	static Exception* allocate(Args&&... args) noexcept
+	{
+		return allocator.allocate(cheerp::forward<Args>(args)...);
+	}
+	void deallocate() noexcept
+	{
+		allocator.deallocate(this);
+	}
+	void set_jsObj(client::Object* o) noexcept
+	{
+		jsObj = o;
+	}
+	void run_dest() noexcept
+	{
 		if(dest)
 			dest(obj);
-		delete reinterpret_cast<char*>(obj);
-		for (Exception* cur = global_list; cur != nullptr; cur = cur->global_next) {
-			if (cur->global_next == this) {
-				cur->global_next = this->global_next;
-				break;
-			}
-		}
+		dest = nullptr;
 	}
 	int incRef() noexcept
 	{
@@ -91,8 +162,8 @@ struct Exception
 		return primary != nullptr;
 	}
 };
+IdAllocator<Exception> Exception::allocator;
 
-Exception* Exception::global_list = nullptr;
 
 // Global variable to store the currently thrown exception.
 // This is needed just by the personality, which will reset it to null immediately
@@ -107,12 +178,7 @@ static int uncaughtExceptions = 0;
 
 static Exception* find_exception_from_unwind_ptr(void* unwind)
 {
-	for (Exception* cur = Exception::global_list; cur != nullptr; cur = cur->global_next) {
-		if (cur->obj == unwind) {
-			return cur;
-		}
-	}
-	return nullptr;
+	return Exception::allocator.get_object(unwind);
 }
 
 extern "C" {
@@ -123,7 +189,7 @@ void __cxa_decrement_exception_refcount(void* obj) noexcept
 	{
 		Exception* ex = find_exception_from_unwind_ptr(obj);
 		if(ex->decRef() == 0)
-			delete ex;
+			ex->deallocate();
 	}
 }
 void __cxa_increment_exception_refcount(void* obj) noexcept
@@ -142,6 +208,7 @@ static void do_throw(Exception* ex)
 	uncaughtExceptions += 1;
 
 	client::CheerpException* wrapper = new client::CheerpException(ex->tinfo->name());
+	ex->set_jsObj(wrapper);
 	__builtin_cheerp_throw(wrapper);
 }
 
@@ -149,7 +216,7 @@ static void do_throw(Exception* ex)
 __attribute((noinline))
 void
 __cxa_throw(void *thrown_object, std::type_info *tinfo, void (*dest)(void *)) {
-	Exception* ex = new Exception(thrown_object, tinfo, dest);
+	Exception* ex = Exception::allocate(thrown_object, tinfo, dest);
 	do_throw(ex);
 }
 
@@ -210,7 +277,7 @@ void __cxa_end_catch() noexcept {
 				// Reset ex to primary and deallocate the dependent exception
 				Exception* dep = ex;
 				ex = dep->primary;
-				delete dep;
+				dep->deallocate();
 			}
 			// Destroy the primary exception only if its refCount goes to 0
 			__cxa_decrement_exception_refcount(ex->obj);
@@ -242,7 +309,7 @@ void __cxa_rethrow_primary_exception(void* obj)
 {
 	Exception* ex = find_exception_from_unwind_ptr(obj);
 	__cxa_increment_exception_refcount(ex);
-	Exception* dep = new Exception(ex->obj, ex->tinfo, nullptr, ex);
+	Exception* dep = Exception::allocate(ex->obj, ex->tinfo, nullptr, ex);
 	do_throw(dep);
 }
 
@@ -282,7 +349,11 @@ __gxx_personality_v0
                     (client::Object* obj, __cheerp_clause* catches, int n) noexcept
 {
 	static bool reent = false;
-	static __cheerp_landingpad lp {nullptr, 0, nullptr};
+#ifdef __ASMJS__
+	[[cheerp::wasm]]
+#endif
+	static __cheerp_landingpad lp {nullptr, 0};
+
 	if(reent)
 		__builtin_cheerp_throw(obj);
 
@@ -300,7 +371,8 @@ __gxx_personality_v0
 			if(static_cast<std::type_info*>(catches[i].val) == &typeid(cheerp::JSException))
 			{
 				cheerp::JSException* foreign = new cheerp::JSException(obj);
-				Exception* ex = new Exception(foreign, &typeid(cheerp::JSException), nullptr);
+				Exception* ex = Exception::allocate(foreign, &typeid(cheerp::JSException), nullptr);
+				ex->set_jsObj(obj);
 				current_exception = ex;
 				uncaughtExceptions += 1;
 				// From now on, we treat this as a native exception;
@@ -311,12 +383,12 @@ __gxx_personality_v0
 	}
 	if(!native)
 	{
-		lp = __cheerp_landingpad{nullptr, 0, nullptr};
+		lp = __cheerp_landingpad{nullptr, 0};
 		return &lp;
 	}
 
 	Exception* ex = current_exception;
-	lp = __cheerp_landingpad { ex->obj, 0, reinterpret_cast<void(*)()>(obj)};
+	lp = __cheerp_landingpad { ex, 0};
 
 	for(int i = 0; i < n; i++)
 	{
