34547a7a30e97321f202dd28725c647677ab381c
Carlo Piovesan <piovesan.carlo@gmail.com>
2021-12-06 14:15:36 +0100

    Deletion/2

--- llvm/include/llvm/Cheerp/PartialExecuter.h
+++ llvm/include/llvm/Cheerp/PartialExecuter.h
@@ -33,9 +33,6 @@ public:
 	static char ID;
 	PartialExecuter();
 	bool runOnModule(llvm::Module& module) override;
-	llvm::BasicBlock* getImmediateDom(llvm::BasicBlock* bb, bool status);
-	llvm::BasicBlock* getOnlyOne(llvm::BasicBlock* bb);
-
 private:
 	bool runOnFunction(llvm::Function&);
 	void processModule(llvm::Module & module);
@@ -45,10 +42,6 @@ private:
 	void getAnalysisUsage(llvm::AnalysisUsage& AU) const override;
 	llvm::StringRef getPassName() const override;
 	std::unordered_map<const llvm::BasicBlock*, int> groupBasicBlocks(const llvm::Function& F);
-
-	std::map<llvm::BasicBlock*, std::set<llvm::BasicBlock*> > implementationSCC;
-	std::map<llvm::BasicBlock*, std::set<llvm::BasicBlock*> > implementationGLOBAL;
-	void findNextVisited(llvm::Function& F, bool status);
 
 	void classifyFunctions(const llvm::Module& module);
 	std::set<const llvm::Function*> isPreExecutableFunction;
