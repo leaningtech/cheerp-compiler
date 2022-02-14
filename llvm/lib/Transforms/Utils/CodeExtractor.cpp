ba59ddfad16295d406dc9260c9c6e35da3f0d09c
Carlo Piovesan <piovesan.carlo@gmail.com>
2022-02-14 11:40:13 +0100

    Fix warning: Add Attribute::Static in CodeExtractor

--- llvm/lib/Transforms/Utils/CodeExtractor.cpp
+++ llvm/lib/Transforms/Utils/CodeExtractor.cpp
@@ -954,6 +954,7 @@ Function *CodeExtractor::constructFunction(const ValueSet &inputs,
       case Attribute::StackProtect:
       case Attribute::StackProtectReq:
       case Attribute::StackProtectStrong:
+      case Attribute::Static:
       case Attribute::StrictFP:
       case Attribute::UWTable:
       case Attribute::VScaleRange:
