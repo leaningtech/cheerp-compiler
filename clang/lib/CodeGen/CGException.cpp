14a497964a279927bc77af7688d5d28cde75134e
Alessandro Pignotti <a.pignotti@sssup.it>
2022-03-10 18:30:00 +0100

    Adapt to API change

--- clang/lib/CodeGen/CGException.cpp
+++ clang/lib/CodeGen/CGException.cpp
@@ -411,9 +411,9 @@ void CodeGenFunction::EmitAnyExprToExn(const Expr *e, Address addr) {
   llvm::Type *ty = ConvertTypeForMem(e->getType());
   Address typedAddr = Builder.CreateElementBitCast(addr, ty);
 
-  if(!CGM.getTarget().isByteAddressable() && ty->getPointerElementType()->isStructTy()) {
+  if(!CGM.getTarget().isByteAddressable() && ty->isStructTy()) {
 	  // CHEERP: We insert a dummy downcast to signal that this type needs the downcast array
-    llvm::Type* Tys[] = { ty, ty };
+    llvm::Type* Tys[] = { ty->getPointerTo(), ty->getPointerTo() };
     llvm::Function* intrinsic = llvm::Intrinsic::getDeclaration(&CGM.getModule(), llvm::Intrinsic::cheerp_downcast, Tys);
     typedAddr = Address(Builder.CreateCall(intrinsic, {typedAddr.getPointer(), llvm::ConstantInt::get(CGM.Int32Ty, 0)}), typedAddr.getAlignment());
   }
