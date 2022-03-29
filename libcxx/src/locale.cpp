leaningtech/cheerp-libcxx

Disable a custom allocator on duetto

src/locale.cpp
@@ -144,7 +144,7 @@ class _LIBCPP_HIDDEN locale::__imp
     : public facet
 {
     enum {N = 28};
-#if defined(_LIBCPP_MSVC)
+#if defined(_LIBCPP_MSVC) || defined(__DUETTO__)
 // FIXME: MSVC doesn't support aligned parameters by value.
 // I can't get the __sso_allocator to work here
 // for MSVC I think for this reason.

leaningtech/cheerp-compiler

libcxx/src/locale.cpp
@@ -145,7 +145,7 @@ class _LIBCPP_HIDDEN locale::__imp
     : public facet
 {
     enum {N = 30};
-#if defined(_LIBCPP_COMPILER_MSVC)
+#if defined(_LIBCPP_COMPILER_MSVC) || defined(__CHEERP__)
 // FIXME: MSVC doesn't support aligned parameters by value.
 // I can't get the __sso_allocator to work here
 // for MSVC I think for this reason.