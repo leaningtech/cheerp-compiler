leaningtech/cheerp-clang

duetto: Integrate the duetto backend in clang

lib/Driver/Driver.cpp
@@ -1750,6 +1750,12 @@ static llvm::Triple computeTargetTriple(StringRef DefaultTargetTriple,
     }
   }
 
+  if (Target.getArch() == llvm::Triple::duetto)
+  {
+    //HACK: We need to fake the OS as Linux to find C++ headers
+    Target.setOS(llvm::Triple::Linux);
+  }
+
   return Target;
 }
 

leaningtech/cheerp-compiler

clang/lib/Driver/Driver.cpp
@@ -651,6 +651,12 @@ static llvm::Triple computeTargetTriple(const Driver &D,
       Target.setArch(llvm::Triple::riscv64);
   }
 
+  if (Target.getArch() == llvm::Triple::cheerp)
+  {
+    //HACK: We need to fake the OS as Linux to find C++ headers
+    Target.setOS(llvm::Triple::Linux);
+  }
+
   return Target;
 }
 