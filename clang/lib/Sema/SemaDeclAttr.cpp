leaningtech/cheerp-clang

Expose the [[cheerp::bytelayout]] attribute to the user

lib/Sema/SemaDeclAttr.cpp
@@ -5955,6 +5955,14 @@ static void handleAsmJSAttr(Sema &S, Decl *D, const AttributeList &Attr) {
 static void handleGenericJSAttr(Sema &S, Decl *D, const AttributeList &Attr) {
   D->addAttr(::new (S.Context) GenericJSAttr(Attr.getRange(), S.Context, Attr.getAttributeSpellingListIndex()));
 }
+
+static void handleByteLayoutAttr(Sema &S, Decl *D, const AttributeList &Attr) {
+  if (!isa<RecordDecl>(D))
+    S.Diag(Attr.getLoc(), diag::warn_attribute_ignored) << Attr.getName();
+  else
+    D->addAttr(::new (S.Context) ByteLayoutAttr(Attr.getRange(), S.Context, Attr.getAttributeSpellingListIndex()));
+}
+
 static void handleDefaultNewAttr(Sema &S, Decl *D, const AttributeList &Attr) {
   D->addAttr(::new (S.Context) DefaultNewAttr(Attr.getRange(), S.Context, Attr.getAttributeSpellingListIndex()));
 }
@@ -6623,6 +6631,9 @@ static void ProcessDeclAttribute(Sema &S, Scope *scope, Decl *D,
     break;
   case AttributeList::AT_GenericJS:
     handleGenericJSAttr(S, D, Attr);
+    break;
+  case AttributeList::AT_ByteLayout:
+    handleByteLayoutAttr(S, D, Attr);
     break;
   case AttributeList::AT_DefaultNew:
     handleDefaultNewAttr(S, D, Attr);

leaningtech/cheerp-compiler

clang/lib/Sema/SemaDeclAttr.cpp
@@ -8210,6 +8210,14 @@ static void handleAsmJSAttr(Sema &S, Decl *D, const AttributeList &Attr) {
 static void handleGenericJSAttr(Sema &S, Decl *D, const AttributeList &Attr) {
   D->addAttr(::new (S.Context) GenericJSAttr(Attr.getRange(), S.Context, Attr.getAttributeSpellingListIndex()));
 }
+
+static void handleByteLayoutAttr(Sema &S, Decl *D, const AttributeList &Attr) {
+  if (!isa<RecordDecl>(D))
+    S.Diag(Attr.getLoc(), diag::warn_attribute_ignored) << Attr.getName();
+  else
+    D->addAttr(::new (S.Context) ByteLayoutAttr(Attr.getRange(), S.Context, Attr.getAttributeSpellingListIndex()));
+}
+
 static void handleDefaultNewAttr(Sema &S, Decl *D, const AttributeList &Attr) {
   D->addAttr(::new (S.Context) DefaultNewAttr(Attr.getRange(), S.Context, Attr.getAttributeSpellingListIndex()));
 }
@@ -8954,8 +8962,11 @@ static void ProcessDeclAttribute(Sema &S, Scope *scope, Decl *D,
   case AttributeList::AT_GenericJS:
     handleGenericJSAttr(S, D, AL);
     break;
+  case AttributeList::AT_ByteLayout:
+    handleByteLayoutAttr(S, D, AL);
+    break;
   case AttributeList::AT_DefaultNew:
-    handleDefaultNewAttr(S, D, Attr);
+    handleDefaultNewAttr(S, D, AL);
     break;
   }
 }