leaningtech/cheerp-llvm

Fix detection of properties for cheerp.allocate

lib/Analysis/MemoryBuiltins.cpp
@@ -100,8 +100,12 @@ static const AllocFnsTy *getAllocationData(const Value *V, AllocType AllocTy,
   if (const IntrinsicInst* II = dyn_cast<IntrinsicInst>(V))
   {
     if (II->getIntrinsicID() == Intrinsic::cheerp_allocate ||
-        II->getIntrinsicID() == Intrinsic::cheerp_allocate_array)
-      return &AllocationFnData[0];
+        II->getIntrinsicID() == Intrinsic::cheerp_allocate_array) {
+      const AllocFnsTy *FnData = &AllocationFnData[0];
+      if ((FnData->AllocTy & AllocTy) != FnData->AllocTy)
+        return nullptr;
+      return FnData;
+    }
     return nullptr;
   }
 

leaningtech/cheerp-compiler

llvm/lib/Analysis/MemoryBuiltins.cpp
@@ -181,8 +181,12 @@ getAllocationDataForFunction(const Function *Callee, AllocType AllocTy,
                              const TargetLibraryInfo *TLI) {
 
   if (Callee->getIntrinsicID() == Intrinsic::cheerp_allocate ||
-      Callee->getIntrinsicID() == Intrinsic::cheerp_allocate_array)
-    return &AllocationFnData[0];
+      Callee->getIntrinsicID() == Intrinsic::cheerp_allocate_array) {
+      const AllocFnsTy *FnData = &AllocationFnData[0];
+      if ((FnData->AllocTy & AllocTy) != FnData->AllocTy)
+        return None;
+      return FnData;
+  }
 
   // Make sure that the function is available.
   LibFunc TLIFn;