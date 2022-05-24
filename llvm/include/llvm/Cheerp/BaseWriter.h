e47c57509d07816f54729d2a788a1dd2bb4206e8
Carlo Piovesan <piovesan.carlo@gmail.com>
2022-05-24 21:23:37 +0200

    CheerpWriters: inherit from CheerpBaseWriter (NFC)

---
+++ llvm/include/llvm/Cheerp/BaseWriter.h
@@ -0,0 +1,24 @@
+//===-- Cheerp/BaseWriter.h - The Cheerp base generator -----------------===//
+//
+//                     Cheerp: The C++ compiler for the Web
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+// Copyright 2022 Leaning Technologies
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef _CHEERP_BASE_WRITER_H
+#define _CHEERP_BASE_WRITER_H
+
+namespace cheerp
+{
+
+class CheerpBaseWriter
+{
+};
+
+} // cheerp
+
+#endif
