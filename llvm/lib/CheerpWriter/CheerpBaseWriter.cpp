6ae14bdd1e538adba4dad225126b6bf782ac9dfa
Carlo Piovesan <piovesan.carlo@gmail.com>
2022-05-24 22:13:34 +0200

    CheerpWriters: move needsUnsignedTruncation to base class (NFC)

---
+++ llvm/lib/CheerpWriter/CheerpBaseWriter.cpp
@@ -0,0 +1,16 @@
+//===-- CheerpBaseWriter.cpp - The Cheerp JavaScript generator ------------===//
+//
+//                     Cheerp: The C++ compiler for the Web
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+// Copyright 2022 Leaning Technologies
+//
+//===----------------------------------------------------------------------===//
+
+#include "llvm/Cheerp/BaseWriter.h"
+
+using namespace cheerp;
+
+CheerpBaseWriter::~CheerpBaseWriter() {}
