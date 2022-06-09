18f340fff25ba12627795db025767140bb3d80bd
Carlo Piovesan <piovesan.carlo@gmail.com>
2022-06-09 15:26:41 +0200

    getPointerElementType: LoopUtils

--- llvm/lib/Transforms/Utils/LoopUtils.cpp
+++ llvm/lib/Transforms/Utils/LoopUtils.cpp
@@ -1378,7 +1378,7 @@ int llvm::rewriteLoopExitValues(Loop *L, LoopInfo *LI, TargetLibraryInfo *TLI,
     if(!SE->getDataLayout().isByteAddressable()) {
       // On Cheerp we need to make sure this did not generate a uglygep
       if(Instruction* bc = dyn_cast<BitCastInst>(ExitVal)) {
-        if(bc->getOperand(0)->getType()->getPointerElementType()->isIntegerTy(8)) {
+        if(bc->getOperand(0)->getType()->getNonOpaquePointerElementType()->isIntegerTy(8)) {
           // Cast from i8* to something, it is most probably a bad idea
           DeadInsts.push_back(ExitVal);
           continue;
