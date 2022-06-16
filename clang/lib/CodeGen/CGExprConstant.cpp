74e6b6f2da5e72ecebe1f7e699eba23e2ca9673f
Carlo Piovesan <piovesan.carlo@gmail.com>
2022-06-16 16:21:42 +0200

    getPointerElementType: CGExprConstant, keep track of C element type

--- clang/lib/CodeGen/CGExprConstant.cpp
+++ clang/lib/CodeGen/CGExprConstant.cpp
@@ -1983,6 +1983,7 @@ private:
 
 
   llvm::Type* CurrentType = C->getType()->getPointerElementType();
+  llvm::Type* CElementType = CurrentType;
 
   if(Value.hasLValuePath() && CGM.getTypes().ConvertTypeForMem(CurType) == CurrentType) {
     ArrayRef<APValue::LValuePathEntry> Path = Value.getLValuePath();
@@ -2002,8 +2003,10 @@ private:
           CurType = FD->getType();
 	  if (CurClass->isUnionType()) {
             // Apply a bitcast, on unions it is safe
-            C = llvm::ConstantExpr::getGetElementPtr(C->getType()->getPointerElementType(), C, Indexes);
+            C = llvm::ConstantExpr::getGetElementPtr(CElementType, C, Indexes);
 	    C = llvm::ConstantExpr::getBitCast(C, CGM.getTypes().ConvertTypeForMem(CurType)->getPointerTo());
+            CElementType = CGM.getTypes().ConvertTypeForMem(CurType);
+
 	    Indexes.clear();
             continue;
           }
@@ -2072,7 +2075,7 @@ private:
     }
 
     if(Indexes.size() > 1)
-      C = llvm::ConstantExpr::getGetElementPtr(C->getType()->getPointerElementType(), C, Indexes);
+      C = llvm::ConstantExpr::getGetElementPtr(CElementType, C, Indexes);
 
     if (OffsetVal == 0)
       return C;
