b880d683825d5b4ef7c748b974b153c23a98f8f1
Carlo Piovesan <piovesan.carlo@gmail.com>
2022-06-23 14:23:27 +0200

    Revert "getPointerElementType: removal from CGExprConstant"
    This reverts commit c1f8f8337a4230f2da086f05bd3b73d24222a34f.

--- clang/lib/CodeGen/CGExprConstant.cpp
+++ clang/lib/CodeGen/CGExprConstant.cpp
@@ -2006,7 +2006,7 @@ private:
   } else {
     // Try to build a naturally looking GEP from the returned expression to the
     // required type
-    while(DestTy->isPointerTy() && (OffsetVal || CurrentType->getPointerTo() != DestTy))
+    while(DestTy->isPointerTy() && (OffsetVal || CurrentType!=DestTy->getPointerElementType()))
     {
       if (llvm::StructType* ST=dyn_cast<llvm::StructType>(CurrentType))
       {
@@ -2018,7 +2018,7 @@ private:
           bool isBaseGood = false;
           while((curBase = curBase->getDirectBase()))
           {
-            if(curBase->getPointerTo() == DestTy)
+            if(curBase == DestTy->getPointerElementType())
             {
               isBaseGood = true;
               break;
