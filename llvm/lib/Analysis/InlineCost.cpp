086305521eb8eee0845c000060eb9e8b5b182327
Carlo Piovesan <piovesan.carlo@gmail.com>
2022-07-28 16:04:14 +0200

    InlineCost: Add constrainst on no invokes

--- llvm/lib/Analysis/InlineCost.cpp
+++ llvm/lib/Analysis/InlineCost.cpp
@@ -3016,6 +3016,9 @@ bool llvm::isInlineViableCheerp(Function &F, Function &Caller) {
           return false;
         }
       }
+      // No invokes
+      if (isa<InvokeInst>(II))
+        return false;
       // No allocas (TODO relax this)
       if (isa<AllocaInst>(II))
         return false;
