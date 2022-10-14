leaningtech/cheerp-compiler

commit 8b3c5e96aed63d9818731ab41645239efa302f5a

encodeDataSegments: Refactor chunks detection (NFC)

llvm/lib/CheerpWriter/CheerpWasmWriter.cpp
@@ -3699,7 +3699,7 @@ uint32_t CheerpWasmWriter::encodeDataSectionChunks(WasmBuffer& data, uint32_t ad
 	// Split data section buffer into chunks based on 6 (or more) zero bytes.
 	uint32_t chunks = 0;
 	size_t cur = 0, last = 0, end = 0;
-	std::string delimiter("\0\0\0\0\0\0\0", 6);
+	std::string delimiter(6, '0');
 	while ((cur = buf.find(delimiter, last)) != std::string::npos) {
 		std::string chunk(buf.substr(last, cur - last));
 		assert(chunk.size() == cur - last);

commit 5779521288b6939164b137dd1bf0686c8ba3b4c6

Fix error introduced in 8b3c5e96aed6 '0' != '\0'

llvm/lib/CheerpWriter/CheerpWasmWriter.cpp
@@ -4378,7 +4378,7 @@ uint32_t CheerpWasmWriter::encodeDataSectionChunks(WasmBuffer& data, uint32_t ad
 	// Split data section buffer into chunks based on 9 (or more) zero bytes.
 	uint32_t chunks = 0;
 	size_t cur = 0, last = 0, end = 0;
-	std::string delimiter(9, '0');
+	std::string delimiter(9, '\0');
 	while ((cur = buf.find(delimiter, last)) != std::string::npos) {
 		if (chunks + 1 == 1e5)
 		{