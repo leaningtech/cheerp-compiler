8e5e1c396427d7e5e787a389c32387dd62e45fd7
Carlo Piovesan <piovesan.carlo@gmail.com>
2022-10-17 15:24:35 +0200

    CheerpWriter: getPointerKindAssert -> getPointerKind

--- llvm/lib/CheerpWriter/CheerpWriter.cpp
+++ llvm/lib/CheerpWriter/CheerpWriter.cpp
@@ -4801,7 +4801,7 @@ void CheerpWriter::compileBB(const BasicBlock& BB)
 		}
 		if(!I.use_empty())
 		{
-			if(I.getType()->isPointerTy() && (!isa<CallBase>(I) || isDowncast) && PA.getPointerKindAssert(&I) == SPLIT_REGULAR && !PA.getConstantOffsetForPointer(&I))
+			if(I.getType()->isPointerTy() && (!isa<CallBase>(I) || isDowncast) && PA.getPointerKind(&I) == SPLIT_REGULAR && !PA.getConstantOffsetForPointer(&I))
 			{
 				stream << getSecondaryName(&I) << '=';
 			}
