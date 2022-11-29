ae8fb6180cd490851bf0e2118e713d111f528f14
Alessandro Pignotti <a.pignotti@sssup.it>
2022-11-29 17:15:26 +0100

    Restore a fix lost in a previous rebase

--- llvm/lib/Analysis/AliasAnalysis.cpp
+++ llvm/lib/Analysis/AliasAnalysis.cpp
@@ -631,6 +631,8 @@ ModRefInfo AAResults::getModRefInfo(const Instruction *I,
     return getModRefInfo((const CatchPadInst *)I, Loc, AAQIP);
   case Instruction::CatchRet:
     return getModRefInfo((const CatchReturnInst *)I, Loc, AAQIP);
+  case Instruction::Resume:
+    return ModRefInfo::ModRef;
   default:
     assert(!I->mayReadOrWriteMemory() &&
            "Unhandled memory access instruction!");
