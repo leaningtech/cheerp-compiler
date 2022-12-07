leaningtech/cheerp-llvm

Rebranding to Cheerp

lib/Analysis/MemoryBuiltins.cpp
@@ -53,7 +53,7 @@ struct AllocFnsTy {
 // know which functions are nounwind, noalias, nocapture parameters, etc.
 static const AllocFnsTy AllocationFnData[] = {
   // Keep this one as the first one
-  {LibFunc::duetto_allocate,     CallocLike,  1, 0,  -1},
+  {LibFunc::cheerp_allocate,     CallocLike,  1, 0,  -1},
   {LibFunc::malloc,              MallocLike,  1, 0,  -1},
   {LibFunc::valloc,              MallocLike,  1, 0,  -1},
   {LibFunc::Znwj,                OpNewLike,   1, 0,  -1}, // new(unsigned int)
@@ -95,10 +95,10 @@ static Function *getCalledFunction(const Value *V, bool LookThroughBitCast) {
 static const AllocFnsTy *getAllocationData(const Value *V, AllocType AllocTy,
                                            const TargetLibraryInfo *TLI,
                                            bool LookThroughBitCast = false) {
-  // Skip all intrinsics but duetto.allocate
+  // Skip all intrinsics but cheerp.allocate
   if (const IntrinsicInst* II = dyn_cast<IntrinsicInst>(V))
   {
-    if (II->getIntrinsicID() == Intrinsic::duetto_allocate)
+    if (II->getIntrinsicID() == Intrinsic::cheerp_allocate)
       return &AllocationFnData[0];
     return nullptr;
   }

leaningtech/cheerp-compiler

llvm/lib/Analysis/MemoryBuiltins.cpp
@@ -178,7 +178,7 @@ getAllocationDataForFunction(const Function *Callee, AllocType AllocTy,
   if (!Callee->getReturnType()->isPointerTy())
     return None;
 
-  if (Callee->getIntrinsicID() == Intrinsic::duetto_allocate)
+  if (Callee->getIntrinsicID() == Intrinsic::cheerp_allocate)
     return &AllocationFnData[0];
 
   // Make sure that the function is available.