leaningtech/cheerp-llvm

CheerpBackend: Rework source maps generation

include/llvm/Cheerp/SourceMaps.h
@@ -12,9 +12,9 @@
 #ifndef _CHEERP_SOURCE_MAPS_H
 #define _CHEERP_SOURCE_MAPS_H
 
-#include "llvm/Support/raw_ostream.h"
 #include "llvm/IR/DebugLoc.h"
 #include "llvm/IR/Metadata.h"
+#include "llvm/Support/ToolOutputFile.h"
 #include <map>
 
 namespace cheerp
@@ -23,7 +23,8 @@ namespace cheerp
 class SourceMapGenerator
 {
 private:
-	llvm::raw_ostream* sourceMap;
+	llvm::tool_output_file sourceMap;
+	const std::string& sourceMapName;
 	llvm::LLVMContext& Ctx;
 	std::map<llvm::MDString*, uint32_t> fileMap;
 	uint32_t lastFile;
@@ -33,7 +34,8 @@ private:
 	bool lineStart;
 	void writeBase64VLQInt(int32_t i);
 public:
-	SourceMapGenerator(llvm::raw_ostream* s, llvm::LLVMContext& C);
+	// sourceMapName life span should be longer than the one of the SourceMapGenerator
+	SourceMapGenerator(const std::string& sourceMapName, llvm::LLVMContext& C, std::string& ErrorString);
 	void setDebugLoc(const llvm::DebugLoc& debugLoc);
 	void clearDebugInfo()
 	{
@@ -42,6 +44,10 @@ public:
 	void beginFile();
 	void finishLine();
 	void endFile();
+	const std::string& getSourceMapName() const
+	{
+		return sourceMapName;
+	}
 };
 
 }

lib/CheerpWriter/SourceMaps.cpp
@@ -17,9 +17,9 @@ using namespace llvm;
 namespace cheerp
 {
 
-SourceMapGenerator::SourceMapGenerator(llvm::raw_ostream* s, llvm::LLVMContext& C):
-	sourceMap(s), Ctx(C), lastFile(0), lastLine(0), lastColoumn(0),
-	validInfo(false), lineStart(true)
+SourceMapGenerator::SourceMapGenerator(const std::string& sourceMapName, llvm::LLVMContext& C, std::string& ErrorString):
+	sourceMap(sourceMapName.c_str(), ErrorString, sys::fs::F_None), sourceMapName(sourceMapName), Ctx(C), lastFile(0),
+	lastLine(0), lastColoumn(0), validInfo(false), lineStart(true)
 {
 }
 
@@ -39,15 +39,13 @@ void SourceMapGenerator::writeBase64VLQInt(int i)
 		i >>= 5;
 		if(i)
 			base64Char |= 0x20;
-		*sourceMap << base64Chars[base64Char];
+		sourceMap.os() << base64Chars[base64Char];
 	}
 	while(i);
 }
 
 void SourceMapGenerator::setDebugLoc(const llvm::DebugLoc& debugLoc)
 {
-	if(!sourceMap)
-		return;
 	if(!lineStart)
 	{
 		//TODO: Support multi-segment
@@ -86,43 +84,38 @@ void SourceMapGenerator::setDebugLoc(const llvm::DebugLoc& debugLoc)
 
 void SourceMapGenerator::beginFile()
 {
-	if(!sourceMap)
-		return;
 	// Output the prologue of the file
-	*sourceMap << "{\n";
-	*sourceMap << "\"version\": 3,\n";
-	*sourceMap << "\"names\": [],\n";
-	*sourceMap << "\"mappings\": \"";
+	sourceMap.os() << "{\n";
+	sourceMap.os() << "\"version\": 3,\n";
+	sourceMap.os() << "\"names\": [],\n";
+	sourceMap.os() << "\"mappings\": \"";
 }
 
 void SourceMapGenerator::finishLine()
 {
-	if(!sourceMap)
-		return;
 	assert(!validInfo);
-	*sourceMap << ";";
+	sourceMap.os() << ";";
 	lineStart = true;
 }
 
 void SourceMapGenerator::endFile()
 {
-	if(!sourceMap)
-		return;
 	// Output the prologue of the file
-	*sourceMap << "\",\n";
+	sourceMap.os() << "\",\n";
 	// Output file names
 	SmallVector<MDString*, 10> files(fileMap.size(), NULL);
 	for(auto mapItem: fileMap)
 		files[mapItem.second] = mapItem.first;
-	*sourceMap << "\"sources\": [";
+	sourceMap.os() << "\"sources\": [";
 	for(uint32_t i=0;i<files.size();i++)
 	{
 		if(i!=0)
-			*sourceMap << ',';
-		*sourceMap << '"' << files[i]->getString() << '"';
+			sourceMap.os() << ',';
+		sourceMap.os() << '"' << files[i]->getString() << '"';
 	}
-	*sourceMap << "]\n";
-	*sourceMap << "}\n";
+	sourceMap.os() << "]\n";
+	sourceMap.os() << "}\n";
+	sourceMap.keep();
 }
 
 }

leaningtech/cheerp-compiler

llvm/include/llvm/Cheerp/SourceMaps.h
@@ -12,9 +12,9 @@
 #ifndef _CHEERP_SOURCE_MAPS_H
 #define _CHEERP_SOURCE_MAPS_H
 
-#include "llvm/Support/raw_ostream.h"
 #include "llvm/IR/DebugLoc.h"
 #include "llvm/IR/Metadata.h"
+#include "llvm/Support/ToolOutputFile.h"
 #include <map>
 
 namespace cheerp
@@ -23,7 +23,8 @@ namespace cheerp
 class SourceMapGenerator
 {
 private:
-	llvm::raw_ostream* sourceMap;
+	llvm::tool_output_file sourceMap;
+	const std::string& sourceMapName;
 	llvm::LLVMContext& Ctx;
 	std::map<llvm::MDString*, uint32_t> fileMap;
 	uint32_t lastFile;
@@ -33,7 +34,8 @@ private:
 	bool lineStart;
 	void writeBase64VLQInt(int32_t i);
 public:
-	SourceMapGenerator(llvm::raw_ostream* s, llvm::LLVMContext& C);
+	// sourceMapName life span should be longer than the one of the SourceMapGenerator
+	SourceMapGenerator(const std::string& sourceMapName, llvm::LLVMContext& C, std::error_code& ErrorCode);
 	void setDebugLoc(const llvm::DebugLoc& debugLoc);
 	void clearDebugInfo()
 	{
@@ -42,6 +44,10 @@ public:
 	void beginFile();
 	void finishLine();
 	void endFile();
+	const std::string& getSourceMapName() const
+	{
+		return sourceMapName;
+	}
 };
 
 }

llvm/lib/CheerpWriter/SourceMaps.cpp
@@ -11,15 +11,16 @@
 
 #include "llvm/Cheerp/SourceMaps.h"
 #include "llvm/IR/Metadata.h"
+#include "llvm/Support/FileSystem.h"
 
 using namespace llvm;
 
 namespace cheerp
 {
 
-SourceMapGenerator::SourceMapGenerator(llvm::raw_ostream* s, llvm::LLVMContext& C):
-	sourceMap(s), Ctx(C), lastFile(0), lastLine(0), lastColoumn(0),
-	validInfo(false), lineStart(true)
+SourceMapGenerator::SourceMapGenerator(const std::string& sourceMapName, llvm::LLVMContext& C, std::error_code& ErrorCode):
+	sourceMap(sourceMapName.c_str(), ErrorCode, sys::fs::F_None), sourceMapName(sourceMapName), Ctx(C), lastFile(0),
+	lastLine(0), lastColoumn(0), validInfo(false), lineStart(true)
 {
 }
 
@@ -39,15 +40,13 @@ void SourceMapGenerator::writeBase64VLQInt(int i)
 		i >>= 5;
 		if(i)
 			base64Char |= 0x20;
-		*sourceMap << base64Chars[base64Char];
+		sourceMap.os() << base64Chars[base64Char];
 	}
 	while(i);
 }
 
 void SourceMapGenerator::setDebugLoc(const llvm::DebugLoc& debugLoc)
 {
-	if(!sourceMap)
-		return;
 	if(!lineStart)
 	{
 		//TODO: Support multi-segment
@@ -86,43 +85,38 @@ void SourceMapGenerator::setDebugLoc(const llvm::DebugLoc& debugLoc)
 
 void SourceMapGenerator::beginFile()
 {
-	if(!sourceMap)
-		return;
 	// Output the prologue of the file
-	*sourceMap << "{\n";
-	*sourceMap << "\"version\": 3,\n";
-	*sourceMap << "\"names\": [],\n";
-	*sourceMap << "\"mappings\": \"";
+	sourceMap.os() << "{\n";
+	sourceMap.os() << "\"version\": 3,\n";
+	sourceMap.os() << "\"names\": [],\n";
+	sourceMap.os() << "\"mappings\": \"";
 }
 
 void SourceMapGenerator::finishLine()
 {
-	if(!sourceMap)
-		return;
 	assert(!validInfo);
-	*sourceMap << ";";
+	sourceMap.os() << ";";
 	lineStart = true;
 }
 
 void SourceMapGenerator::endFile()
 {
-	if(!sourceMap)
-		return;
 	// Output the prologue of the file
-	*sourceMap << "\",\n";
+	sourceMap.os() << "\",\n";
 	// Output file names
 	SmallVector<MDString*, 10> files(fileMap.size(), NULL);
 	for(auto mapItem: fileMap)
 		files[mapItem.second] = mapItem.first;
-	*sourceMap << "\"sources\": [";
+	sourceMap.os() << "\"sources\": [";
 	for(uint32_t i=0;i<files.size();i++)
 	{
 		if(i!=0)
-			*sourceMap << ',';
-		*sourceMap << '"' << files[i]->getString() << '"';
+			sourceMap.os() << ',';
+		sourceMap.os() << '"' << files[i]->getString() << '"';
 	}
-	*sourceMap << "]\n";
-	*sourceMap << "}\n";
+	sourceMap.os() << "]\n";
+	sourceMap.os() << "}\n";
+	sourceMap.keep();
 }
 
 }