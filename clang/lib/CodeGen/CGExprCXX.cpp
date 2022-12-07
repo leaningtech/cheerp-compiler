leaningtech/cheerp-clang

CheerpFrontend: Use fully typed allocations for non structures as well

lib/CodeGen/CGExprCXX.cpp
@@ -977,11 +977,9 @@ static RValue EmitNewDeleteCall(CodeGenFunction &CGF,
                                 QualType* allocType = NULL) {
   llvm::Instruction *CallOrInvoke;
   llvm::Value *CalleeAddr = CGF.CGM.GetAddrOfFunction(Callee);
-  llvm::Type* allocatedType = allocType?CGF.ConvertType(*allocType):NULL;
 
   RValue RV;
-  if(Callee->hasAttr<MallocAttr>() && !CGF.getTarget().isByteAddressable() &&
-     allocatedType && allocatedType->isStructTy())
+  if(Callee->hasAttr<MallocAttr>() && !CGF.getTarget().isByteAddressable())
   {
     // Forge a call to a special type safe allocator intrinsic
     QualType allocPtrType = CGF.getContext().getPointerType(*allocType);

leaningtech/cheerp-compiler

clang/lib/CodeGen/CGExprCXX.cpp
@@ -1328,8 +1328,7 @@ static RValue EmitNewDeleteCall(CodeGenFunction &CGF,
   llvm::Constant *CalleePtr = CGF.CGM.GetAddrOfFunction(CalleeDecl);
 
   RValue RV;
-  if(CalleeDecl->hasAttr<MallocAttr>() && !CGF.getTarget().isByteAddressable() &&
-     allocatedType && allocatedType->isStructTy())
+  if(CalleeDecl->hasAttr<MallocAttr>() && !CGF.getTarget().isByteAddressable())
   {
     // Forge a call to a special type safe allocator intrinsic
     QualType allocPtrType = CGF.getContext().getPointerType(*allocType);