leaningtech/cheerp-clang

Fix previous commit

lib/CodeGen/CGClass.cpp
@@ -244,7 +244,10 @@ llvm::Value *CodeGenFunction::GetAddressOfBaseClass(
       EmitTypeCheck(TCK_Upcast, Loc, Value, DerivedTy, DerivedAlign,
                     !NullCheckValue);
     }
-    return Builder.CreateBitCast(Value, BasePtrTy);
+    if(getTarget().isByteAddressable())
+      return Builder.CreateBitCast(Value, BasePtrTy);
+    else
+      return GenerateUpcastCollapsed(Value, BasePtrTy);
   }
 
   llvm::BasicBlock *origBB = nullptr;

leaningtech/cheerp-compiler

clang/lib/CodeGen/CGClass.cpp
@@ -394,7 +394,10 @@ Address CodeGenFunction::GetAddressOfBaseClass(
       EmitTypeCheck(TCK_Upcast, Loc, Value.getPointer(),
                     DerivedTy, DerivedAlign, SkippedChecks);
     }
-    return Builder.CreateElementBitCast(Value, BaseValueTy);
+    if(getTarget().isByteAddressable())
+      return Builder.CreateElementBitCast(Value, BaseValueTy);
+    else
+      return GenerateUpcastCollapsed(Value, BasePtrTy);
   }
 
   llvm::BasicBlock *origBB = nullptr;