leaningtech/cheerp-clang

Use fully typed memory intrinsics

lib/CodeGen/CGExprAgg.cpp
@@ -1330,9 +1330,10 @@ static void CheckAggExprForMemSetUse(AggValueSlot &Slot, const Expr *E,
 
   llvm::Value *Loc = Slot.getAddr();
   
-  Loc = CGF.Builder.CreateBitCast(Loc, CGF.Int8PtrTy);
+  if (CGF.getTarget().isByteAddressable())
+    Loc = CGF.Builder.CreateBitCast(Loc, CGF.Int8PtrTy);
   CGF.Builder.CreateMemSet(Loc, CGF.Builder.getInt8(0), SizeVal, 
-                           Align.getQuantity(), false);
+                           Align.getQuantity(), false, NULL, CGF.getTarget().isByteAddressable());
   
   // Tell the AggExprEmitter that the slot is known zero.
   Slot.setZeroed();
@@ -1426,15 +1427,18 @@ void CodeGenFunction::EmitAggregateCopy(llvm::Value *DestPtr,
   // we need to use a different call here.  We use isVolatile to indicate when
   // either the source or the destination is volatile.
 
-  llvm::PointerType *DPT = cast<llvm::PointerType>(DestPtr->getType());
-  llvm::Type *DBP =
-    llvm::Type::getInt8PtrTy(getLLVMContext(), DPT->getAddressSpace());
-  DestPtr = Builder.CreateBitCast(DestPtr, DBP);
+  if (getTarget().isByteAddressable())
+  {
+    llvm::PointerType *DPT = cast<llvm::PointerType>(DestPtr->getType());
+    llvm::Type *DBP =
+      llvm::Type::getInt8PtrTy(getLLVMContext(), DPT->getAddressSpace());
+    DestPtr = Builder.CreateBitCast(DestPtr, DBP);
 
-  llvm::PointerType *SPT = cast<llvm::PointerType>(SrcPtr->getType());
-  llvm::Type *SBP =
-    llvm::Type::getInt8PtrTy(getLLVMContext(), SPT->getAddressSpace());
-  SrcPtr = Builder.CreateBitCast(SrcPtr, SBP);
+    llvm::PointerType *SPT = cast<llvm::PointerType>(SrcPtr->getType());
+    llvm::Type *SBP =
+      llvm::Type::getInt8PtrTy(getLLVMContext(), SPT->getAddressSpace());
+    SrcPtr = Builder.CreateBitCast(SrcPtr, SBP);
+  }
 
   // Don't do any of the memmove_collectable tests if GC isn't set.
   if (CGM.getLangOpts().getGC() == LangOptions::NonGC) {
@@ -1473,5 +1477,5 @@ void CodeGenFunction::EmitAggregateCopy(llvm::Value *DestPtr,
                        llvm::ConstantInt::get(IntPtrTy, 
                                               TypeInfo.first.getQuantity()),
                        alignment.getQuantity(), isVolatile,
-                       /*TBAATag=*/0, TBAAStructTag);
+                       /*TBAATag=*/0, TBAAStructTag, getTarget().isByteAddressable());
 }

leaningtech/cheerp-compiler

clang/lib/CodeGen/CGExprAgg.cpp
@@ -1975,10 +1975,11 @@ static void CheckAggExprForMemSetUse(AggValueSlot &Slot, const Expr *E,
   // Okay, it seems like a good idea to use an initial memset, emit the call.
   llvm::Constant *SizeVal = CGF.Builder.getInt64(Size.getQuantity());
 
-  Address Loc = Slot.getAddress();
-  Loc = CGF.Builder.CreateElementBitCast(Loc, CGF.Int8Ty);
-  CGF.Builder.CreateMemSet(Loc, CGF.Builder.getInt8(0), SizeVal, false);
-
+  Address Loc = Slot.getAddress();  
+  if (CGF.getTarget().isByteAddressable())
+    Loc = CGF.Builder.CreateElementBitCast(Loc, CGF.Int8Ty);
+  CGF.Builder.CreateMemSet(Loc, CGF.Builder.getInt8(0), SizeVal, false, CGF.getTarget().isByteAddressable());
+  
   // Tell the AggExprEmitter that the slot is known zero.
   Slot.setZeroed();
 }
@@ -2140,8 +2141,11 @@ void CodeGenFunction::EmitAggregateCopy(LValue Dest, LValue Src, QualType Ty,
   // we need to use a different call here.  We use isVolatile to indicate when
   // either the source or the destination is volatile.
 
-  DestPtr = Builder.CreateElementBitCast(DestPtr, Int8Ty);
-  SrcPtr = Builder.CreateElementBitCast(SrcPtr, Int8Ty);
+  if (getTarget().isByteAddressable())
+  {
+    DestPtr = Builder.CreateElementBitCast(DestPtr, Int8Ty);
+    SrcPtr = Builder.CreateElementBitCast(SrcPtr, Int8Ty);
+  }
 
   // Don't do any of the memmove_collectable tests if GC isn't set.
   if (CGM.getLangOpts().getGC() == LangOptions::NonGC) {
@@ -2164,7 +2168,7 @@ void CodeGenFunction::EmitAggregateCopy(LValue Dest, LValue Src, QualType Ty,
     }
   }
 
-  auto Inst = Builder.CreateMemCpy(DestPtr, SrcPtr, SizeVal, isVolatile);
+  auto Inst = Builder.CreateMemCpy(DestPtr, SrcPtr, SizeVal, isVolatile, getTarget().isByteAddressable());
 
   // Determine the metadata to describe the position of any padding in this
   // memcpy, as well as the TBAA tags for the members of the struct, in case