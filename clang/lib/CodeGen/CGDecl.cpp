leaningtech/cheerp-clang

Generate type safe lifetime intrinsics

lib/CodeGen/CGDecl.cpp
@@ -475,9 +475,8 @@ namespace {
       : Addr(addr), Size(size) {}
 
     void Emit(CodeGenFunction &CGF, Flags flags) override {
-      llvm::Value *castAddr = CGF.Builder.CreateBitCast(Addr, CGF.Int8PtrTy);
-      CGF.Builder.CreateCall2(CGF.CGM.getLLVMLifetimeEndFn(),
-                              Size, castAddr)
+      CGF.Builder.CreateCall2(CGF.CGM.getLLVMLifetimeEndFn(Addr->getType()),
+                              Size, Addr)
         ->setDoesNotThrow();
     }
   };
@@ -924,8 +923,7 @@ CodeGenFunction::EmitAutoVarAlloca(const VarDecl &D) {
         llvm::Value *sizeV = llvm::ConstantInt::get(Int64Ty, size);
 
         emission.SizeForLifetimeMarkers = sizeV;
-        llvm::Value *castAddr = Builder.CreateBitCast(Alloc, Int8PtrTy);
-        Builder.CreateCall2(CGM.getLLVMLifetimeStartFn(), sizeV, castAddr)
+        Builder.CreateCall2(CGM.getLLVMLifetimeStartFn(Alloc->getType()), sizeV, Alloc)
           ->setDoesNotThrow();
       } else {
         assert(!emission.useLifetimeMarkers());
@@ -1581,19 +1579,13 @@ void CodeGenFunction::pushRegularPartialArrayCleanup(llvm::Value *arrayBegin,
 }
 
 /// Lazily declare the @llvm.lifetime.start intrinsic.
-llvm::Constant *CodeGenModule::getLLVMLifetimeStartFn() {
-  if (LifetimeStartFn) return LifetimeStartFn;
-  LifetimeStartFn = llvm::Intrinsic::getDeclaration(&getModule(),
-                                            llvm::Intrinsic::lifetime_start);
-  return LifetimeStartFn;
+llvm::Constant *CodeGenModule::getLLVMLifetimeStartFn(llvm::Type* ptrType) {
+  return llvm::Intrinsic::getDeclaration(&getModule(), llvm::Intrinsic::lifetime_start, ptrType);
 }
 
 /// Lazily declare the @llvm.lifetime.end intrinsic.
-llvm::Constant *CodeGenModule::getLLVMLifetimeEndFn() {
-  if (LifetimeEndFn) return LifetimeEndFn;
-  LifetimeEndFn = llvm::Intrinsic::getDeclaration(&getModule(),
-                                              llvm::Intrinsic::lifetime_end);
-  return LifetimeEndFn;
+llvm::Constant *CodeGenModule::getLLVMLifetimeEndFn(llvm::Type* ptrType) {
+  return llvm::Intrinsic::getDeclaration(&getModule(), llvm::Intrinsic::lifetime_end, ptrType);
 }
 
 namespace {

lib/CodeGen/CodeGenModule.h
@@ -422,12 +422,6 @@ class CodeGenModule : public CodeGenTypeCache {
     int GlobalUniqueCount;
   } Block;
 
-  /// void @llvm.lifetime.start(i64 %size, i8* nocapture <ptr>)
-  llvm::Constant *LifetimeStartFn;
-
-  /// void @llvm.lifetime.end(i64 %size, i8* nocapture <ptr>)
-  llvm::Constant *LifetimeEndFn;
-
   GlobalDecl initializedGlobalDecl;
 
   std::unique_ptr<llvm::SpecialCaseList> SanitizerBlacklist;
@@ -840,8 +834,8 @@ public:
 
   ///@}
 
-  llvm::Constant *getLLVMLifetimeStartFn();
-  llvm::Constant *getLLVMLifetimeEndFn();
+  llvm::Constant *getLLVMLifetimeStartFn(llvm::Type* ptrType);
+  llvm::Constant *getLLVMLifetimeEndFn(llvm::Type* ptrType);
 
   // UpdateCompleteType - Make sure that this type is translated.
   void UpdateCompletedType(const TagDecl *TD);

leaningtech/cheerp-compiler

clang/lib/CodeGen/CGDecl.cpp
@@ -1346,9 +1346,8 @@ llvm::Value *CodeGenFunction::EmitLifetimeStart(llvm::TypeSize Size,
          "Pointer should be in alloca address space");
   llvm::Value *SizeV = llvm::ConstantInt::get(
       Int64Ty, Size.isScalable() ? -1 : Size.getFixedValue());
-  Addr = Builder.CreateBitCast(Addr, AllocaInt8PtrTy);
   llvm::CallInst *C =
-      Builder.CreateCall(CGM.getLLVMLifetimeStartFn(), {SizeV, Addr});
+      Builder.CreateCall(CGM.getLLVMLifetimeStartFn(Addr->getType()), {SizeV, Addr});
   C->setDoesNotThrow();
   return SizeV;
 }
@@ -1357,9 +1356,8 @@ void CodeGenFunction::EmitLifetimeEnd(llvm::Value *Size, llvm::Value *Addr) {
   assert(Addr->getType()->getPointerAddressSpace() ==
              CGM.getDataLayout().getAllocaAddrSpace() &&
          "Pointer should be in alloca address space");
-  Addr = Builder.CreateBitCast(Addr, AllocaInt8PtrTy);
   llvm::CallInst *C =
-      Builder.CreateCall(CGM.getLLVMLifetimeEndFn(), {Size, Addr});
+      Builder.CreateCall(CGM.getLLVMLifetimeEndFn(Addr->getType()), {Size, Addr});
   C->setDoesNotThrow();
 }
 
@@ -2415,21 +2413,13 @@ void CodeGenFunction::pushRegularPartialArrayCleanup(llvm::Value *arrayBegin,
 }
 
 /// Lazily declare the @llvm.lifetime.start intrinsic.
-llvm::Function *CodeGenModule::getLLVMLifetimeStartFn() {
-  if (LifetimeStartFn)
-    return LifetimeStartFn;
-  LifetimeStartFn = llvm::Intrinsic::getDeclaration(&getModule(),
-    llvm::Intrinsic::lifetime_start, AllocaInt8PtrTy);
-  return LifetimeStartFn;
+llvm::Function *CodeGenModule::getLLVMLifetimeStartFn(llvm::Type* ptrType) {
+  return llvm::Intrinsic::getDeclaration(&getModule(), llvm::Intrinsic::lifetime_start, ptrType);
 }
 
 /// Lazily declare the @llvm.lifetime.end intrinsic.
-llvm::Function *CodeGenModule::getLLVMLifetimeEndFn() {
-  if (LifetimeEndFn)
-    return LifetimeEndFn;
-  LifetimeEndFn = llvm::Intrinsic::getDeclaration(&getModule(),
-    llvm::Intrinsic::lifetime_end, AllocaInt8PtrTy);
-  return LifetimeEndFn;
+llvm::Function *CodeGenModule::getLLVMLifetimeEndFn(llvm::Type* ptrType) {
+  return llvm::Intrinsic::getDeclaration(&getModule(), llvm::Intrinsic::lifetime_end, ptrType);
 }
 
 namespace {

clang/lib/CodeGen/CodeGenModule.h
@@ -569,12 +569,6 @@ private:
 
   /// @}
 
-  /// void @llvm.lifetime.start(i64 %size, i8* nocapture <ptr>)
-  llvm::Function *LifetimeStartFn = nullptr;
-
-  /// void @llvm.lifetime.end(i64 %size, i8* nocapture <ptr>)
-  llvm::Function *LifetimeEndFn = nullptr;
-
   std::unique_ptr<SanitizerMetadata> SanitizerMD;
 
   llvm::MapVector<const Decl *, bool> DeferredEmptyCoverageMappingDecls;
@@ -1170,8 +1164,8 @@ public:
 
   ///@}
 
-  llvm::Function *getLLVMLifetimeStartFn();
-  llvm::Function *getLLVMLifetimeEndFn();
+  llvm::Function *getLLVMLifetimeStartFn(llvm::Type* ptrType);
+  llvm::Function *getLLVMLifetimeEndFn(llvm::Type* ptrType);
 
   // Make sure that this type is translated.
   void UpdateCompletedType(const TagDecl *TD);