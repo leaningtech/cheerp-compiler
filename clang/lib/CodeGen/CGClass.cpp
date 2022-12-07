leaningtech/cheerp-clang

Improve type safety of vtable access

lib/CodeGen/CGClass.cpp
@@ -2127,10 +2127,22 @@ void CodeGenFunction::InitializeVTablePointers(const CXXRecordDecl *RD) {
 
 llvm::Value *CodeGenFunction::GetVTablePtr(llvm::Value *This,
                                            llvm::Type *Ty) {
-  llvm::Value *VTablePtrSrc = Builder.CreateBitCast(This, Ty->getPointerTo());
+  //HACK: Not really clean, it will iterate until structs are found on the first element
+  SmallVector<llvm::Value*, 4> GEPIndexes;
+  llvm::Type* t=This->getType();
+  assert(t->isPointerTy());
+  GEPIndexes.push_back(llvm::ConstantInt::get(Int32Ty, 0));
+  t=cast<llvm::PointerType>(t)->getElementType();
+  while(t->isStructTy())
+  {
+    GEPIndexes.push_back(llvm::ConstantInt::get(Int32Ty, 0));
+    llvm::StructType* st=cast<llvm::StructType>(t);
+    t=st->getElementType(0);
+  }
+  llvm::Value *VTablePtrSrc = Builder.CreateGEP(This, GEPIndexes);
   llvm::Instruction *VTable = Builder.CreateLoad(VTablePtrSrc, "vtable");
   CGM.DecorateInstruction(VTable, CGM.getTBAAInfoForVTablePtr());
-  return VTable;
+  return Builder.CreateBitCast(VTable, Ty);
 }
 
 static const CXXRecordDecl *getMostDerivedClassDecl(const Expr *Base) {

leaningtech/cheerp-compiler

clang/lib/CodeGen/CGClass.cpp
@@ -2814,7 +2814,24 @@ void CodeGenFunction::InitializeVTablePointers(const CXXRecordDecl *RD) {
 llvm::Value *CodeGenFunction::GetVTablePtr(Address This,
                                            llvm::Type *VTableTy,
                                            const CXXRecordDecl *RD) {
-  Address VTablePtrSrc = Builder.CreateElementBitCast(This, VTableTy);
+  //HACK: Not really clean, it will iterate until structs are found on the first element
+  SmallVector<llvm::Value*, 4> GEPIndexes;
+  llvm::Type* t=This->getType();
+  assert(t->isPointerTy());
+  GEPIndexes.push_back(llvm::ConstantInt::get(Int32Ty, 0));
+  t=cast<llvm::PointerType>(t)->getElementType();
+  while(t->isStructTy())
+  {
+    GEPIndexes.push_back(llvm::ConstantInt::get(Int32Ty, 0));
+    llvm::StructType* st=cast<llvm::StructType>(t);
+    t=st->getElementType(0);
+  }
+  llvm::Value *VTablePtrSrc = Builder.CreateGEP(This, GEPIndexes);
+  if(!VTablePtrSrc->getType()->getPointerElementType()->isPointerTy())
+  {
+    // We did not find a pointer, use the type unsafe code path
+    VTablePtrSrc = Builder.CreateElementBitCast(This, VTableTy);
+  }
   llvm::Instruction *VTable = Builder.CreateLoad(VTablePtrSrc, "vtable");
   TBAAAccessInfo TBAAInfo = CGM.getTBAAVTablePtrAccessInfo(VTableTy);
   CGM.DecorateInstructionWithTBAA(VTable, TBAAInfo);
@@ -2823,7 +2840,7 @@ llvm::Value *CodeGenFunction::GetVTablePtr(Address This,
       CGM.getCodeGenOpts().StrictVTablePointers)
     CGM.DecorateInstructionWithInvariantGroup(VTable, RD);
 
-  return VTable;
+  return Builder.CreateBitCast(VTable, VTableTy);
 }
 
 // If a class has a single non-virtual base and does not introduce or override