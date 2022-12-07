leaningtech/cheerp-clang

Add support for the static attribute

lib/Sema/SemaDeclAttr.cpp
@@ -4019,6 +4019,14 @@ static void handleServer(Sema &S, Decl *D, const AttributeList &Attr)
     S.Diag(Attr.getLoc(), diag::err_duetto_attribute_not_on_function);
 }
 
+static void handleStatic(Sema &S, Decl *D, const AttributeList &Attr)
+{
+  D->addAttr(::new (S.Context) StaticAttr(Attr.getRange(), S.Context, Attr.getAttributeSpellingListIndex()));
+  //This should be a function
+  if (!isa<FunctionDecl>(D))
+    S.Diag(Attr.getLoc(), diag::err_duetto_attribute_not_on_function);
+}
+
 static void handleNoInit(Sema &S, Decl* D, const AttributeList &Attr)
 {
   D->addAttr(::new (S.Context) NoInitAttr(Attr.getRange(), S.Context, Attr.getAttributeSpellingListIndex()));
@@ -4048,7 +4056,8 @@ static void ProcessDeclAttribute(Sema &S, Scope *scope, Decl *D,
   // instead.
   if (Attr.isCXX11Attribute() && !IncludeCXX11Attributes &&
       Attr.getKind()!=AttributeList::AT_Server &&
-      Attr.getKind()!=AttributeList::AT_Client)
+      Attr.getKind()!=AttributeList::AT_Client &&
+      Attr.getKind()!=AttributeList::AT_Static)
   {
     return;
   }
@@ -4534,6 +4543,9 @@ static void ProcessDeclAttribute(Sema &S, Scope *scope, Decl *D,
     break;
   case AttributeList::AT_Server:
     handleServer(S, D, Attr);
+    break;
+  case AttributeList::AT_Static:
+    handleStatic(S, D, Attr);
     break;
   case AttributeList::AT_NoInit:
     handleNoInit(S, D, Attr);

lib/Sema/SemaType.cpp
@@ -4873,6 +4873,7 @@ static void processTypeAttrs(TypeProcessingState &state, QualType &type,
     case AttributeList::IgnoredAttribute:
     case AttributeList::AT_Server:
     case AttributeList::AT_Client:
+    case AttributeList::AT_Static:
       break;
 
     case AttributeList::AT_MayAlias:

leaningtech/cheerp-compiler

clang/lib/Sema/SemaDeclAttr.cpp
@@ -8423,6 +8423,14 @@ EnforceTCBLeafAttr *Sema::mergeEnforceTCBLeafAttr(
       *this, D, AL);
 }
 
+static void handleStatic(Sema &S, Decl *D, const AttributeList &Attr)
+{
+  D->addAttr(::new (S.Context) StaticAttr(Attr.getRange(), S.Context, Attr.getAttributeSpellingListIndex()));
+  //This should be a function
+  if (!isa<FunctionDecl>(D))
+    S.Diag(Attr.getLoc(), diag::err_duetto_attribute_not_on_function);
+}
+
 static void handleNoInit(Sema &S, Decl* D, const AttributeList &Attr)
 {
   D->addAttr(::new (S.Context) NoInitAttr(Attr.getRange(), S.Context, Attr.getAttributeSpellingListIndex()));
@@ -8487,8 +8495,11 @@ ProcessDeclAttribute(Sema &S, Scope *scope, Decl *D, const ParsedAttr &AL,
   // `DeclSpec`, so we need to let it through here to make sure it is processed
   // appropriately. Once the behavior of isCXX11Attribute() is fixed, we can
   // go back to using that here.
-  if (AL.getSyntax() == ParsedAttr::AS_CXX11 && !Options.IncludeCXX11Attributes)
+  if (AL.getSyntax() == ParsedAttr::AS_CXX11 && !Options.IncludeCXX11Attributes &&
+      AL.getKind()!=AttributeList::AT_Static)
+  {
     return;
+  }
 
   // Unknown attributes are automatically warned on. Target-specific attributes
   // which do not apply to the current target architecture are treated as
@@ -9244,11 +9255,14 @@ ProcessDeclAttribute(Sema &S, Scope *scope, Decl *D, const ParsedAttr &AL,
     break;
 
   // Cheerp attributes
+  case AttributeList::AT_Static:
+    handleStatic(S, D, AL);
+    break;
   case AttributeList::AT_NoInit:
-    handleNoInit(S, D, Attr);
+    handleNoInit(S, D, AL);
     break;
   case AttributeList::AT_JsExport:
-    handleJsExportAttr(S, D, Attr);
+    handleJsExportAttr(S, D, AL);
     break;
   }
 }

clang/lib/Sema/SemaType.cpp
@@ -8379,6 +8379,7 @@ static void processTypeAttrs(TypeProcessingState &state, QualType &type,
       break;
 
     case ParsedAttr::IgnoredAttribute:
+    case ParsedAttr::AT_Static:
       break;
 
     case ParsedAttr::AT_BTFTypeTag: