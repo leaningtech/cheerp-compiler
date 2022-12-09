fa1eca13b7cbdc23629e2228b876ce89d7a949a9
Mark Peerdeman <mark.peerdeman@leaningtech.com>
2022-12-09 14:58:25 +0100

    LoopVectorizer - removed check for isByteAddressable

--- llvm/lib/Transforms/Vectorize/LoopVectorize.cpp
+++ llvm/lib/Transforms/Vectorize/LoopVectorize.cpp
@@ -10577,13 +10577,6 @@ LoopVectorizeResult LoopVectorizePass::runImpl(
       TTI->getMaxInterleaveFactor(1) < 2)
     return LoopVectorizeResult(false, false);
 
-  //Cheerp: currently JS does not support vector instructions,
-  //they might be supported in the future though.
-  const DataLayout &DL = F.getParent()->getDataLayout();
-  if (!DL.isByteAddressable() && F.getSection() != "asmjs") {
-    LLVM_DEBUG(dbgs() << "LV: Not vectorizing on NBA target");
-    return LoopVectorizeResult(false, false);
-  }
   bool Changed = false, CFGChanged = false;
 
   // The vectorizer requires loops to be in simplified form.
