6f6adb7d635a69401bf024805e139fd711d397c8
Yuri Iozzelli <y.iozzelli@gmail.com>
2023-03-06 11:31:28 +0100

    Update license to Apache 2.0 with LLVM Exceptions.
    Most of the files were under the old LLVM license (University of
    Illinois). cheerpintrin.h was something else (GPL?) and is now like the
    rest.

--- libcxxabi/src/cxa_cheerp.cpp
+++ libcxxabi/src/cxa_cheerp.cpp
@@ -1,3 +1,14 @@
+//===-- cxa_cheerp.cpp - Cheerp-specific ABI implementation ----===//
+//
+//                     Cheerp: The C++ compiler for the Web
+//
+// This file is distributed under the Apache License v2.0 with LLVM Exceptions.
+// See LICENSE.TXT for details.
+//
+// Copyright 2011-2023 Leaning Technlogies
+//
+//===----------------------------------------------------------------------===//
+
 #include "unwind.h"
 #include "private_typeinfo.h"
 #include <typeinfo>
