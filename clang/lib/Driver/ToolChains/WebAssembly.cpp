leaningtech/cheerp-compiler

commit ea5f8c0df0a2bf0c67846f9796ebeb6cfaf71dda

Implement generation of a minimal .d.ts file

clang/lib/Driver/ToolChains/WebAssembly.cpp
@@ -843,6 +843,19 @@ void cheerp::CheerpCompiler::ConstructJob(Compilation &C, const JobAction &JA,
   if(Arg* cheerpSecondaryOutputPath = Args.getLastArg(options::OPT_cheerp_secondary_output_path_EQ))
     cheerpSecondaryOutputPath->render(Args, CmdArgs);
 
+  if(Arg* cheerpDTSOutputFile = Args.getLastArg(options::OPT_cheerp_dts_output_file_EQ))
+    cheerpDTSOutputFile->render(Args, CmdArgs);
+  else
+  {
+    SmallString<64> path(Output.getFilename());
+    StringRef ext = ".d.ts";
+    if (llvm::sys::path::extension(path) == ext)
+      path.append(ext);
+    else
+      llvm::sys::path::replace_extension(path, ext);
+    CmdArgs.push_back(Args.MakeArgString(Twine("-cheerp-dts-output-file=")+path));
+  }
+
   if(Args.getLastArg(options::OPT_cheerp_make_module)) {
     CmdArgs.push_back("-cheerp-make-module=closure");
   }
@@ -948,6 +961,8 @@ void cheerp::CheerpCompiler::ConstructJob(Compilation &C, const JobAction &JA,
     cheerpFixFuncCasts->render(Args, CmdArgs);
   if(Arg* cheerpUseBigInts = Args.getLastArg(options::OPT_cheerp_use_bigints))
     cheerpUseBigInts->render(Args, CmdArgs);
+  if(Arg* cheerpMakeDTS = Args.getLastArg(options::OPT_cheerp_make_dts))
+    cheerpMakeDTS->render(Args, CmdArgs);
   else if (getToolChain().getTriple().getOS() == llvm::Triple::WASI)
     CmdArgs.push_back("-cheerp-use-bigints");
 

clang/include/clang/Driver/Options.td
@@ -4939,6 +4939,8 @@ def cheerp_secondary_output_file_EQ : Joined<["-"], "cheerp-secondary-output-fil
   HelpText<"Write the secondary output file (wasm module or asm.js memory file) to <file>">, MetaVarName<"<file>">;
 def cheerp_secondary_output_path_EQ : Joined<["-"], "cheerp-secondary-output-path=">, Flags<[NoXarchOption]>,
   HelpText<"Assume the secondary output file (wasm module or asm.js memory file) to be in path <path> at runtime">, MetaVarName<"<path>">;
+def cheerp_dts_output_file_EQ : Joined<["-"], "cheerp-dts-output-file=">, Flags<[NoXarchOption]>,
+  HelpText<"Write the .d.ts output file to <file>">, MetaVarName<"<file>">;
 def cheerp_linear_heap_size : Joined<["-"], "cheerp-linear-heap-size=">, Flags<[NoXarchOption]>,
   HelpText<"Set wasm/asm.js heap size (in MB, default is 8)">;
 def cheerp_linear_stack_size : Joined<["-"], "cheerp-linear-stack-size=">, Flags<[NoXarchOption]>,
@@ -4957,6 +4959,8 @@ def cheerp_make_module : Flag<["-"], "cheerp-make-module">, Flags<[NoXarchOption
   HelpText<"Create a closure around JS to avoid global namespace pollution">;
 def cheerp_make_module_EQ : Joined<["-"], "cheerp-make-module=">, Flags<[NoXarchOption]>,
   HelpText<"Expose the compiled code as a [closure/commonjs/es6] module">;
+def cheerp_make_dts : Flag<["-"], "cheerp-make-dts">, Flags<[NoXarchOption]>,
+  HelpText<"Generate a .d.ts file for use with TypeScript">;
 def cheerp_no_lto : Flag<["-"], "cheerp-no-lto">, Flags<[NoXarchOption]>,
   HelpText<"Disable final optimization step at link time">;
 def cheerp_dump_bc : Flag<["-"], "cheerp-dump-bc">, Flags<[NoXarchOption]>,

llvm/include/llvm/Cheerp/CommandLine.h
@@ -21,9 +21,11 @@ enum LinearOutputTy {
 extern llvm::cl::opt<LinearOutputTy> LinearOutput;
 extern llvm::cl::opt<std::string> SecondaryOutputFile;
 extern llvm::cl::opt<std::string> SecondaryOutputPath;
+extern llvm::cl::opt<std::string> DTSOutputFile;
 extern llvm::cl::opt<std::string> SourceMap;
 extern llvm::cl::opt<std::string> SourceMapPrefix;
 extern llvm::cl::opt<std::string> MakeModule;
+extern llvm::cl::opt<bool> MakeDTS;
 extern llvm::cl::opt<bool> WasmOnly;
 extern llvm::cl::opt<bool> SourceMapStandAlone;
 extern llvm::cl::opt<bool> PrettyCode;

llvm/include/llvm/Cheerp/DTSWriter.h
@@ -1,0 +1,35 @@
+//===-- Cheerp/DTSWriter.h - The Cheerp TypeScript declaration generator --===//
+//
+//                     Cheerp: The C++ compiler for the Web
+//
+// This file is distributed under the Apache License v2.0 with LLVM Exceptions.
+// See LICENSE.TXT for details.
+//
+// Copyright 2023-2023 Leaning Technologies
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef _CHEERP_DTS_WRITER_H
+#define _CHEERP_DTS_WRITER_H
+
+#include "llvm/Cheerp/Writer.h"
+
+namespace cheerp
+{
+
+class CheerpDTSWriter final
+{
+public:
+  ostream_proxy stream;
+
+  CheerpDTSWriter(llvm::raw_ostream& s, SourceMapGenerator* sourceMapGenerator, bool readableOutput):
+    stream(s, sourceMapGenerator, readableOutput)
+  {
+  }
+
+  void makeDTS();
+};
+
+}
+
+#endif

llvm/lib/CheerpUtils/CommandLine.cpp
@@ -14,6 +14,8 @@ llvm::cl::opt<std::string> SecondaryOutputFile("cheerp-secondary-output-file", l
 llvm::cl::opt<std::string> SecondaryOutputPath("cheerp-secondary-output-path", llvm::cl::Optional,
   llvm::cl::desc("If specified, the runtime relative path of the secondary output file"), llvm::cl::value_desc("filename"));
 
+llvm::cl::opt<std::string> DTSOutputFile("cheerp-dts-output-file", llvm::cl::Optional, llvm::cl::desc("If specified, the file name of the .d.ts output file"));
+
 llvm::cl::opt<std::string> SourceMap("cheerp-sourcemap", llvm::cl::Optional,
   llvm::cl::desc("If specified, the file name of the source map"), llvm::cl::value_desc("filename"));
 
@@ -21,6 +23,8 @@ llvm::cl::opt<std::string> SourceMapPrefix("cheerp-sourcemap-prefix", llvm::cl::
   llvm::cl::desc("If specified, this prefix will be removed from source map file paths"), llvm::cl::value_desc("path"));
 
 llvm::cl::opt<std::string> MakeModule("cheerp-make-module", llvm::cl::Optional, llvm::cl::desc("Create a [closure/commonjs] module around the generated code.") );
+
+llvm::cl::opt<bool> MakeDTS("cheerp-make-dts", llvm::cl::desc("Generate a .d.ts file for use by TypeScript"));
 
 llvm::cl::opt<bool> SourceMapStandAlone("cheerp-sourcemap-standalone", llvm::cl::desc("Generate a standalone sourcemap by including _all_ sources in the map file") );
 

llvm/lib/CheerpWriter/CMakeLists.txt
@@ -3,6 +3,7 @@ add_llvm_component_library(LLVMCheerpWriter
   CheerpBaseWriter.cpp
   CheerpWriter.cpp
   CheerpWasmWriter.cpp
+  CheerpDTSWriter.cpp
   JSInterop.cpp
   NameGenerator.cpp
   Types.cpp

llvm/lib/CheerpWriter/CheerpDTSWriter.cpp
@@ -1,0 +1,23 @@
+//===-- CheerpDTSWriter.cpp - The Cheerp TypeScript declaration generator -===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the Apache License v2.0 with LLVM Exceptions.
+// See LICENSE.TXT for details.
+//
+// Copyright 2023-2023 Leaning Technologies
+//
+//===----------------------------------------------------------------------===//
+
+#include "llvm/Cheerp/DTSWriter.h"
+
+using namespace llvm;
+using namespace std;
+using namespace cheerp;
+
+static const NewLineHandler NewLine;
+
+void CheerpDTSWriter::makeDTS()
+{
+  stream << "export default function(): Promise<object>;" << NewLine;
+}

llvm/lib/Target/CheerpBackend/CheerpBackend.cpp
@@ -25,6 +25,7 @@
 #include "llvm/MC/TargetRegistry.h"
 #include "llvm/Cheerp/Writer.h"
 #include "llvm/Cheerp/WasmWriter.h"
+#include "llvm/Cheerp/DTSWriter.h"
 #include "llvm/Cheerp/PassRegistry.h"
 #include "llvm/Cheerp/PassUtility.h"
 #include "llvm/Cheerp/I64Lowering.h"
@@ -226,6 +227,14 @@ PreservedAnalyses cheerp::CheerpWritePassImpl::run(Module& M, ModuleAnalysisMana
   {
     secondaryOut.reset(new formatted_raw_ostream(Out));
   }
+  
+  std::error_code dtsErrorCode;
+  llvm::ToolOutputFile dtsFile(DTSOutputFile, dtsErrorCode, sys::fs::OF_None);
+  std::unique_ptr<llvm::formatted_raw_ostream> dtsOut;
+  if (!DTSOutputFile.empty())
+  {
+    dtsOut.reset(new formatted_raw_ostream(dtsFile.os()));
+  }
 
   // Build the ordered list of reserved names
   std::vector<std::string> reservedNames(ReservedNames.begin(), ReservedNames.end());
@@ -253,6 +262,12 @@ PreservedAnalyses cheerp::CheerpWritePassImpl::run(Module& M, ModuleAnalysisMana
       break;
   }
 
+  if (MakeDTS && dtsOut)
+  {
+    cheerp::CheerpDTSWriter dtsWriter(*dtsOut, sourceMapGenerator.get(), PrettyCode);
+    dtsWriter.makeDTS();
+  }
+
   if (!WasmOnly)
   {
     cheerp::CheerpWriter writer(M, MAM, Out, PA, registerize, GDA, linearHelper, namegen, allocaStoresExtractor, IW.getLandingPadTable(), memOut, asmjsMemFile,
@@ -270,6 +285,7 @@ PreservedAnalyses cheerp::CheerpWritePassImpl::run(Module& M, ModuleAnalysisMana
                                     WasmExportedTable);
     wasmWriter.makeWasm();
   }
+
   allocaStoresExtractor.destroyStores();
  
   if (!SecondaryOutputFile.empty() && ErrorCode)
@@ -278,9 +294,15 @@ PreservedAnalyses cheerp::CheerpWritePassImpl::run(Module& M, ModuleAnalysisMana
     llvm::report_fatal_error(StringRef(ErrorCode.message()), false);
     return PreservedAnalyses::none();
   }
+  if (!DTSOutputFile.empty() && dtsErrorCode)
+  {
+    llvm::report_fatal_error(StringRef(dtsErrorCode.message()), false);
+    return PreservedAnalyses::none();
+  }
   if (!WasmOnly)
     secondaryFile.keep();
-
+  if (MakeDTS)
+    dtsFile.keep();
 
 
 	return PreservedAnalyses::none();

commit 0d6ef9483b301a751ac8fbf2ac0a008c2143d375

fix incorrectly matched else if
Introduced in: ea5f8c0df0a2b

This fixes the location of an else if statement, making sure that WASI
always uses bigints.

clang/lib/Driver/ToolChains/WebAssembly.cpp
@@ -961,10 +961,10 @@ void cheerp::CheerpCompiler::ConstructJob(Compilation &C, const JobAction &JA,
     cheerpFixFuncCasts->render(Args, CmdArgs);
   if(Arg* cheerpUseBigInts = Args.getLastArg(options::OPT_cheerp_use_bigints))
     cheerpUseBigInts->render(Args, CmdArgs);
-  if(Arg* cheerpMakeDTS = Args.getLastArg(options::OPT_cheerp_make_dts))
-    cheerpMakeDTS->render(Args, CmdArgs);
   else if (getToolChain().getTriple().getOS() == llvm::Triple::WASI)
     CmdArgs.push_back("-cheerp-use-bigints");
+  if(Arg* cheerpMakeDTS = Args.getLastArg(options::OPT_cheerp_make_dts))
+    cheerpMakeDTS->render(Args, CmdArgs);
 
   // Set output to binary mode to avoid linefeed conversion on Windows.
   CmdArgs.push_back("-filetype");