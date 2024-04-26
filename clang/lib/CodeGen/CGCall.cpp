d637b4d70a788d772f44b3d83df1c0ec27c0bef4
Yuri Iozzelli <y.iozzelli@gmail.com>
2024-04-26 10:20:16 +0200

    clang: put locals and args in a non-default address space

--- clang/lib/CodeGen/CGCall.cpp
+++ clang/lib/CodeGen/CGCall.cpp
@@ -5023,9 +5023,16 @@ RValue CodeGenFunction::EmitCall(const CGFunctionInfo &CallInfo,
           I->copyInto(*this, AI);
         } else {
           // Skip the extra memcpy call.
+          unsigned AS = CGM.getDataLayout().getAllocaAddrSpace(); 
+          if (getLangOpts().Cheerp) {
+            if (IRFuncTy->getNumParams() <= FirstIRArg) {
+              AS = IRFuncTy->getParamType(FirstIRArg)->getPointerAddressSpace();
+            } else {
+              AS = V->getType()->getPointerAddressSpace();
+            }
+          }
           auto *T = llvm::PointerType::getWithSamePointeeType(
-              cast<llvm::PointerType>(V->getType()),
-              CGM.getDataLayout().getAllocaAddrSpace());
+              cast<llvm::PointerType>(V->getType()), AS);
 
           llvm::Value *Val = getTargetHooks().performAddrSpaceCast(
               *this, V, LangAS::Default, CGM.getASTAllocaAddressSpace(), T,
