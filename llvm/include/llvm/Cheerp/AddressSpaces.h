08d909c746ca29bee5d4c881dbf939a3fcec9a46
Yuri Iozzelli <y.iozzelli@gmail.com>
2024-05-15 12:06:14 +0200

    WIP use AS in TypeSupport

--- llvm/include/llvm/Cheerp/AddressSpaces.h
+++ llvm/include/llvm/Cheerp/AddressSpaces.h
@@ -12,6 +12,10 @@
 #ifndef _CHEERP_ADDRESS_SPACES_H
 #define _CHEERP_ADDRESS_SPACES_H
 
+#include "llvm/IR/DerivedTypes.h"
+#include "llvm/IR/Value.h"
+#include <cassert>
+
 namespace cheerp {
 
 enum class CheerpAS {
@@ -21,6 +25,16 @@ enum class CheerpAS {
 	ByteLayout = 3,
 	Wasm = 4,
 };
+
+inline CheerpAS getCheerpAS(const llvm::PointerType* t) {
+	unsigned AS = t->getAddressSpace();
+	assert(AS <= unsigned(CheerpAS::Wasm));
+	return static_cast<CheerpAS>(AS);
+}
+inline CheerpAS getCheerpAS(const llvm::Value* v) {
+	return getCheerpAS(llvm::cast<llvm::PointerType>(v->getType()));
+}
+
 
 } // namespace cheerp
 
