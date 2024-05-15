f403a10dcc042e3efc0e39dcceed723c97f5200b
Yuri Iozzelli <y.iozzelli@gmail.com>
2024-05-15 12:12:21 +0200

    Writer: mark HEAP8 as used if any asmjs global present

--- llvm/lib/CheerpWriter/CheerpWriter.cpp
+++ llvm/lib/CheerpWriter/CheerpWriter.cpp
@@ -6011,6 +6011,7 @@ void CheerpWriter::compileGlobalAsmJS(const GlobalVariable& G)
 
 void CheerpWriter::compileGlobalsInitAsmJS()
 {
+	markHeapNameAsUsed(HEAP8);
 	if (asmJSMem)
 	{
 		ostream_proxy os(*asmJSMem, nullptr, false);
