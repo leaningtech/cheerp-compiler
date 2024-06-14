d1714e16ffff9b9c219d4495912df41417ad6da6
Yuri Iozzelli <y.iozzelli@gmail.com>
2024-06-14 15:13:41 +0200

    fix(CodeGen): propagate AS when bitcasting constant initializers

--- clang/lib/CodeGen/CGExprConstant.cpp
+++ clang/lib/CodeGen/CGExprConstant.cpp
@@ -2012,7 +2012,8 @@ private:
 	  if (CurClass->isUnionType()) {
             // Apply a bitcast, on unions it is safe
             C = llvm::ConstantExpr::getGetElementPtr(CElementType, C, Indexes);
-	    C = llvm::ConstantExpr::getBitCast(C, CGM.getTypes().ConvertTypeForMem(CurType)->getPointerTo());
+            unsigned AS = C->getType()->getPointerAddressSpace();
+	    C = llvm::ConstantExpr::getBitCast(C, CGM.getTypes().ConvertTypeForMem(CurType)->getPointerTo(AS));
             CElementType = CGM.getTypes().ConvertTypeForMem(CurType);
 
 	    Indexes.clear();
@@ -2026,7 +2027,8 @@ private:
           if(fieldIndex == -1) {
             // Collapsed struct
             CElementType = CGM.getTypes().ConvertTypeForMem(CurType);
-            C = llvm::ConstantExpr::getBitCast(C, CElementType->getPointerTo());
+            unsigned AS = C->getType()->getPointerAddressSpace();
+            C = llvm::ConstantExpr::getBitCast(C, CElementType->getPointerTo(AS));
             continue;
           }
           Indexes.push_back(llvm::ConstantInt::get(CGM.Int32Ty, fieldIndex));
