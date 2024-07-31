b1bcacbd4bfcc977dbee0d724c4a368b3474ec5a
Yuri Iozzelli <y.iozzelli@gmail.com>
2024-07-31 17:31:32 +0200

    feat: rework bytelayout and union handling
    Unions are not in the bl address space by default, but accessing their
    ields implicitly casts to bl. if the field is a struct or marked bl,
    then the AS remains bl, otherwise there is an immediate
    cheerp_typed_ptrcast back to genericjs.
    
    This design allows to optimize unions to be just a genericjs array if
    all the possible accesses have the same type and offset (not done in
    this commit).
    The cheerp_typed_ptrcast is necessary to add a wrapper typed array with
    the proper type in that location

--- clang/lib/Sema/SemaDecl.cpp
+++ clang/lib/Sema/SemaDecl.cpp
@@ -18218,9 +18218,6 @@ FieldDecl *Sema::CheckFieldDecl(DeclarationName Name, QualType T,
 
   NewFD->setAccess(AS);
 
-  if (getLangOpts().Cheerp && T->isRecordType() && T->getAsRecordDecl()->isByteLayout())
-    deduceCheerpAddressSpace(NewFD);
-
   return NewFD;
 }
 
@@ -18701,6 +18698,10 @@ void Sema::ActOnFields(Scope *S, SourceLocation RecLoc, Decl *EnclosingDecl,
     if (!FD->isAnonymousStructOrUnion()) {
       // Remember all fields written by the user.
       RecFields.push_back(FD);
+    }
+
+    if (LangOpts.Cheerp) {
+      deduceCheerpAddressSpace(FD);
     }
 
     // If the field is already invalid for some reason, don't emit more
