66d0d9ffb9144a3ba3d50ef38737ea4f1b6a6a18
Yuri Iozzelli <y.iozzelli@gmail.com>
2024-08-09 10:11:30 +0200

    feat(Writer): remove one use of getNonOpaquePointerElementType

--- llvm/lib/CheerpWriter/Opcodes.cpp
+++ llvm/lib/CheerpWriter/Opcodes.cpp
@@ -370,7 +370,7 @@ void CheerpWriter::compileBitCastOffset(const llvm::User* bi, PARENT_PRIORITY pa
 	if(PA.getPointerKind(bi->getOperand(0)) == BYTE_LAYOUT)
 	{
 		//Find the type
-		llvm::Type* elementType = dst->getNonOpaquePointerElementType();
+		llvm::Type* elementType = cast<CallInst>(bi)->getRetElementType();
 		bool isArray=isa<ArrayType>(elementType);
 		llvm::Type* pointedType = (isArray)?elementType->getArrayElementType():elementType;
 		if(TypeSupport::isTypedArrayType(pointedType, /* forceTypedArray*/ true))
