aaa7e5a6658121428ea06ce61ac6667da21e8ebf
Yuri Iozzelli <y.iozzelli@gmail.com>
2024-11-19 14:13:54 +0100

    fix(CallConstructors): initialize constructors before calling spawnUtility
    otherwise globals used by the utility thread will be reset on
    initialization

--- llvm/lib/CheerpUtils/CallConstructors.cpp
+++ llvm/lib/CheerpUtils/CallConstructors.cpp
@@ -52,14 +52,6 @@ PreservedAnalyses CallConstructorsPass::run(llvm::Module &M, llvm::ModuleAnalysi
 	// Add a call to initialise the tls to _start.
 	Function* cheerpInitTls = cast<Function>(M.getOrInsertFunction("__cheerp_init_tls", Ty).getCallee());
 	Builder.CreateCall(Ty, cheerpInitTls);
-	if (!LowerAtomics)
-	{
-		// If -pthread is passed, add a call to spawnUtility to setup the utility thread.
-		Function* spawnUtility = cast<Function>(M.getOrInsertFunction("spawnUtility", Ty).getCallee());
-		Builder.CreateCall(Ty, spawnUtility);
-		Builder.CreateRetVoid();
-		Builder.SetInsertPoint(StartEntry);
-	}
 
 	Function* GetEnviron = M.getFunction("__syscall_main_environ");
 	if (GetEnviron)
@@ -69,6 +61,16 @@ PreservedAnalyses CallConstructorsPass::run(llvm::Module &M, llvm::ModuleAnalysi
 	{
 		Builder.CreateCall(Ty, cast<Function>(C->getAggregateElement(1)->stripPointerCastsSafe()));
 	}
+
+	if (!LowerAtomics)
+	{
+		// If -pthread is passed, add a call to spawnUtility to setup the utility thread.
+		Function* spawnUtility = cast<Function>(M.getOrInsertFunction("spawnUtility", Ty).getCallee());
+		Builder.CreateCall(Ty, spawnUtility);
+		Builder.CreateRetVoid();
+		Builder.SetInsertPoint(StartEntry);
+	}
+
 	Function* Main = getMainFunction(M);
 	bool Wasi = Triple(M.getTargetTriple()).getOS() == Triple::WASI;
 	if (Wasi || (Main && Main->getSection() == "asmjs"))
