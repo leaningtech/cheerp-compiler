ba4359060d72ad9d2eca9779527e75b29c745116
Yuri Iozzelli <y.iozzelli@gmail.com>
2025-08-07 11:01:43 +0200

    Revert "WIP bail out when visiting address space casts in SROA to work around casts"
    This reverts commit 2a3b0e22269625d5f65835307c285610998ed88c.

--- llvm/lib/Transforms/Scalar/SROA.cpp
+++ llvm/lib/Transforms/Scalar/SROA.cpp
@@ -729,7 +729,6 @@ private:
   void visitAddrSpaceCastInst(AddrSpaceCastInst &ASC) {
     if (ASC.use_empty())
       return markAsDead(ASC);
-    return PI.setAborted(&ASC);
 
     return Base::visitAddrSpaceCastInst(ASC);
   }
